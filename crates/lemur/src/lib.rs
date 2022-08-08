ffceeb7d9c9b8326b126a7a1f993f7d9f500ffe9
Sean Smith <scsmithr@gmail.com>
2022-08-08 13:43:20 -0500

    Add crate docs for sqlengine and lemur (#7)
    * Add crate docs for sqlengine and lemur
    
    * Apply suggestions from code review
    
    Co-authored-by: Justin Rubek <25621857+justinrubek@users.noreply.github.com>
    
    Co-authored-by: Justin Rubek <25621857+justinrubek@users.noreply.github.com>

--- crates/lemur/src/lib.rs
+++ crates/lemur/src/lib.rs
@@ -1,2 +1,37 @@
+//! Lemur provides data representations for the system and execution operations.
+//!
+//! The choice of "lemur" comes from the homemade acronym LMIR, which itself is
+//! a combination of LIR (low-level intermediate representation) and MIR
+//! (mid-level intermediate representation).
+//!
+//! # Data representation
+//!
+//! A partial goal of lemur is to provide and MIR and LIR for the system. The
+//! core type that much of the system will be concerned with is the `DataFrame`.
+//! `DataFrame`s are made up of an ordered list of value vectors, with each
+//! value vector being a homogeneous list of values of the same type. During
+//! expression evaluation, expressions will be evaluated against entire
+//! dataframes.
+//!
+//! # Expressions
+//!
+//! Lemur provides two categories of expressions, scalar expressions, and
+//! relational expressions. A scalar expression is an expression that logically
+//! acts on an individual values for each row. For example, adding two columns
+//! together would be a scalar expression. On the other hand, relational
+//! expressions are expressions that logically act on entire relations at a
+//! time. This includes things like joins and filtering.
+//!
+//! During query planning, the goal of the sqlengine is to transform a SQL ast
+//! into a single relation expression. It is this expression that then gets
+//! executed.
+//!
+//! # Execution
+//!
+//! Once we have a relational expression, we can then start execution. Each node
+//! of the expression tree operates in a pull-based manner, with each node
+//! producing a stream of dataframes to operate on.
+//!
+//! Execution is completed when the top-level dataframe stream completes.
 pub mod execute;
 pub mod repr;
--- crates/sqlengine/src/lib.rs
+++ crates/sqlengine/src/lib.rs
@@ -1,3 +1,38 @@
+//! High-level SQL query planning and execution.
+//!
+//! # Life of a query
+//!
+//! The first stop for a query entering the system is parsing. Parsing is done
+//! via the sqlparser-rs library using the Postgres dialect option.
+//!
+//! If the query is transaction related ("commit", "begin", "rollback"), the
+//! session for that connection executes the appropriate functions for creating,
+//! committing, or rolling back the transaction. The result of which is then
+//! returned to the client, and the query is finished.
+//!
+//! DML queries ("select", "insert", etc) and DDL queries ("create table", etc)
+//! go through a planning phase which attempts to reconcile table names and other
+//! identifiers with physical items in the database as reported by the catalog.
+//! If the catalog errors for whatever reason (missing table, etc), the query
+//! exits. During planning, the query is transformed to an equivalent
+//! intermediate representation with appropriately bound references to tables and
+//! whatnot. This intermediate representation is very close to lemur's
+//! `RelationExpr`.
+//!
+//! Once the query is planned and we have our intermediate representation, we
+//! perform some rule based rewrites for a more optimal query. Additional
+//! optimization will be added in the future.
+//!
+//! After optimization, the intermediate representation is lowered in to a lemur
+//! `RelationExpr`. It is this final expression that gets executed. The result of
+//! execution is returned to the client.
+//!
+//! # Transactions
+//!
+//! Every query must execute within the context of a transaction. If the user
+//! hasn't started a transaction, a non-interactive transaction will be created
+//! and used. If the user has started a transaction, that transaction will be
+//! used until the user commits or rolls back the transaction.
 pub mod catalog;
 pub mod engine;
 pub mod plan;
