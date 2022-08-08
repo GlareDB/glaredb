d5823efe4446b3fe98b68737f5d21030c970d6bd
Sean Smith <scsmithr@gmail.com>
2022-08-08 13:27:31 -0500

    Add example to execute a query
    `cargo run --example execute "select * from (values (1, 'hello'), (2,'world'));"`
    
    results in:
    
    ```
    QueryResult { df: df: num rows: 2, num_cols: 2
    0: Int8(Some(1)) Utf8(Some("hello"))
    1: Int8(Some(2)) Utf8(Some("world"))  }
    ```
    
    Note that the catalog interface currently panics, so this example cannot
    reference any tables right now. Eventually the catalog should be stubbable, e.g.:
    https://github.com/GlareDB/glaredb/blob/e24d4cc035cca5d3f72e1651228e12a547a96cca/crates/sqlengine/examples/logicalplan.rs#L60
    
    This also showcases a bug where `select 1` returns nothing.

---
+++ crates/sqlengine/examples/execute.rs
@@ -0,0 +1,22 @@
+//! Execute a query string against an in-memory data source.
+use lemur::execute::stream::source::MemoryDataSource;
+use sqlengine::engine::Engine;
+use sqlengine::server::{Client, Response, Server};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    logutil::init(1);
+
+    let query = std::env::args().nth(1).ok_or("expected query argument")?;
+
+    let source = MemoryDataSource::new();
+    let engine = Engine::new(source);
+    let mut session = engine.begin_session()?;
+
+    let results = session.execute_query(&query).await?;
+    for result in results.into_iter() {
+        println!("{:?}", result);
+    }
+
+    Ok(())
+}
