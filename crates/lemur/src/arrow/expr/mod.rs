2ff1a1f99ad89381e8cfbab7809a48100e67bc01
Sean Smith <scsmithr@gmail.com>
2022-10-08 12:17:34 -0500

    Remove lemur crate

--- crates/lemur/src/arrow/expr/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod scalar;
-pub use scalar::*;
--- crates/lemur/src/repr/expr/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod scalar;
-pub use scalar::*;
-pub mod relation;
-pub use relation::*;
-pub mod mutation;
-pub use mutation::*;
-pub mod explain;
-pub use explain::*;
