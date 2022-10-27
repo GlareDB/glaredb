77046f85dff499ee1c1283df4f740cdb448abf5b
Sean Smith <scsmithr@gmail.com>
2022-10-26 20:34:23 -0500

    chore: Get nix checks working on darwin
    - Moves clang from `otherBuildInputs` to `otherNativeBuildInputs` for
    "stddefs.h"
    - Add libiconv and apply security framework (aes) to native build inputs.
    - Minor formatting.

--- flake-parts/lib/default.nix
+++ flake-parts/lib/default.nix
@@ -7,26 +7,35 @@
   inherit (inputs.gitignore.lib) gitignoreSource;
 in {
   flake.lib = {
-    otherNativeBuildInputs = pkgs: with pkgs; [pkgconfig openssl openssl.dev llvmPackages.bintools llvmPackages.libclang];
+    otherNativeBuildInputs = pkgs: with pkgs; [
+      pkgconfig
+      openssl
+      openssl.dev
+      clang
+      llvmPackages.bintools
+      llvmPackages.libclang
+    ] ++ lib.optional stdenv.isDarwin [
+      libiconv
+      darwin.apple_sdk.frameworks.Security
+    ];
     otherBuildInputs = pkgs:
       with pkgs; [
-        clang
         openssl
         protobuf
       ];
 
     # Filters for files to include in nix store
     filterProto = orig_path: type:
-    let
-      path = (toString orig_path);
-      base = baseNameOf path;
+      let
+        path = (toString orig_path);
+        base = baseNameOf path;
 
-      matchesSuffix = lib.any (suffix: lib.hasSuffix suffix base) [
-        # Include all proto files
-        ".proto"
-      ];
-    in
-      type == "directory" || matchesSuffix;
+        matchesSuffix = lib.any (suffix: lib.hasSuffix suffix base) [
+          # Include all proto files
+          ".proto"
+        ];
+      in
+        type == "directory" || matchesSuffix;
 
     filterSrc = craneLib: orig_path: type:
       (self.lib.filterProto orig_path type) || (craneLib.filterCargoSources orig_path type);
