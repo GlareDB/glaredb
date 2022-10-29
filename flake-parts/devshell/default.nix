d774c68b6f1b1ad1ef377e675bb97e11dbf32229
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-10-29 03:10:08 -0500

    ci: add docker-credential component to google-cloud-sdk

--- flake-parts/devshell/default.nix
+++ flake-parts/devshell/default.nix
@@ -25,7 +25,10 @@
     ];
 
     ciPackages = [
-      (pkgs.google-cloud-sdk.withExtraComponents ([pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin])) 
+      (pkgs.google-cloud-sdk.withExtraComponents ([
+        pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin
+        pkgs.google-cloud-sdk.components.docker-credential-gcr
+      ])) 
       pkgs.kubectl
     ];
 
