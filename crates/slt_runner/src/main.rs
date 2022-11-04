2cff9317b99dad527efbfb1598e6663ea9c74d41
Sean Smith <scsmithr@gmail.com>
2022-11-04 14:55:46 -0500

    chore: Allow slt_runner to run against external databases (#274)
    Subcommands now determine if SLT tests are ran against an embedded or external
    database.

--- crates/slt_runner/src/main.rs
+++ crates/slt_runner/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use glaredb::server::{Server, ServerConfig};
 use glob::glob;
 use sqllogictest::{AsyncDB, Runner};
@@ -19,76 +19,117 @@ use tokio_postgres::{Client, Config as ClientConfig, NoTls, SimpleQueryMessage};
 struct Cli {
     #[clap(short, long, action = clap::ArgAction::Count)]
     verbose: u8,
+    #[clap(subcommand)]
+    command: Commands,
+}
 
-    /// TCP address to bind to for the GlareDB server.
-    ///
-    /// Omitting this will attempt to bind to any available port.
-    #[clap(long, value_parser)]
-    bind: Option<String>,
+#[derive(Subcommand)]
+enum Commands {
+    /// Start an embedded GlareDB server to execute the tests against.
+    Embedded {
+        /// TCP address to bind to for the GlareDB server.
+        ///
+        /// Omitting this will attempt to bind to any available port.
+        #[clap(long, value_parser)]
+        bind: Option<String>,
 
-    /// Whether or not to keep the GlareDB server running after a failure.
-    ///
-    /// This allow for an external client to connect to allow for additional
-    /// debugging.
-    #[clap(long, value_parser)]
-    keep_running: bool,
+        /// Whether or not to keep the GlareDB server running after a failure.
+        ///
+        /// This allow for an external client to connect to allow for additional
+        /// debugging.
+        #[clap(long, value_parser)]
+        keep_running: bool,
 
-    /// Name of the database to connect to.
-    #[clap(short, long, value_parser, default_value_t = String::from("slt"))]
-    db_name: String,
+        /// Name of the database to connect to.
+        #[clap(short, long, value_parser, default_value_t = String::from("slt"))]
+        db_name: String,
 
-    /// Type of object storage database will access
-    #[clap(short, long, value_parser, default_value_t = String::from("local"))]
-    object_store: String,
+        /// Type of object storage database will access
+        #[clap(short, long, value_parser, default_value_t = String::from("local"))]
+        object_store: String,
 
-    /// Path to test files.
-    files: Vec<String>,
+        /// Path to test files.
+        files: Vec<String>,
+    },
+
+    /// Connect to a remote instance to execute tests against.
+    External {
+        /// Connection string to use for connecting to the database.
+        #[clap(short, long, value_parser)]
+        connection_str: String,
+
+        /// Path to test files.
+        files: Vec<String>,
+    },
+}
+
+impl Commands {
+    fn collect_globbed_files(&self) -> Result<Vec<PathBuf>> {
+        let files = match self {
+            Commands::Embedded { files, .. } => files.clone(),
+            Commands::External { files, .. } => files.clone(),
+        };
+        Ok(files
+            .into_iter()
+            .map(|s| glob(&s))
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .flatten()
+            .collect::<Result<Vec<_>, _>>()?)
+    }
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
     logutil::init(cli.verbose, false);
 
-    let files = cli
-        .files
-        .into_iter()
-        .map(|s| glob(&s))
-        .collect::<Result<Vec<_>, _>>()?
-        .into_iter()
-        .flatten()
-        .collect::<Result<Vec<_>, _>>()?;
+    let files = cli.command.collect_globbed_files()?;
     if files.is_empty() {
         println!("no files, exiting...");
         return Ok(());
     }
 
     let runtime = Builder::new_multi_thread().enable_all().build()?;
-    runtime.block_on(async move {
-        let pg_listener =
-            TcpListener::bind(cli.bind.unwrap_or_else(|| "localhost:0".to_string())).await?;
-        let pg_addr = pg_listener.local_addr()?;
-        let conf = ServerConfig { pg_listener };
+    match cli.command {
+        Commands::Embedded {
+            bind,
+            keep_running,
+            db_name,
+            object_store,
+            ..
+        } => runtime.block_on(async move {
+            let pg_listener =
+                TcpListener::bind(bind.unwrap_or_else(|| "localhost:0".to_string())).await?;
+            let pg_addr = pg_listener.local_addr()?;
+            let conf = ServerConfig { pg_listener };
 
-        let server = Server::connect(cli.db_name, &cli.object_store).await?;
-        let _ = tokio::spawn(server.serve(conf));
+            let server = Server::connect(db_name, &object_store).await?;
+            let _ = tokio::spawn(server.serve(conf));
 
-        let runner = TestRunner::connect(pg_addr).await?;
-        match runner.exec_tests(&files).await {
-            Ok(taken) => {
-                println!("tests completed in {:?}", taken);
-                Ok(())
-            }
-            Err(e) => {
-                if cli.keep_running {
-                    eprintln!("{}", e);
-                    println!("keeping the server running, addr: {}", pg_addr);
-                    println!("CTRL-C to exit");
-                    tokio::signal::ctrl_c().await?;
+            let runner = TestRunner::connect_embedded(pg_addr).await?;
+            match runner.exec_tests(&files).await {
+                Ok(taken) => {
+                    println!("tests completed in {:?}", taken);
+                    Ok(())
                 }
-                Err(e)
+                Err(e) => {
+                    if keep_running {
+                        eprintln!("{}", e);
+                        println!("keeping the server running, addr: {}", pg_addr);
+                        println!("CTRL-C to exit");
+                        tokio::signal::ctrl_c().await?;
+                    }
+                    Err(e)
+                }
             }
-        }
-    })
+        }),
+        Commands::External { connection_str, .. } => runtime.block_on(async move {
+            let runner = TestRunner::connect_external(&connection_str).await?;
+            let taken = runner.exec_tests(&files).await?;
+            println!("tests completed in {:?}", taken);
+            Ok(())
+        }),
+    }
 }
 
 struct TestRunner {
@@ -97,8 +138,8 @@ struct TestRunner {
 }
 
 impl TestRunner {
-    /// Connect to the database at the given addr.
-    async fn connect(pg_addr: SocketAddr) -> Result<TestRunner> {
+    /// Connect to an embedded database at the given addr.
+    async fn connect_embedded(pg_addr: SocketAddr) -> Result<TestRunner> {
         let host = pg_addr.ip().to_string();
         let port = pg_addr.port();
         let (client, conn) = ClientConfig::new()
@@ -110,6 +151,17 @@ impl TestRunner {
             .connect(NoTls)
             .await?;
 
+        let (conn_err_tx, conn_err_rx) = oneshot::channel();
+        tokio::spawn(async move { conn_err_tx.send(conn.await) });
+
+        Ok(TestRunner {
+            client: TestClient { client },
+            conn_err: conn_err_rx,
+        })
+    }
+
+    async fn connect_external(conn: &str) -> Result<TestRunner> {
+        let (client, conn) = tokio_postgres::connect(conn, NoTls).await?;
         let (conn_err_tx, conn_err_rx) = oneshot::channel();
         tokio::spawn(async move { conn_err_tx.send(conn.await) });
 
