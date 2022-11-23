223dd268e6268ae6cefe3b6ef641ef5d3f79043f
Sean Smith <scsmithr@gmail.com>
2022-11-23 06:30:02 -0600

    chore: Rework background worker to be multi-threaded (#334)
    * chore: Rework background worker to be multi-threaded
    
    Previously ran on a single thread. Since we skipped missed ticks, this could
    result in some workers not actually being ran. This change makes it so that
    every background job has its own (tokio) thread with independent interval
    checking.
    
    This also introduces the `BackgroundJob` trait to act as an interface to for all
    background jobs. This made stubbing out a dummy job for test pretty simple. This
    can be expanded in the future to accomadate background jobs for other parts of
    the system.
    
    * fixup! chore: Rework background worker to be multi-threaded
    
    * Check correct dummy
    
    * close -> clone

--- crates/background/src/lib.rs
+++ crates/background/src/lib.rs
@@ -2,37 +2,60 @@
 pub mod errors;
 pub mod storage;
 
-use access::runtime::AccessRuntime;
-use cloud::client::CloudClient;
-use common::background::BackgroundConfig;
-use std::sync::Arc;
+use std::fmt;
+use std::future::Future;
+use std::pin::Pin;
 use std::time::Duration;
-use tokio::time::{Interval, MissedTickBehavior};
-use tracing::{debug, debug_span, error, info, Instrument};
-
-use storage::{DatabaseStorageUsageJob, DatabaseStorageUsageSink};
+use tokio::sync::oneshot;
+use tokio::time::Interval;
+use tracing::{debug, error};
 
 const DEBUG_DURATION: Duration = Duration::from_secs(60);
+
+pub trait BackgroundJob: Send + fmt::Debug {
+    /// Return the interval that this job should run on.
+    ///
+    /// This should return an interval with the appropriate skipped behavior
+    /// necessary for the job.
+    fn interval(&self) -> Interval;
+
+    /// Return the future for completing the job. The future will be sent to
+    /// some background thread for execution.
+    fn job_fut(&self) -> Pin<Box<dyn Future<Output = ()> + Send + '_>>;
+}
+
+/// A simple job that prints a debug log at some interval. Useful to check that
+/// the background worker is running.
+#[derive(Debug)]
+pub struct DebugJob;
+
+impl BackgroundJob for DebugJob {
+    fn interval(&self) -> Interval {
+        tokio::time::interval(DEBUG_DURATION)
+    }
+
+    fn job_fut(&self) -> Pin<Box<dyn Future<Output = ()> + Send>> {
+        Box::pin(async {
+            debug!("debug interval hit");
+        })
+    }
+}
 
 /// Run all background jobs on periodic intervals.
 #[derive(Debug)]
 pub struct BackgroundWorker {
-    conf: BackgroundConfig,
-    cloud_client: Option<Arc<CloudClient>>,
-    runtime: Arc<AccessRuntime>,
+    jobs: Vec<Box<dyn BackgroundJob>>,
+    // NOTE: Only currently used to avoid dropping tokio task handles.
+    shutdown: oneshot::Receiver<()>,
 }
 
 impl BackgroundWorker {
     pub fn new(
-        conf: BackgroundConfig,
-        cloud_client: Option<Arc<CloudClient>>,
-        runtime: Arc<AccessRuntime>,
+        jobs: impl IntoIterator<Item = Box<dyn BackgroundJob>>,
+        shutdown: oneshot::Receiver<()>,
     ) -> BackgroundWorker {
-        BackgroundWorker {
-            conf,
-            cloud_client,
-            runtime,
-        }
+        let jobs = jobs.into_iter().collect();
+        BackgroundWorker { jobs, shutdown }
     }
 
     /// Begin the background worker.
@@ -40,48 +63,76 @@ impl BackgroundWorker {
     /// Note that this handles all errors internally. Errors should not stop the
     /// worker from continuing to process jobs.
     pub async fn begin(self) {
-        debug!("starting background worker");
-        let usage_job = DatabaseStorageUsageJob::new(self.runtime.clone());
-        let usage_sink = DatabaseStorageUsageSink::new(self.cloud_client.clone());
-        let mut usage_interval = interval_with_skipped_ticks(self.conf.storage_reporting.interval);
+        debug!(jobs = ?self.jobs, "starting background worker");
 
-        let mut debug_interval = interval_with_skipped_ticks(DEBUG_DURATION);
-
-        loop {
-            tokio::select! {
-                _ = usage_interval.tick() => Self::handle_usage_job(&usage_job, &usage_sink).await,
-                _ = debug_interval.tick() => debug!("debug interval hit"),
-            }
-        }
-    }
-
-    async fn handle_usage_job(job: &DatabaseStorageUsageJob, sink: &DatabaseStorageUsageSink) {
-        let span = debug_span!("database_storage_usage_job");
-        async move {
-            match job.compute_storage_total_bytes().await {
-                Ok(usage_bytes) => {
-                    info!(%usage_bytes, "total storage used");
-                    if let Err(e) = sink.send_usage(usage_bytes).await {
-                        error!(%e, "failed to send usage bytes to sink");
-                    }
+        // Spin up a thread for each job.
+        for job in self.jobs.into_iter() {
+            let mut interval = job.interval();
+            let _handle = tokio::spawn(async move {
+                loop {
+                    interval.tick().await;
+                    let job_fut = job.job_fut();
+                    job_fut.await;
                 }
-                Err(e) => {
-                    error!(%e, "failed to compute total storage usage");
-                }
-            }
+            });
         }
-        .instrument(span)
-        .await;
+
+        if let Err(e) = self.shutdown.await {
+            error!(%e, "failed to await background worker shutdown channel");
+        };
     }
 }
 
-/// Create an interval with skipped ticks.
-///
-/// By default, intervals will "burst" to catch up if ticks are skipped. Since
-/// our jobs may be somewhat long lived, bursting is not ideal for us as it
-/// could lead to background jobs doing a lot of expensive and useless work.
-fn interval_with_skipped_ticks(dur: Duration) -> Interval {
-    let mut interval = tokio::time::interval(dur);
-    interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
-    interval
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::Arc;
+
+    #[derive(Debug, Default, Clone)]
+    struct DummyJob {
+        hits: Arc<AtomicUsize>,
+    }
+
+    impl BackgroundJob for DummyJob {
+        fn interval(&self) -> Interval {
+            tokio::time::interval(Duration::from_millis(100))
+        }
+
+        fn job_fut(&self) -> Pin<Box<dyn Future<Output = ()> + Send + '_>> {
+            Box::pin(async move {
+                self.hits.fetch_add(1, Ordering::Relaxed);
+            })
+        }
+    }
+
+    #[tokio::test]
+    async fn independent_loops() {
+        let dummy1 = DummyJob::default();
+        let dummy2 = DummyJob::default();
+
+        let (tx, rx) = oneshot::channel();
+        // Note we clone to retain a reference to atomic counters.
+        let worker = BackgroundWorker::new(
+            [
+                Box::new(dummy1.clone()) as Box<dyn BackgroundJob>,
+                Box::new(dummy2.clone()) as Box<dyn BackgroundJob>,
+            ],
+            rx,
+        );
+
+        tokio::spawn(worker.begin());
+
+        // Ensure at least two iterations have passed for the dummy jobs.
+        tokio::time::sleep(Duration::from_millis(200)).await;
+
+        // Shutdown
+        tx.send(()).unwrap();
+
+        // Check that each dummy job was called at least twice.
+        let hits1 = dummy1.hits.load(Ordering::Relaxed);
+        assert!(hits1 >= 2);
+        let hits2 = dummy2.hits.load(Ordering::Relaxed);
+        assert!(hits2 >= 2);
+    }
 }
