01e6aae2874c90995815fb29bf714c6294501bfd
Sean Smith <scsmithr@gmail.com>
2022-12-12 14:16:40 -0600

    fix: Actually fix version
    Adds an `GIT_TAG_OVERRIDE` that's set by nix during build time. This uses nix's
    `rev` thing. On untagged commits, it'll be the full commit hash, on tagged
    commits, it'll be the tag.
    
    Fixes https://github.com/GlareDB/glaredb/issues/398

--- crates/buildenv/build.rs
+++ crates/buildenv/build.rs
@@ -1,12 +1,21 @@
 use std::process::Command;
 
+/// Environment variable to use to override the git tag. If unset, we'll shell
+/// out `git` to get the tag info.
+const GIT_TAG_OVERRIDE: &str = "GIT_TAG_OVERRIDE";
+
 fn main() {
-    let git_tag = match Command::new("git")
-        .args(["describe", "--tags", "--always"])
-        .output()
-    {
-        Ok(output) => String::from_utf8(output.stdout).unwrap(),
-        Err(_) => String::from("unknown"),
+    let git_tag = match std::env::var(GIT_TAG_OVERRIDE) {
+        Ok(tag) => tag,
+        Err(_) => {
+            match Command::new("git")
+                .args(["describe", "--tags", "--always"])
+                .output()
+            {
+                Ok(output) => String::from_utf8(output.stdout).unwrap(),
+                Err(_) => String::from("unknown"),
+            }
+        }
     };
     println!("cargo:rustc-env=GIT_TAG={}", git_tag);
 }
