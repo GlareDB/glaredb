1959a9b93b42f96ad9dbb878df729893d3b5f4d8
Sean Smith <scsmithr@gmail.com>
2023-02-03 18:08:58 -0600

    fix: Fix allowing duplicate names (#592)
    * fix: Fix allowing duplicate names
    
    Attempting to add connections (and other objects) with the same name should now
    always error.
    
    The fix was the order in which we appended builtin objects. In short, we we're
    overwriting the `schema_objects` map for the "public" schema. No tests caught
    this for two reasons:
    
    1. Builtins were added later.
    2. No tests were added that intermingled a user object with a builtin object.
    
    In addition to 1, we were also incorrectly persisting builtin objects. I've made
    that change, and added a small comment to the storage proto, so that this is
    documented in multiple places (in addition to what's in `builtin.rs`).
    
    * Check that we don't load builtins

--- crates/metastore/src/errors.rs
+++ crates/metastore/src/errors.rs
@@ -3,6 +3,17 @@ pub enum MetastoreError {
     #[error("Duplicate name: {0}")]
     DuplicateName(String),
 
+    #[error("Duplicate object names found during load; name {name}, schema: {schema}, first: {first}, second: {second}")]
+    DuplicateNameFoundDuringLoad {
+        name: String,
+        schema: u32,
+        first: u32,
+        second: u32,
+    },
+
+    #[error("Builtin object persisted when it shouldn't have been: {0:?}")]
+    BuiltinObjectPersisted(crate::types::catalog::EntryMeta),
+
     #[error("Missing database catalog: {0}")]
     MissingCatalog(uuid::Uuid),
 
@@ -20,6 +31,12 @@ pub enum MetastoreError {
 
     #[error("Invalid database id: {0:?}")]
     InvalidDatabaseId(Vec<u8>),
+
+    #[error("Schema {schema} has non-zero parent: {parent}")]
+    SchemaHasNonZeroParent { schema: u32, parent: u32 },
+
+    #[error("Object {object} has invalid parent id: {parent}")]
+    ObjectHasInvalidParentId { object: u32, parent: u32 },
 
     #[error(transparent)]
     Storage(#[from] crate::storage::StorageError),
