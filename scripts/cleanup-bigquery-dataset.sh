2a74d9c9fa0853c6bc12d894d041010a368d5116
Vaibhav Rabber <vrongmeal@gmail.com>
2023-02-25 21:23:57 +0530

    fix: cleanup bigquery dataset using correct branch after merge (#692)
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

--- scripts/cleanup-bigquery-dataset.sh
+++ scripts/cleanup-bigquery-dataset.sh
@@ -4,14 +4,10 @@
 
 set -e
 
-# BIGQUERY_DATASET_ID is later used to run the tests.
-# Override exists for CI, not recommended to be used by dev.
-if [ -z "$BIGQUERY_DATASET_ID" ]; then
-	BQ_DATASET=$(git branch --show-current | base64)
-	BQ_DATASET="glaredb_test_$BQ_DATASET"
-else
-	BQ_DATASET="$BIGQUERY_DATASET_ID"
-fi
+GIT_BRANCH=$1
+
+BQ_DATASET=$(echo "$GIT_BRANCH" | base64)
+BQ_DATASET="glaredb_test_$BQ_DATASET"
 
 LOCATION="US"
 BQ="bq --location=${LOCATION} --project_id=${GCP_PROJECT_ID}"
