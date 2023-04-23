8ff0887b3766ee9a260f11f061830c9ee2a47b5f
Sean Smith <scsmithr@gmail.com>
2023-04-23 13:32:34 -0500

    Run queries

--- scripts/run-tpch-queries.sh
+++ scripts/run-tpch-queries.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
 : ${CONNECTION_STRING?"CONNECTION_STRING needs to be set"}
 : ${GCP_SERVICE_ACCOUNT_JSON?"GCP_SERVICE_ACCOUNT_JSON needs to be set"}
 
 sf="${SCALE_FACTOR:-1}"
 
+schema="tpch_${RANDOM}"
+echo "SCHEMA: ${schema}"
+
 psql "${CONNECTION_STRING}" -c "
+SET force_catalog_refresh TO true;
+CREATE SCHEMA ${schema};
+SET search_path TO ${schema};
 CREATE EXTERNAL TABLE customer FROM gcs OPTIONS (service_account_key = '${GCP_SERVICE_ACCOUNT_JSON}', bucket = 'glaredb-benchmarks', location = 'data/tpch/sf${sf}/customer/part-0.parquet' );
 CREATE EXTERNAL TABLE lineitem FROM gcs OPTIONS (service_account_key = '${GCP_SERVICE_ACCOUNT_JSON}', bucket = 'glaredb-benchmarks', location = 'data/tpch/sf${sf}/lineitem/part-0.parquet' );
 CREATE EXTERNAL TABLE nation FROM gcs OPTIONS (service_account_key = '${GCP_SERVICE_ACCOUNT_JSON}', bucket = 'glaredb-benchmarks', location = 'data/tpch/sf${sf}/nation/part-0.parquet' );
@@ -16,3 +24,10 @@ CREATE EXTERNAL TABLE region FROM gcs OPTIONS (service_account_key = '${GCP_SERV
 CREATE EXTERNAL TABLE supplier FROM gcs OPTIONS (service_account_key = '${GCP_SERVICE_ACCOUNT_JSON}', bucket = 'glaredb-benchmarks', location = 'data/tpch/sf${sf}/supplier/part-0.parquet' );
 "
 
+for i in $(seq 1 22); do
+    echo "query ${i}"
+    psql "${CONNECTION_STRING}" -c "SET search_path TO ${schema};" -c '\timing' -f "./testdata/tpch/${i}.sql" | grep 'Time'
+done;
+
+
+psql "${CONNECTION_STRING}" -c "DROP SCHEMA ${schema} CASCADE;"
--- testdata/tpch/1.sql
+++ testdata/tpch/1.sql
@@ -15,7 +15,7 @@ select
 from
 	lineitem
 where
-	l_shipdate <= date '1998-12-01' - interval '90' day (3)
+	l_shipdate <= date '1998-12-01' - interval '90'
 group by
 	l_returnflag,
 	l_linestatus
