319f097632f0cd2b672655929ff5d0194eadd45b
Sean Smith <scsmithr@gmail.com>
2023-05-23 13:31:01 -0500

    feat: Add clickbench scripts and queries (#1014)
    Also changes where tpch benchmark artifacts are downloaded. Everything is placed
    in `./benchmarks/artifacts/` now.

--- benchmarks/tpch/load_s100.sql
+++ benchmarks/tpch/load_s100.sql
@@ -1,8 +1,8 @@
-CREATE EXTERNAL TABLE customer FROM local OPTIONS (location = './gcs-artifacts/tpch_100/customer/part-0.parquet');
-CREATE EXTERNAL TABLE lineitem FROM local OPTIONS (location = './gcs-artifacts/tpch_100/lineitem/part-0.parquet');
-CREATE EXTERNAL TABLE nation FROM local OPTIONS (location = './gcs-artifacts/tpch_100/nation/part-0.parquet');
-CREATE EXTERNAL TABLE orders FROM local OPTIONS (location = './gcs-artifacts/tpch_100/orders/part-0.parquet');
-CREATE EXTERNAL TABLE part FROM local OPTIONS (location = './gcs-artifacts/tpch_100/part/part-0.parquet');
-CREATE EXTERNAL TABLE partsupp FROM local OPTIONS (location = './gcs-artifacts/tpch_100/partsupp/part-0.parquet');
-CREATE EXTERNAL TABLE region FROM local OPTIONS (location = './gcs-artifacts/tpch_100/region/part-0.parquet');
-CREATE EXTERNAL TABLE supplier FROM local OPTIONS (location = './gcs-artifacts/tpch_100/supplier/part-0.parquet');
+CREATE EXTERNAL TABLE customer FROM local OPTIONS (location = './benchmarks/artifacts/tpch_100/customer/part-0.parquet');
+CREATE EXTERNAL TABLE lineitem FROM local OPTIONS (location = './benchmarks/artifacts/tpch_100/lineitem/part-0.parquet');
+CREATE EXTERNAL TABLE nation FROM local OPTIONS (location = './benchmarks/artifacts/tpch_100/nation/part-0.parquet');
+CREATE EXTERNAL TABLE orders FROM local OPTIONS (location = './benchmarks/artifacts/tpch_100/orders/part-0.parquet');
+CREATE EXTERNAL TABLE part FROM local OPTIONS (location = './benchmarks/artifacts/tpch_100/part/part-0.parquet');
+CREATE EXTERNAL TABLE partsupp FROM local OPTIONS (location = './benchmarks/artifacts/tpch_100/partsupp/part-0.parquet');
+CREATE EXTERNAL TABLE region FROM local OPTIONS (location = './benchmarks/artifacts/tpch_100/region/part-0.parquet');
+CREATE EXTERNAL TABLE supplier FROM local OPTIONS (location = './benchmarks/artifacts/tpch_100/supplier/part-0.parquet');
