5205a84585c5ddf2e8fb1086e5018e399a5e2333
Sean Smith <scsmithr@gmail.com>
2023-05-25 19:18:17 -0500

    chore: Add release workflow (#1018)
    * chore: Add release workflow
    
    * fixup! chore: Add release workflow
    
    * fixup! chore: Add release workflow
    
    * fixup! chore: Add release workflow
    
    * windows no go
    
    * Be less ambitious
    
    * please
    
    * I would like at least one to work
    
    * fixup! I would like at least one to work
    
    * :pray:
    
    * feat: Add xtask
    
    * Add some other ci stuff
    
    * set protoc
    
    * fixup! set protoc
    
    * try
    
    * typo
    
    * Skip windows for now
    
    * chore: Add `dist` command to xtask
    
    This will build the binary, and create a zip in `target/dist/<out>.zip`
    
    * single zip
    
    * fixup! single zip
    
    * override target
    
    * cron

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,13 +1,12 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use std::env;
 use std::io::Cursor;
-use std::path::{Path, PathBuf};
 use target::Target;
 use xshell::{cmd, Shell};
 use zip::ZipArchive;
 
 mod target;
+mod util;
 
 #[derive(Parser)]
 #[clap(name = "xtask")]
@@ -40,12 +39,21 @@ enum Commands {
 
     /// Check formatting.
     FmtCheck,
+
+    /// Build the dist binary for release.
+    ///
+    /// A zip archive will be placed in `target/dist` containing the release
+    /// binary.
+    ///
+    /// By default, the compilation target matches the host machine. This can be
+    /// overridden via the `DIST_TARGET_TRIPLE` environment variable.
+    Dist,
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
     let sh = &Shell::new()?;
-    sh.change_dir(project_root());
+    sh.change_dir(util::project_root());
 
     let target = Target::from_cfg()?;
     ensure_protoc(sh, &target)?;
@@ -57,6 +65,7 @@ fn main() -> Result<()> {
         Commands::Test { rest } => run_tests(sh, rest.as_slice())?,
         Commands::Clippy => run_clippy(sh)?,
         Commands::FmtCheck => run_fmt_check(sh)?,
+        Commands::Dist => run_dist(sh, &target)?,
     }
 
     Ok(())
@@ -88,33 +97,48 @@ fn run_fmt_check(sh: &Shell) -> Result<()> {
     Ok(())
 }
 
+fn run_dist(sh: &Shell, target: &Target) -> Result<()> {
+    let triple = target.dist_target_triple()?;
+    cmd!(sh, "cargo build --release --bin glaredb --target {triple}").run()?;
+
+    // TODO: Code signing goes here.
+
+    sh.remove_path(util::project_root().join("target").join("dist"))?;
+    sh.create_dir(util::project_root().join("target").join("dist"))?;
+
+    let src_path = util::project_root()
+        .join("target")
+        .join(target.dist_target_triple()?)
+        .join("release")
+        .join("glaredb");
+    let dest_path = util::project_root()
+        .join("target")
+        .join("dist")
+        .join(target.dist_zip_name()?);
+
+    util::zip(&src_path, &dest_path)?;
+
+    println!("Dist zip: {:?}", dest_path);
+    Ok(())
+}
+
 /// Check if protoc is in path. If not, download and set the PROTOC env var.
 fn ensure_protoc(sh: &Shell, target: &Target) -> Result<()> {
     const PROTOC_OUT_DIR: &str = "deps/protoc";
     const PROTOC_PATH: &str = "deps/protoc/bin/protoc";
 
     if cmd!(sh, "protoc --version").run().is_err() {
-        if sh.path_exists(project_root().join(PROTOC_PATH)) {
+        if sh.path_exists(util::project_root().join(PROTOC_PATH)) {
             println!("Downloaded protoc already exists");
         } else {
             println!("Missing protoc, downloading...");
-            sh.remove_path(project_root().join(PROTOC_OUT_DIR))?;
+            sh.remove_path(util::project_root().join(PROTOC_OUT_DIR))?;
             let res = reqwest::blocking::get(target.protoc_url()?)?;
             ZipArchive::new(Cursor::new(res.bytes()?))?
-                .extract(project_root().join(PROTOC_OUT_DIR))?;
+                .extract(util::project_root().join(PROTOC_OUT_DIR))?;
         }
 
-        sh.set_var("PROTOC", project_root().join(PROTOC_PATH));
+        sh.set_var("PROTOC", util::project_root().join(PROTOC_PATH));
     }
     Ok(())
-}
-
-fn project_root() -> PathBuf {
-    Path::new(
-        &env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| env!("CARGO_MANIFEST_DIR").to_owned()),
-    )
-    .ancestors()
-    .nth(1)
-    .unwrap()
-    .to_path_buf()
 }
