9a577e869e9965126f37ce74b19c6907473279c3
Sean Smith <scsmithr@gmail.com>
2023-06-28 10:39:17 -0500

    Return record batch on other execution results too

---
+++ py-glaredb/examples/multiple.py
@@ -0,0 +1,19 @@
+import glaredb
+import polars as pl
+
+con1 = glaredb.connect()
+con1.sql("create table hello (a int)").to_polars();
+
+t1 = con1.sql("select * from glare_catalog.tables where table_name = 'hello'").to_polars();
+print(t1)
+
+con2 = glaredb.connect()
+con2.sql("create table hello (a int)").to_polars();
+
+t2 = con2.sql("select * from glare_catalog.tables where table_name = 'hello'").to_polars();
+print(t2)
+
+try:
+    con2.sql("create table hello (a int)").to_polars();
+except Exception:
+    print("Duplicate table failed as expected")
