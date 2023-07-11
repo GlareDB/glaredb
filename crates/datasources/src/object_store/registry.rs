952d09b3c1947f8e7b196f425be95ea30608a3a7
Sean Smith <scsmithr@gmail.com>
2023-07-11 11:33:46 -0500

    feat: Add s3 to object store registry create
    Should fix panics.
    
    Closes https://github.com/GlareDB/glaredb/issues/1289

--- crates/datasources/src/object_store/registry.rs
+++ crates/datasources/src/object_store/registry.rs
@@ -2,43 +2,53 @@ use std::sync::Arc;
 
 use dashmap::DashMap;
 use datafusion::{error::DataFusionError, execution::object_store::ObjectStoreRegistry};
-use metastoreproto::{session::NamespacedCatalogEntry, types::options::TableOptionsGcs};
+use metastoreproto::{
+    session::NamespacedCatalogEntry,
+    types::options::{TableOptionsGcs, TableOptionsS3},
+};
 use object_store::{local::LocalFileSystem, ObjectStore};
 use url::Url;
 
-use crate::object_store::gcs::GcsTableAccess;
+use crate::object_store::{errors::Result, gcs::GcsTableAccess, s3::S3TableAccess};
 
 #[derive(Debug)]
 pub struct GlareDBRegistry {
-    // catalog: Vec<NamespacedCatalogEntry<'a>>,
+    /// Object stores keyed by scheme and bucket name.
+    ///
+    /// Note that this means we currently only support one set of credentials
+    /// per bucket.
     object_stores: DashMap<String, Arc<dyn ObjectStore>>,
 }
 
 impl GlareDBRegistry {
-    pub fn new<'a>(entries: impl Iterator<Item = NamespacedCatalogEntry<'a>>) -> Self {
+    /// Create a new registry for a set of catalog entries.
+    pub fn try_new<'a>(entries: impl Iterator<Item = NamespacedCatalogEntry<'a>>) -> Result<Self> {
         use metastoreproto::types::catalog::CatalogEntry;
         use metastoreproto::types::options::TableOptions;
         let object_stores: DashMap<String, Arc<dyn ObjectStore>> = DashMap::new();
         object_stores.insert("file://".to_string(), Arc::new(LocalFileSystem::new()));
         for entry in entries {
-            match entry.entry {
-                CatalogEntry::Table(tbl_entry) => match &tbl_entry.options {
+            if let CatalogEntry::Table(tbl_entry) = entry.entry {
+                match &tbl_entry.options {
                     TableOptions::Gcs(opts) => {
-                        let store = gcs_to_store(opts);
+                        let store = gcs_to_store(opts)?;
                         let key = format!("gs://{}", opts.bucket);
-
                         object_stores.insert(key, store);
                     }
-                    _ => todo!("add support for other object stores"),
-                },
-                _ => todo!("add support for other object stores"),
+                    TableOptions::S3(opts) => {
+                        let store = s3_to_store(opts)?;
+                        let key = format!("s3://{}", opts.bucket);
+                        object_stores.insert(key, store);
+                    }
+                    _ => (), // TODO: Add azure when supported.
+                }
             };
         }
-        Self { object_stores }
+        Ok(Self { object_stores })
     }
 }
 
-fn gcs_to_store(opts: &TableOptionsGcs) -> Arc<dyn ObjectStore> {
+fn gcs_to_store(opts: &TableOptionsGcs) -> Result<Arc<dyn ObjectStore>> {
     GcsTableAccess {
         bucket_name: opts.bucket.clone(),
         service_acccount_key_json: opts.service_account_key.clone(),
@@ -46,7 +56,19 @@ fn gcs_to_store(opts: &TableOptionsGcs) -> Arc<dyn ObjectStore> {
         file_type: None,
     }
     .into_object_store()
-    .unwrap()
+}
+
+fn s3_to_store(opts: &TableOptionsS3) -> Result<Arc<dyn ObjectStore>> {
+    let (store, _) = S3TableAccess {
+        region: opts.region.clone(),
+        bucket_name: opts.bucket.clone(),
+        access_key_id: opts.access_key_id.clone(),
+        secret_access_key: opts.secret_access_key.clone(),
+        location: opts.location.clone(),
+        file_type: None,
+    }
+    .store_and_path()?;
+    Ok(store)
 }
 
 impl ObjectStoreRegistry for GlareDBRegistry {
