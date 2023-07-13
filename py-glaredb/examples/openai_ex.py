85c3abd3cd56aaebe4559db11843383820510b1d
Sean Smith <scsmithr@gmail.com>
2023-07-13 12:19:34 -0500

    chore: Add example for running openai on data from postgres

---
+++ py-glaredb/examples/openai_ex.py
@@ -0,0 +1,29 @@
+import glaredb
+import os
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+
+con = glaredb.connect()
+
+host = os.environ["PG_HOST"]
+user = os.environ["PG_USER"]
+password = os.environ["PG_PASS"]
+
+con.execute(f"""
+CREATE EXTERNAL DATABASE my_pg
+	FROM postgres
+	OPTIONS (
+		host = '{host}',
+		port = '5432',
+		user = '{user}',
+		password = '{password}',
+		database = 'postgres',
+	);
+""")
+
+df = con.sql("select * from my_pg.public.users").to_pandas()
+
+llm = OpenAI(api_token=os.environ["OPEN_AI_KEY"])
+
+pandas_ai = PandasAI(llm)
+pandas_ai(df, prompt="Which users are from GlareDB?")
