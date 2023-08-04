d3f472101eea4a913e995bcab8412835000d6274
Sean Smith <scsmithr@gmail.com>
2023-08-04 09:55:40 -0500

    build(deps): Bump deps (#1486)
    * build(deps): bump rust_decimal from 1.30.0 to 1.31.0
    
    Bumps [rust_decimal](https://github.com/paupino/rust-decimal) from 1.30.0 to 1.31.0.
    - [Changelog](https://github.com/paupino/rust-decimal/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/paupino/rust-decimal/compare/1.30.0...1.31.0)
    
    ---
    updated-dependencies:
    - dependency-name: rust_decimal
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump pyo3 from 0.19.1 to 0.19.2
    
    Bumps [pyo3](https://github.com/pyo3/pyo3) from 0.19.1 to 0.19.2.
    - [Release notes](https://github.com/pyo3/pyo3/releases)
    - [Changelog](https://github.com/PyO3/pyo3/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/pyo3/pyo3/compare/v0.19.1...v0.19.2)
    
    ---
    updated-dependencies:
    - dependency-name: pyo3
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump sqllogictest from 0.15.1 to 0.15.2
    
    Bumps [sqllogictest](https://github.com/risinglightdb/sqllogictest-rs) from 0.15.1 to 0.15.2.
    - [Release notes](https://github.com/risinglightdb/sqllogictest-rs/releases)
    - [Changelog](https://github.com/risinglightdb/sqllogictest-rs/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/risinglightdb/sqllogictest-rs/compare/v0.15.1...v0.15.2)
    
    ---
    updated-dependencies:
    - dependency-name: sqllogictest
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump rustls from 0.21.5 to 0.21.6
    
    Bumps [rustls](https://github.com/rustls/rustls) from 0.21.5 to 0.21.6.
    - [Release notes](https://github.com/rustls/rustls/releases)
    - [Commits](https://github.com/rustls/rustls/compare/v/0.21.5...v/0.21.6)
    
    ---
    updated-dependencies:
    - dependency-name: rustls
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix deprecation
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- crates/datasources/src/postgres/tls.rs
+++ crates/datasources/src/postgres/tls.rs
@@ -27,7 +27,7 @@ impl MakeRustlsConnect {
 impl Default for MakeRustlsConnect {
     fn default() -> Self {
         let mut root_store = rustls::RootCertStore::empty();
-        root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
+        root_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
             rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
                 ta.subject,
                 ta.spki,
