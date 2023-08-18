0e2c1a70706fe173d9ebbd835a04310c209c1f9d
Sean Smith <scsmithr@gmail.com>
2023-08-17 19:02:02 -0500

    stream

--- crates/sqlexec/src/planner/physical_plan/send_recv.rs
+++ crates/sqlexec/src/planner/physical_plan/send_recv.rs
@@ -8,6 +8,7 @@ use datafusion::arrow::record_batch::RecordBatch;
 use datafusion::error::{DataFusionError, Result as DataFusionResult};
 use datafusion::execution::TaskContext;
 use datafusion::physical_expr::PhysicalSortExpr;
+use datafusion::physical_plan::RecordBatchStream;
 use datafusion::physical_plan::{
     stream::RecordBatchStreamAdapter, DisplayAs, DisplayFormatType, ExecutionPlan, Partitioning,
     SendableRecordBatchStream, Statistics,
@@ -121,7 +122,14 @@ impl ExecutionPlan for SendRecvJoinExec {
         // Now get the output stream we want to read the results from.
         let stream = self.input.execute(partition, context)?;
 
-        unimplemented!()
+        let stream = SendRecvJoinStream {
+            join_set,
+            stream,
+            task_did_err: false,
+            tasks_all_completed: false,
+        };
+
+        Ok(Box::pin(stream))
     }
 
     fn statistics(&self) -> Statistics {
@@ -132,5 +140,87 @@ impl ExecutionPlan for SendRecvJoinExec {
 impl DisplayAs for SendRecvJoinExec {
     fn fmt_as(&self, _t: DisplayFormatType, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "SendRecvExec")
+    }
+}
+
+/// The stream which will drive all tasks in the join set, as well as the output
+/// record batch stream.
+///
+/// This will attempt to read from the output stream before trying to make
+/// progress on driving the join set.
+struct SendRecvJoinStream {
+    /// The tasks for sending local record batches to the remote node.
+    join_set: JoinSet<Result<(), DataFusionError>>,
+    /// The stream producing output.
+    stream: SendableRecordBatchStream,
+    /// Set to true if any of the tasks errored.
+    task_did_err: bool,
+    /// Set to true if all the tasks completed without error.
+    tasks_all_completed: bool,
+}
+
+impl Stream for SendRecvJoinStream {
+    type Item = Result<RecordBatch, DataFusionError>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let this = &mut *self;
+
+        if this.task_did_err {
+            return Poll::Ready(None);
+        }
+
+        loop {
+            // Try poll stream first.
+            match this.stream.poll_next_unpin(cx) {
+                // Stream has data (batch or error)
+                Poll::Ready(out) => return Poll::Ready(out),
+
+                // Tasks all completed, we're just waiting for the stream to
+                // finish.
+                Poll::Pending if this.tasks_all_completed => return Poll::Pending,
+
+                // Only fall down to join set if we know the tasks haven't been
+                // completed yet.
+                Poll::Pending => {
+                    // Stream waiting, now try polling tokio tasks.
+                    match this.join_set.poll_join_next(cx) {
+                        // A task completed with no error. Continue to try to do
+                        // more work.
+                        Poll::Ready(Some(Ok(Ok(_)))) => continue,
+
+                        // A task completed with an error. This should
+                        // effectively stop the entire stream and completion of
+                        // other tasks.
+                        Poll::Ready(Some(Ok(Err(e)))) => {
+                            this.task_did_err = true;
+                            return Poll::Ready(Some(Err(e)));
+                        }
+
+                        // Task was aborted.
+                        Poll::Ready(Some(Err(e))) => {
+                            this.task_did_err = true;
+                            return Poll::Ready(Some(Err(DataFusionError::Execution(format!(
+                                "task aborted: {e}"
+                            )))));
+                        }
+
+                        // All tasks completed. Stream might still have more to
+                        // do, so continue.
+                        Poll::Ready(None) => {
+                            this.tasks_all_completed = true;
+                            continue;
+                        }
+
+                        Poll::Pending => return Poll::Pending,
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl RecordBatchStream for SendRecvJoinStream {
+    fn schema(&self) -> Arc<Schema> {
+        self.stream.schema()
     }
 }
