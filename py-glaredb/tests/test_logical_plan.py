ccb914fdf844ec3317c81dfb3449b963813e37bc
Adhish Singla <adhisingla94@gmail.com>
2023-09-08 02:13:20 +0530

    fix: pydict order issue in python test
    Fixes #1704

--- py-glaredb/tests/test_logical_plan.py
+++ py-glaredb/tests/test_logical_plan.py
@@ -1,6 +1,11 @@
 import pytest
 import glaredb
 
+def pydict_sort(pd):
+    res = dict()
+    for key in sorted(pd):
+        res[key] = sorted(pd[key])
+    return res
 
 def test_compose():
     con = glaredb.connect()
@@ -9,10 +14,11 @@ def test_compose():
     con.execute("insert into tbl values (3, 4);")
     con.execute("insert into tbl values (5, 6);")
     intermediate = con.sql("select * from tbl where a > 2;")
-    out_1 = intermediate.to_arrow().to_pydict()
+    out_1 = pydict_sort(intermediate.to_arrow().to_pydict())
     expected = {"a": [3, 5], "b": [4, 6]}
     assert out_1 == expected
     out_2 = con.sql("select * from intermediate where b > 4;").to_arrow().to_pydict()
     expected = {"a": [5], "b": [6]}
     assert out_2 == expected
     con.close()
+    
