db1f7e627d2ebed1c639f813849058b4cdc04097
Adhish Singla <adhisingla94@gmail.com>
2023-09-12 22:51:46 +0530

    generate certs for tls setup

--- scripts/gen-certs.sh
+++ scripts/gen-certs.sh
@@ -10,3 +10,7 @@ openssl req -new -x509 -days 365 -nodes -text \
         -out server.crt \
         -keyout server.key \
         -subj "/CN=glaredb.com"
+
+openssl req -noenc -newkey rsa -keyout client.key -out client.csr -subj '/CN=glaredb.com' -addext subjectAltName=DNS:glaredb.com
+
+openssl x509 -req -in client.csr -CA server.crt -CAkey server.key -days 365 -out client.crt -copy_extensions copy
