1c000de48dcff2ebb9d7c3ee5f5cb5293677cd7c
Sean Smith <scsmithr@gmail.com>
2023-10-15 18:50:46 -0500

    feat: Scheduling

--- crates/sqlexec/src/session.rs
+++ crates/sqlexec/src/session.rs
@@ -5,12 +5,6 @@ use std::pin::Pin;
 use std::sync::Arc;
 use std::task::{Context, Poll};
 
-use crate::planner::physical_plan::{
-    get_count_from_batch, get_operation_from_batch, GENERIC_OPERATION_AND_COUNT_PHYSICAL_SCHEMA,
-    GENERIC_OPERATION_PHYSICAL_SCHEMA,
-};
-use crate::remote::client::RemoteClient;
-use crate::remote::planner::{DDLExtensionPlanner, RemotePhysicalPlanner};
 use catalog::session_catalog::{CatalogMutator, SessionCatalog};
 use datafusion::arrow::datatypes::Schema;
 use datafusion::arrow::record_batch::RecordBatch;
@@ -35,10 +29,18 @@ use telemetry::Tracker;
 use uuid::Uuid;
 
 use crate::context::local::{LocalSessionContext, Portal, PreparedStatement};
+use crate::distexec::scheduler::{OutputSink, Scheduler};
+use crate::distexec::stream::create_coalescing_adapter;
 use crate::environment::EnvironmentReader;
 use crate::errors::{ExecError, Result};
 use crate::parser::StatementWithExtensions;
 use crate::planner::logical_plan::*;
+use crate::planner::physical_plan::{
+    get_count_from_batch, get_operation_from_batch, GENERIC_OPERATION_AND_COUNT_PHYSICAL_SCHEMA,
+    GENERIC_OPERATION_PHYSICAL_SCHEMA,
+};
+use crate::remote::client::RemoteClient;
+use crate::remote::planner::{DDLExtensionPlanner, RemotePhysicalPlanner};
 
 static EMPTY_EXEC_PLAN: Lazy<Arc<dyn ExecutionPlan>> = Lazy::new(|| {
     Arc::new(EmptyExec::new(
@@ -347,6 +349,7 @@ impl Session {
         native_tables: NativeTableStorage,
         tracker: Arc<Tracker>,
         spill_path: Option<PathBuf>,
+        task_scheduler: Scheduler,
     ) -> Result<Session> {
         let metrics_handler = SessionMetricsHandler::new(
             vars.user_id(),
@@ -362,6 +365,7 @@ impl Session {
             native_tables,
             metrics_handler,
             spill_path,
+            task_scheduler,
         )?;
 
         Ok(Session { ctx })
@@ -414,12 +418,29 @@ impl Session {
     }
 
     /// Execute a datafusion physical plan.
-    pub fn execute_physical(
+    pub async fn execute_physical(
         &self,
         plan: Arc<dyn ExecutionPlan>,
     ) -> Result<SendableRecordBatchStream> {
         let context = self.ctx.task_context();
-        let stream = execute_stream(plan, context)?;
+
+        let stream = if self.ctx.get_session_vars().enable_experimental_scheduler() {
+            let scheduler = self.ctx.get_task_scheduler();
+            let (sink, stream) =
+                create_coalescing_adapter(plan.output_partitioning(), plan.schema());
+            let sink = Arc::new(sink);
+
+            let output = OutputSink {
+                batches: sink.clone(),
+                errors: sink,
+            };
+
+            scheduler.schedule(plan, context, output)?;
+            Box::pin(stream)
+        } else {
+            execute_stream(plan, context)?
+        };
+
         Ok(stream)
     }
 
@@ -484,7 +505,7 @@ impl Session {
             }
             LogicalPlan::Datafusion(plan) => {
                 let physical = self.create_physical_plan(plan).await?;
-                let stream = self.execute_physical(physical.clone())?;
+                let stream = self.execute_physical(physical.clone()).await?;
 
                 let stream = ExecutionResult::from_stream(stream).await;
 
