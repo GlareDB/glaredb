12423c81895158151585753257b78dd2ba9c7801
Sean Smith <scsmithr@gmail.com>
2023-10-30 18:04:05 -0500

    idk

--- crates/datasources/src/sqlserver/mod.rs
+++ crates/datasources/src/sqlserver/mod.rs
@@ -53,13 +53,45 @@ impl SqlServerAccess {
     }
 }
 
-struct SqlServerAccessState {
+struct ClientPool {
+    config: tiberius::Config,
+    clients: Mutex<Vec<WrappedClient>>,
+}
+
+impl ClientPool {
+    fn new(config: tiberius::Config) -> Self {
+        ClientPool {
+            config,
+            clients: Mutex::new(Vec::new()),
+        }
+    }
+
+    /// Put a client for reuse.
+    fn put_client(&self, wrapped: WrappedClient) {
+        let mut clients = self.clients.lock();
+        clients.push(wrapped);
+    }
+
+    /// Reuse or create a new client.
+    async fn reuse_or_connect(&self) -> Result<WrappedClient> {
+        let mut clients = self.clients.lock();
+        if let Some(client) = clients.pop() {
+            return Ok(client);
+        }
+        std::mem::drop(clients);
+
+        let client = WrappedClient::connect(self.config.clone()).await?;
+        Ok(client)
+    }
+}
+
+struct WrappedClient {
     /// SQL Server client. `Compat` is used for compatability between tokio's
     /// async read/write traits and futures' read/write traits.
     client: tiberius::Client<Compat<TcpStream>>,
 }
 
-impl SqlServerAccessState {
+impl WrappedClient {
     async fn connect(config: tiberius::Config) -> Result<Self> {
         let socket = TcpStream::connect(config.get_addr()).await?;
         socket.set_nodelay(true)?;
@@ -67,6 +99,7 @@ impl SqlServerAccessState {
         Ok(Self { client })
     }
 
+    /// Get the arrow schema for a table.
     async fn get_table_schema(&mut self, schema: &str, name: &str) -> Result<ArrowSchema> {
         let mut query = self
             .client
@@ -131,18 +164,21 @@ pub struct SqlServerTableProviderConfig {
 pub struct SqlServerTableProvider {
     schema: String,
     table: String,
-    state: Arc<Mutex<SqlServerAccessState>>,
+    clients: ClientPool,
     arrow_schema: ArrowSchemaRef,
 }
 
 impl SqlServerTableProvider {
     pub async fn try_new(conf: SqlServerTableProviderConfig) -> Result<Self> {
-        let mut state = SqlServerAccessState::connect(conf.access.config).await?;
-        let arrow_schema = state.get_table_schema(&conf.schema, &conf.table).await?;
+        let clients = ClientPool::new(conf.access.config);
+        let mut client = clients.reuse_or_connect().await?;
+        let arrow_schema = client.get_table_schema(&conf.schema, &conf.table).await?;
+        clients.put_client(client);
+
         Ok(Self {
             schema: conf.schema,
             table: conf.table,
-            state: Arc::new(Mutex::new(state)),
+            clients,
             arrow_schema: Arc::new(arrow_schema),
         })
     }
@@ -176,7 +212,39 @@ impl TableProvider for SqlServerTableProvider {
         filters: &[Expr],
         limit: Option<usize>,
     ) -> DatafusionResult<Arc<dyn ExecutionPlan>> {
+        // Project the schema.
+        let projected_schema = match projection {
+            Some(projection) => Arc::new(self.arrow_schema.project(projection)?),
+            None => self.arrow_schema.clone(),
+        };
+
+        // Get the projected columns, joined by a ','. This will be put in the
+        // 'SELECT ...' portion of the query.
+        let projection_string = projected_schema
+            .fields
+            .iter()
+            .map(|f| f.name().clone())
+            .collect::<Vec<_>>()
+            .join(",");
+
+        let limit_string = match limit {
+            Some(limit) => format!("LIMIT {}", limit),
+            None => String::new(),
+        };
+
+        // TODO: Where
+
+        let query = format!(
+            "SELECT {projection_string} FROM {}.{} {limit_string}",
+            self.schema, self.table
+        );
+
         unimplemented!()
+        // Ok(Arc::new(SqlServerExec {
+        //     query,
+        //     arrow_schema: projected_schema,
+        //     metrics: ExecutionPlanMetricsSet::new(),
+        // }))
     }
 
     async fn insert_into(
@@ -191,8 +259,12 @@ impl TableProvider for SqlServerTableProvider {
     }
 }
 
-#[derive(Debug)]
-struct SqlServerExec {}
+struct SqlServerExec {
+    query: String,
+    client: Mutex<Option<WrappedClient>>,
+    arrow_schema: ArrowSchemaRef,
+    metrics: ExecutionPlanMetricsSet,
+}
 
 impl ExecutionPlan for SqlServerExec {
     fn as_any(&self) -> &dyn Any {
@@ -200,7 +272,7 @@ impl ExecutionPlan for SqlServerExec {
     }
 
     fn schema(&self) -> ArrowSchemaRef {
-        unimplemented!()
+        self.arrow_schema.clone()
     }
 
     fn output_partitioning(&self) -> Partitioning {
@@ -229,6 +301,22 @@ impl ExecutionPlan for SqlServerExec {
         partition: usize,
         _context: Arc<TaskContext>,
     ) -> DatafusionResult<SendableRecordBatchStream> {
+        if partition != 0 {
+            return Err(DataFusionError::Execution(
+                "only single partition supported".to_string(),
+            ));
+        }
+
+        let mut client = self.client.lock();
+        let client = match client.take() {
+            Some(client) => client,
+            None => {
+                return Err(DataFusionError::Execution(
+                    "partition executed more than once".to_string(),
+                ))
+            }
+        };
+
         unimplemented!()
     }
 
@@ -237,12 +325,102 @@ impl ExecutionPlan for SqlServerExec {
     }
 
     fn metrics(&self) -> Option<MetricsSet> {
-        unimplemented!()
+        Some(self.metrics.clone_inner())
     }
 }
 
 impl DisplayAs for SqlServerExec {
     fn fmt_as(&self, _t: DisplayFormatType, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "SqlServerExec")
+    }
+}
+
+impl fmt::Debug for SqlServerExec {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SqlServerExec")
+            .field("query", &self.query)
+            .field("arrow_schema", &self.arrow_schema)
+            .finish_non_exhaustive()
+    }
+}
+
+/// Stream state.
+///
+/// Transitions:
+/// Open -> Opening
+/// Opening -> Scan
+/// Scan -> Done
+enum RowStreamState {
+    Open {
+        query: String,
+    },
+    Opening {
+        opening_fut: BoxFuture<'static, tiberius::Result<tiberius::QueryStream<'a>>>,
+    },
+    Scan {
+        stream: BoxStream<'static, tiberius::Result<tiberius::Row>>,
+    },
+    Done,
+}
+
+struct RowStream {
+    stream_state: RowStreamState<'_>,
+    arrow_schema: ArrowSchemaRef,
+}
+
+impl RowStream {
+    fn poll_next_inner(
+        &mut self,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<DatafusionResult<RecordBatch>>> {
+        loop {
+            match &mut self.stream_state {
+                RowStreamState::Open { client, query } => {
+                    let query = std::mem::take(query);
+                    let fut = client.client.simple_query(query);
+                    self.stream_state = RowStreamState::Opening {
+                        opening_fut: Box::pin(fut),
+                    };
+
+                    break;
+                }
+                _ => unimplemented!(),
+            }
+        }
+
+        unimplemented!()
+    }
+}
+
+impl Stream for RowStream {
+    type Item = DatafusionResult<RecordBatch>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let this = &mut *self;
+
+        unimplemented!()
+
+        // let state = this
+        //     .stream_state
+        //     .take()
+        //     .expect("state should always exist at beginning of poll");
+
+        // let state = match &state {
+        //     RowStreamState::Open { query } => {
+        //         let fut = this.client.client.simple_query(query);
+        //         RowStreamState::Opening {
+        //             opening_fut: fut.boxed(),
+        //         }
+        //     }
+        //     _ => unimplemented!(),
+        // };
+
+        // unimplemented!()
+    }
+}
+
+impl RecordBatchStream for RowStream {
+    fn schema(&self) -> ArrowSchemaRef {
+        self.arrow_schema.clone()
     }
 }
