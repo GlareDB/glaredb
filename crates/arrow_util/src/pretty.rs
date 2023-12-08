852161cefe1671d438e636486f468ef43c0bcc90
hemanth5566 <kotipallihemanth@gmail.com>
2023-12-08 05:35:04 +0530

    formatting for FixedSizeList

--- crates/arrow_util/src/pretty.rs
+++ crates/arrow_util/src/pretty.rs
@@ -285,6 +285,9 @@ pub fn fmt_dtype(dtype: &DataType) -> String {
         DataType::List(fld) | DataType::LargeList(fld) => {
             format!("List<{}>", fmt_dtype(fld.data_type()))
         }
+        DataType::FixedSizeList(fld,length) => {
+            format!("FixedSizeList<{}, Fixed_Length:{}>", fmt_dtype(fld.data_type()), length)
+        }
         DataType::Struct(flds) => flds
             .iter()
             .map(|f| format!("{}: {}", f.name(), fmt_dtype(f.data_type())))
