6255f3fd1fd9ef13d8b520af707e11f120635f1d
tycho garen <garen@tychoish.com>
2023-12-16 08:25:36 -0500

    intro test

--- tests/fixtures/glaredb.py
+++ tests/fixtures/glaredb.py
@@ -1,3 +1,9 @@
+import time
+import tempfile
+import subprocess
+import sys
+import os.path
+import pathlib
 import pytest
 import tests
 
@@ -10,3 +16,31 @@ def release():
 @pytest.fixture
 def debug():
     return tests.PKG_DIRECTORY.joinpath("target", "debug", "glaredb")
+
+
+@pytest.fixture
+def run_debug(debug: pathlib.Path) -> str:
+    addr = ("127.0.0.1", "5432")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with subprocess.Popen(
+            [
+                debug.absolute(),
+                "server",
+                "--data-dir",
+                os.path.join(tmpdir, "data"),
+                "--spill-path",
+                os.path.join(tmpdir, "spill"),
+                # "--mode",
+                # "json",
+                "--bind",
+                ":".join(addr),
+            ],
+            cwd=tmpdir,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            close_fds="posix" in sys.builtin_module_names,
+        ) as p:
+            time.sleep(0.5)
+            assert not p.poll(), p.stdout.read().decode("utf-8")
+            yield addr
+            p.terminate()
--- tests/tests/test_smoke.py
+++ tests/tests/test_smoke.py
@@ -1,9 +1,30 @@
-from fixtures.glaredb import release, debug
+import pathlib
+import subprocess
+import psycopg2
+
+from fixtures.glaredb import run_debug, release, debug
 
 
 def test_release_exists(release):
-    assert not release.exists()
+    assert not release.exists()  # the release binary does not exist
 
 
 def test_debug_exists(debug):
-    assert debug.exists()
+    assert debug.exists()  # the debug binary exists
+
+
+def test_debug_executes(debug: pathlib.Path):
+    # run the binary and see if it returns:
+    assert subprocess.check_call([debug.absolute(), "-q", "SELECT 1;"]) == 0
+
+
+def test_start(run_debug: tuple[str, str]):
+    assert run_debug[0] == "127.0.0.1"
+    assert run_debug[1] == "5432"
+
+    conn = psycopg2.connect(host=run_debug[0], port=run_debug[1], database="glaredb")
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT 1")
+
+    conn.close()
