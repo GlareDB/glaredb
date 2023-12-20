8fb2eb6ba3cff3488e61f725aeeff5ee3c8606eb
tycho garen <garen@tychoish.com>
2023-12-19 21:43:09 -0500

    pull out function

--- crates/sqlbuiltins/src/functions/table/bson.rs
+++ crates/sqlbuiltins/src/functions/table/bson.rs
@@ -16,6 +16,7 @@ use datafusion_ext::errors::ExtensionError;
 use datafusion_ext::functions::{FuncParamValue, TableFuncContextProvider};
 use datasources::bson::schema::{merge_schemas, schema_from_document};
 use datasources::bson::stream::BsonPartitionStream;
+use datasources::common::url::DatasourceUrl;
 use datasources::object_store::generic::GenericStoreAccess;
 use datasources::object_store::ObjStoreAccess;
 use protogen::metastore::types::catalog::RuntimePreference;
@@ -64,138 +65,147 @@ impl TableFunc for BsonScan {
         )
         .map_err(|e| ExtensionError::Arrow(e.into()))?;
 
-        let store = store_access
-            .create_store()
-            .map_err(|e| ExtensionError::Arrow(e.into()))?;
+        bson_streaming_table(store_access, sample_size as usize, source_url).await
+    }
+}
 
-        let path = source_url.path();
+pub async fn bson_streaming_table(
+    store_access: GenericStoreAccess,
+    schema_inference_sample_size: usize,
+    source_url: DatasourceUrl,
+) -> Result<Arc<dyn TableProvider>, ExtensionError> {
+    let store = store_access
+        .create_store()
+        .map_err(|e| ExtensionError::Arrow(e.into()))?;
 
-        // assume that the file type is a glob and see if there are
-        // more files...
-        let mut list = store_access
-            .list_globbed(&store, path.as_ref())
-            .await
-            .map_err(|e| ExtensionError::Arrow(e.into()))?;
+    let path = source_url.path();
 
-        if list.is_empty() {
-            return Err(ExtensionError::String(format!(
-                "no matching objects for '{path}'"
-            )));
-        }
+    // assume that the file type is a glob and see if there are
+    // more files...
+    let mut list = store_access
+        .list_globbed(&store, path.as_ref())
+        .await
+        .map_err(|e| ExtensionError::Arrow(e.into()))?;
 
-        // for consistent results, particularly for the sample, always
-        // sort by location
-        list.sort_by(|a, b| a.location.cmp(&b.location));
+    if list.is_empty() {
+        return Err(ExtensionError::String(format!(
+            "no matching objects for '{path}'"
+        )));
+    }
 
-        // build a vector of streams, one for each file, that handle BSON's framing.
-        let mut readers = VecDeque::with_capacity(list.len());
-        for obj in list {
-            readers.push_back(
-                // BSON is just length-prefixed byte sequences
-                LengthDelimitedCodec::builder()
-                    // set up the framing parameters, use a 16MB max-doc size,
-                    // which is the same as the MongoDB server, this is
-                    // arbitrary, and we could easily support larger documents,
-                    // but one has to draw the line somewhere, 16MB is (frankly)
-                    // absurdly large for a row size, and anything larger
-                    // wouldn't be round-trippable to MongoDB.
-                    .max_frame_length(16 * 1024 * 1024)
-                    .length_field_type::<u32>() // actually signed int32s
-                    .length_field_offset(0) // length field is first
-                    .length_adjustment(4) // length prefix includes
-                    // the prefix use the object_store buffered reader
-                    // to stream data from the object store:
-                    .new_read(object_store::buffered::BufReader::with_capacity(
-                        store.to_owned(),
-                        &obj,
-                        32 * 1024 * 1024, // 32 MB buffer, probably still too small
-                    ))
-                    // convert the chunk of bytes to bson.
-                    .map(
-                        // TODO: this probably wants to be a raw document
-                        // eventually, so we can put all the _fields_ in a map,
-                        // iterate over the document once, and check each bson
-                        // field name against the schema, and only pull out the
-                        // fields that match. This is easier in the short term
-                        // but less performant for large documents where the
-                        // docuemnts are a superset of the schema, we'll end up
-                        // doing much more parsing work than is actually needed
-                        // for the bson documents.
-                        |bt: Result<BytesMut, std::io::Error>| -> Result<Document, DataFusionError> {
-                            Ok(bson::de::from_slice::<Document>(
-                                bt?.freeze().as_bytes().to_owned().as_slice(),
-                            ).map_err(|e| DataFusionError::External(Box::new(e)))?
-                            .to_owned())
-                        }),
-            );
-        }
+    // for consistent results, particularly for the sample, always
+    // sort by location
+    list.sort_by(|a, b| a.location.cmp(&b.location));
 
-        // iterate through the readers and build up a sample of the first <n>
-        // documents to be used to infer the schema.
-        let mut sample = Vec::<Document>::with_capacity(sample_size as usize);
-        let mut first_active: usize = 0;
-        'readers: for reader in readers.iter_mut() {
-            while let Some(res) = reader.next().await {
-                match res {
-                    Ok(doc) => sample.push(doc),
-                    Err(e) => return Err(e.into()),
-                };
+    // build a vector of streams, one for each file, that handle BSON's framing.
+    let mut readers = VecDeque::with_capacity(list.len());
+    for obj in list {
+        readers.push_back(
+            // BSON is just length-prefixed byte sequences
+            LengthDelimitedCodec::builder()
+                // set up the framing parameters, use a 16MB max-doc size,
+                // which is the same as the MongoDB server, this is
+                // arbitrary, and we could easily support larger documents,
+                // but one has to draw the line somewhere, 16MB is (frankly)
+                // absurdly large for a row size, and anything larger
+                // wouldn't be round-trippable to MongoDB.
+                .max_frame_length(16 * 1024 * 1024)
+                .length_field_type::<u32>() // actually signed int32s
+                .length_field_offset(0) // length field is first
+                .length_adjustment(4) // length prefix includes
+                // the prefix use the object_store buffered reader
+                // to stream data from the object store:
+                .new_read(object_store::buffered::BufReader::with_capacity(
+                    store.to_owned(),
+                    &obj,
+                    32 * 1024 * 1024, // 32 MB buffer, probably still too small
+                ))
+                // convert the chunk of bytes to bson.
+                .map(
+                    // TODO: this probably wants to be a raw document
+                    // eventually, so we can put all the _fields_ in a map,
+                    // iterate over the document once, and check each bson
+                    // field name against the schema, and only pull out the
+                    // fields that match. This is easier in the short term
+                    // but less performant for large documents where the
+                    // docuemnts are a superset of the schema, we'll end up
+                    // doing much more parsing work than is actually needed
+                    // for the bson documents.
+                    |bt: Result<BytesMut, std::io::Error>| -> Result<Document, DataFusionError> {
+                        Ok(bson::de::from_slice::<Document>(
+                            bt?.freeze().as_bytes().to_owned().as_slice(),
+                        )
+                        .map_err(|e| DataFusionError::External(Box::new(e)))?
+                        .to_owned())
+                    },
+                ),
+        );
+    }
 
-                if sample.len() as i64 >= sample_size {
-                    break 'readers;
-                }
+    // iterate through the readers and build up a sample of the first <n>
+    // documents to be used to infer the schema.
+    let mut sample = Vec::<Document>::with_capacity(schema_inference_sample_size as usize);
+    let mut first_active: usize = 0;
+    'readers: for reader in readers.iter_mut() {
+        while let Some(res) = reader.next().await {
+            match res {
+                Ok(doc) => sample.push(doc),
+                Err(e) => return Err(e.into()),
+            };
+
+            if sample.len() >= schema_inference_sample_size {
+                break 'readers;
             }
-            first_active += 1;
         }
+        first_active += 1;
+    }
 
-        // if we had to read through one or more than of the input files in the
-        // glob, we already have their documents and should truncate the vector
-        // of readers.
-        for _ in 0..first_active {
-            readers.pop_front();
-        }
+    // if we had to read through one or more than of the input files in the
+    // glob, we already have their documents and should truncate the vector
+    // of readers.
+    for _ in 0..first_active {
+        readers.pop_front();
+    }
 
-        // infer the sechema; in the future we can allow users to specify the
-        // schema directly; in the future users could specify the schema (kind
-        // of as a base-level projection, but we'd need a schema specification
-        // language). Or have some other strategy for inference rather than
-        // every unique field from the first <n> documents.
-        let schema = Arc::new(
-            merge_schemas(sample.iter().map(|doc| schema_from_document(doc).unwrap()))
-                .map_err(|e| ExtensionError::String(e.to_string()))?,
-        );
+    // infer the sechema; in the future we can allow users to specify the
+    // schema directly; in the future users could specify the schema (kind
+    // of as a base-level projection, but we'd need a schema specification
+    // language). Or have some other strategy for inference rather than
+    // every unique field from the first <n> documents.
+    let schema = Arc::new(
+        merge_schemas(sample.iter().map(|doc| schema_from_document(doc).unwrap()))
+            .map_err(|e| ExtensionError::String(e.to_string()))?,
+    );
 
-        let mut streams = Vec::<std::sync::Arc<(dyn PartitionStream + 'static)>>::with_capacity(
-            readers.len() + 1,
-        );
+    let mut streams =
+        Vec::<std::sync::Arc<(dyn PartitionStream + 'static)>>::with_capacity(readers.len() + 1);
 
-        // all the documents we read for the sample are hanging around
-        // somewhere and we want to make sure that callers access
-        // them: we're going to make a special stream with these
-        // documents here.
+    // all the documents we read for the sample are hanging around
+    // somewhere and we want to make sure that callers access
+    // them: we're going to make a special stream with these
+    // documents here.
+    streams.push(Arc::new(BsonPartitionStream::new(
+        schema.clone(),
+        futures::stream::iter(
+            sample
+                .into_iter()
+                .map(|doc| -> Result<Document, DataFusionError> { Ok(doc) }),
+        )
+        .boxed(),
+    )));
+
+    // for all remaining streams we wrap the stream of documents
+    // and convert them into partition streams which the streaming
+    // table exec can read.
+    while let Some(reader) = readers.pop_front() {
         streams.push(Arc::new(BsonPartitionStream::new(
             schema.clone(),
-            futures::stream::iter(
-                sample
-                    .into_iter()
-                    .map(|doc| -> Result<Document, DataFusionError> { Ok(doc) }),
-            )
-            .boxed(),
+            reader.boxed(),
         )));
-
-        // for all remaining streams we wrap the stream of documents
-        // and convert them into partition streams which the streaming
-        // table exec can read.
-        while let Some(reader) = readers.pop_front() {
-            streams.push(Arc::new(BsonPartitionStream::new(
-                schema.clone(),
-                reader.boxed(),
-            )));
-        }
-
-        Ok(Arc::new(StreamingTable::try_new(
-            schema.clone(), // <= inferred schema
-            streams,        // <= vector of partition streams
-        )?))
     }
+
+    Ok(Arc::new(StreamingTable::try_new(
+        schema.clone(), // <= inferred schema
+        streams,        // <= vector of partition streams
+    )?))
 }
