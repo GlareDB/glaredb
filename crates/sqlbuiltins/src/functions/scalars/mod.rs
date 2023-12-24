94895c0e6cdb9bb815c86280ccfee9cd045a7a54
tycho garen <garen@tychoish.com>
2023-12-24 10:58:58 -0500

    interger upcast

--- crates/sqlbuiltins/src/functions/scalars/mod.rs
+++ crates/sqlbuiltins/src/functions/scalars/mod.rs
@@ -6,10 +6,12 @@ use std::sync::Arc;
 
 use datafusion::arrow::array::{make_array, Array, ArrayDataBuilder};
 use datafusion::arrow::datatypes::{DataType, Field};
+use datafusion::error::DataFusionError;
 use datafusion::logical_expr::BuiltinScalarFunction;
 use datafusion::logical_expr::{Expr, ScalarUDF, Signature, TypeSignature, Volatility};
 use datafusion::physical_plan::ColumnarValue;
 use datafusion::scalar::ScalarValue;
+use num_traits::ToPrimitive;
 
 use crate::document;
 use crate::errors::BuiltinError;
@@ -76,6 +78,83 @@ fn get_nth_scalar_value(
             }
         },
         None => Ok(ColumnarValue::Scalar(ScalarValue::Boolean(Some(true)))),
+    }
+}
+
+fn safe_up_cast_integer_scalar(
+    dt: DataType,
+    idx: usize,
+    value: i64,
+) -> Result<u64, DataFusionError> {
+    if value < 0 {
+        Err(datafusion::error::DataFusionError::Execution(
+            format!(
+                "expected {} value at {} to be greater than zero or unsigned",
+                dt, idx,
+            )
+            .to_string(),
+        ))
+    } else {
+        Ok(value as u64)
+    }
+}
+
+fn get_nth_scalar_as_u64(input: &[ColumnarValue], n: usize) -> Result<u64, DataFusionError> {
+    match input.get(n) {
+        Some(input) => match input {
+            ColumnarValue::Scalar(scalar) => match scalar.clone() {
+                ScalarValue::Int8(Some(value)) => {
+                    safe_up_cast_integer_scalar(scalar.data_type(), n, value as i64)
+                }
+                ScalarValue::Int16(Some(value)) => {
+                    safe_up_cast_integer_scalar(scalar.data_type(), n, value as i64)
+                }
+                ScalarValue::Int32(Some(value)) => {
+                    safe_up_cast_integer_scalar(scalar.data_type(), n, value as i64)
+                }
+                ScalarValue::Int64(Some(value)) => {
+                    safe_up_cast_integer_scalar(scalar.data_type(), n, value)
+                }
+                ScalarValue::UInt8(Some(value)) => Ok(value as u64),
+                ScalarValue::UInt16(Some(value)) => Ok(value as u64),
+                ScalarValue::UInt32(Some(value)) => Ok(value as u64),
+                ScalarValue::Float64(Some(value)) => {
+                    if value.trunc() != value {
+                        Err(datafusion::error::DataFusionError::Execution(
+                            format!("float value {} at index {}, expected integer", value, n)
+                                .to_string(),
+                        ))
+                    } else {
+                        Ok(value.to_i64().unwrap() as u64)
+                    }
+                }
+                ScalarValue::Float32(Some(value)) => {
+                    if value.trunc() != value {
+                        Err(datafusion::error::DataFusionError::Execution(
+                            format!("float value {} at index {}, expected integer", value, n)
+                                .to_string(),
+                        ))
+                    } else {
+                        Ok(value.to_i64().unwrap() as u64)
+                    }
+                }
+                ScalarValue::UInt64(Some(value)) => Ok(value),
+                _ => Err(datafusion::error::DataFusionError::Execution(
+                    format!(
+                        "value in index {} was {}, expected integer",
+                        n,
+                        scalar.data_type()
+                    )
+                    .to_string(),
+                )),
+            },
+            ColumnarValue::Array(_) => Err(datafusion::error::DataFusionError::Execution(
+                format!("invalid array value in index {}, expected integer", n).to_string(),
+            )),
+        },
+        None => Err(datafusion::error::DataFusionError::Execution(
+            format!("expected integer value in index {}", n).to_string(),
+        )),
     }
 }
 
