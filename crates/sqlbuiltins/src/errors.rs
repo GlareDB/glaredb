bd3da32198de0dd5163797ff1a2d9d9bce6580ae
tycho garen <garen@tychoish.com>
2023-12-24 10:49:52 -0500

    fix: scalar udf function parsing

--- crates/sqlbuiltins/src/errors.rs
+++ crates/sqlbuiltins/src/errors.rs
@@ -1,7 +1,15 @@
+use datafusion::arrow::error::ArrowError;
+
 #[derive(Debug, thiserror::Error)]
 pub enum BuiltinError {
     #[error(transparent)]
     DatafusionExtError(#[from] datafusion_ext::errors::ExtensionError),
+
+    #[error(transparent)]
+    DatafusionError(#[from] datafusion::error::DataFusionError),
+
+    #[error(transparent)]
+    ArrowError(#[from] ArrowError),
 }
 
 pub type Result<T, E = BuiltinError> = std::result::Result<T, E>;
--- crates/sqlbuiltins/src/functions/scalars/mod.rs
+++ crates/sqlbuiltins/src/functions/scalars/mod.rs
@@ -1,24 +1,23 @@
 pub mod df_scalars;
 pub mod kdl;
 pub mod postgres;
-use crate::{
-    document,
-    functions::{BuiltinFunction, BuiltinScalarUDF, ConstBuiltinFunction},
-};
-use datafusion::logical_expr::BuiltinScalarFunction;
-
-use protogen::metastore::types::catalog::FunctionType;
 
 use std::sync::Arc;
 
-use datafusion::{
-    arrow::datatypes::{DataType, Field},
-    logical_expr::{Expr, ScalarUDF, Signature, TypeSignature, Volatility},
-    physical_plan::ColumnarValue,
-    scalar::ScalarValue,
-};
+use datafusion::arrow::array::{make_array, Array, ArrayDataBuilder};
+use datafusion::arrow::datatypes::{DataType, Field};
+use datafusion::logical_expr::BuiltinScalarFunction;
+use datafusion::logical_expr::{Expr, ScalarUDF, Signature, TypeSignature, Volatility};
+use datafusion::physical_plan::ColumnarValue;
+use datafusion::scalar::ScalarValue;
+
+use crate::document;
+use crate::errors::BuiltinError;
+use crate::functions::{BuiltinFunction, BuiltinScalarUDF, ConstBuiltinFunction};
+use protogen::metastore::types::catalog::FunctionType;
 
 pub struct ConnectionId;
+
 impl ConstBuiltinFunction for ConnectionId {
     const NAME: &'static str = "connection_id";
     const DESCRIPTION: &'static str = "Returns the connection id of the current session";
@@ -34,7 +33,9 @@ impl BuiltinScalarUDF for ConnectionId {
         session_var("connection_id")
     }
 }
+
 pub struct Version;
+
 impl ConstBuiltinFunction for Version {
     const NAME: &'static str = "version";
     const DESCRIPTION: &'static str = "Returns the version of the database";
@@ -51,13 +52,30 @@ impl BuiltinScalarUDF for Version {
     }
 }
 
-fn get_nth_scalar_value(input: &[ColumnarValue], n: usize) -> Option<ScalarValue> {
+fn get_nth_scalar_value(
+    input: &[ColumnarValue],
+    n: usize,
+    op: &dyn Fn(Option<ScalarValue>) -> Result<ScalarValue, BuiltinError>,
+    output_type: DataType,
+) -> Result<ColumnarValue, BuiltinError> {
     match input.get(n) {
         Some(input) => match input {
-            ColumnarValue::Scalar(scalar) => Some(scalar.clone()),
-            ColumnarValue::Array(arr) => ScalarValue::try_from_array(arr, 0).ok(),
+            ColumnarValue::Scalar(scalar) => Ok(ColumnarValue::Scalar(op(Some(scalar.clone()))?)),
+            ColumnarValue::Array(arr) => {
+                let mut builder = ArrayDataBuilder::new(output_type);
+
+                for idx in 0..arr.len() {
+                    builder.add_child_data(
+                        op(Some(ScalarValue::try_from_array(arr, idx)?))?
+                            .to_array()
+                            .into_data(),
+                    );
+                }
+
+                Ok(ColumnarValue::Array(make_array(builder.build()?)))
+            }
         },
-        None => None,
+        None => Ok(ColumnarValue::Scalar(ScalarValue::Boolean(Some(true)))),
     }
 }
 
