3ce73d8853db2289637c7c07b23b91f7672fafbb
tycho garen <garen@tychoish.com>
2023-12-24 20:37:09 -0500

    backport

--- crates/sqlbuiltins/src/functions/scalars/mod.rs
+++ crates/sqlbuiltins/src/functions/scalars/mod.rs
@@ -57,18 +57,16 @@ impl BuiltinScalarUDF for Version {
 fn get_nth_scalar_value(
     input: &[ColumnarValue],
     n: usize,
-    op: &dyn Fn(Option<ScalarValue>) -> Result<ScalarValue, BuiltinError>,
+    op: &dyn Fn(ScalarValue) -> Result<ScalarValue, BuiltinError>,
 ) -> Result<ColumnarValue, BuiltinError> {
     match input.get(n) {
         Some(input) => match input {
-            ColumnarValue::Scalar(scalar) => Ok(ColumnarValue::Scalar(op(Some(scalar.clone()))?)),
+            ColumnarValue::Scalar(scalar) => Ok(ColumnarValue::Scalar(op(scalar.clone())?)),
             ColumnarValue::Array(arr) => {
                 let mut values = Vec::with_capacity(arr.len());
 
                 for idx in 0..arr.len() {
-                    let value = ScalarValue::try_from_array(arr, idx)?;
-                    let value = op(Some(value))?;
-                    values.push(value);
+                    values.push(op(ScalarValue::try_from_array(arr, idx)?)?);
                 }
 
                 Ok(ColumnarValue::Array(ScalarValue::iter_to_array(
@@ -80,90 +78,65 @@ fn get_nth_scalar_value(
     }
 }
 
-fn safe_up_cast_integer_scalar(
-    dt: DataType,
-    idx: usize,
-    value: i64,
-) -> Result<u64, DataFusionError> {
+fn try_from_u64_scalar(scalar: ScalarValue) -> Result<u64, BuiltinError> {
+    match scalar {
+        ScalarValue::Int8(Some(value)) => safe_up_cast_integer_scalar(value as i64),
+        ScalarValue::Int16(Some(value)) => safe_up_cast_integer_scalar(value as i64),
+        ScalarValue::Int32(Some(value)) => safe_up_cast_integer_scalar(value as i64),
+        ScalarValue::Int64(Some(value)) => safe_up_cast_integer_scalar(value),
+        ScalarValue::UInt8(Some(value)) => Ok(value as u64),
+        ScalarValue::UInt16(Some(value)) => Ok(value as u64),
+        ScalarValue::UInt32(Some(value)) => Ok(value as u64),
+        ScalarValue::Float64(Some(value)) => {
+            if value.trunc() != value {
+                return Err(BuiltinError::ParseError(
+                    format!("expected whole value for float {}", value).to_string(),
+                ));
+            }
+            Ok(value.to_i64().ok_or(BuiltinError::IncorrectType(
+                scalar.data_type(),
+                DataType::UInt64,
+            ))? as u64)
+        }
+        ScalarValue::Float32(Some(value)) => {
+            if value.trunc() != value {
+                return Err(BuiltinError::InvalidValue(
+                    format!("expected whole value for float {}", value).to_string(),
+                ));
+            }
+            Ok(value.to_i64().ok_or(BuiltinError::IncorrectType(
+                scalar.data_type(),
+                DataType::UInt64,
+            ))? as u64)
+        }
+        ScalarValue::UInt64(Some(value)) => Ok(value),
+        _ => {
+            return Err(BuiltinError::IncorrectType(
+                scalar.data_type(),
+                DataType::UInt64,
+            ))
+        }
+    }
+}
+
+fn safe_up_cast_integer_scalar(value: i64) -> Result<u64, BuiltinError> {
     if value < 0 {
-        Err(datafusion::error::DataFusionError::Execution(
-            format!(
-                "expected {} value at {} to be greater than zero or unsigned",
-                dt, idx,
-            )
-            .to_string(),
+        Err(BuiltinError::ParseError(
+            format!("{} cannot be a uint64", value).to_string(),
         ))
     } else {
         Ok(value as u64)
     }
 }
 
-#[allow(dead_code)] // will get removed before this hits mainline; stacked commit issue
-fn get_nth_scalar_as_u64(
-    input: &[ColumnarValue],
-    n: usize,
-    op: &dyn Fn(Option<u64>) -> Result<ScalarValue, BuiltinError>,
-) -> Result<ColumnarValue, BuiltinError> {
-    get_nth_scalar_value(input, n, &|scalar| -> Result<ScalarValue, BuiltinError> {
-        let scalar = match scalar.clone() {
-            Some(v) => v.to_owned(),
-            None => return Err(BuiltinError::MissingValueAtIndex(n)),
-        };
-
-        let value = match scalar {
-            ScalarValue::Int8(Some(value)) => {
-                safe_up_cast_integer_scalar(scalar.data_type(), n, value as i64)
-            }
-            ScalarValue::Int16(Some(value)) => {
-                safe_up_cast_integer_scalar(scalar.data_type(), n, value as i64)
-            }
-            ScalarValue::Int32(Some(value)) => {
-                safe_up_cast_integer_scalar(scalar.data_type(), n, value as i64)
-            }
-            ScalarValue::Int64(Some(value)) => {
-                safe_up_cast_integer_scalar(scalar.data_type(), n, value)
-            }
-            ScalarValue::UInt8(Some(value)) => Ok(value as u64),
-            ScalarValue::UInt16(Some(value)) => Ok(value as u64),
-            ScalarValue::UInt32(Some(value)) => Ok(value as u64),
-            ScalarValue::Float64(Some(value)) => {
-                if value.trunc() != value {
-                    return Err(BuiltinError::InvalidValueAtIndex(
-                        n,
-                        format!("expected whole value for float {}", value).to_string(),
-                    ));
-                }
-                Ok(value.to_i64().ok_or(BuiltinError::IncorrectTypeAtIndex(
-                    n,
-                    scalar.data_type(),
-                    DataType::UInt64,
-                ))? as u64)
-            }
-            ScalarValue::Float32(Some(value)) => {
-                if value.trunc() != value {
-                    return Err(BuiltinError::InvalidValueAtIndex(
-                        n,
-                        format!("expected whole value for float {}", value).to_string(),
-                    ));
-                }
-                Ok(value.to_i64().ok_or(BuiltinError::IncorrectTypeAtIndex(
-                    n,
-                    scalar.data_type(),
-                    DataType::UInt64,
-                ))? as u64)
-            }
-            ScalarValue::UInt64(Some(value)) => Ok(value),
-            _ => {
-                return Err(BuiltinError::IncorrectTypeAtIndex(
-                    n,
-                    scalar.data_type(),
-                    DataType::UInt64,
-                ))
-            }
-        }?;
-
-        op(Some(value))
-    })
+// get_nth_64_fn_arg extracts a string value (or tries to) from a
+// function argument; columns are always an error.
+fn get_nth_u64_fn_arg(input: &[ColumnarValue], idx: usize) -> Result<u64, BuiltinError> {
+    match input.get(idx) {
+        Some(ColumnarValue::Scalar(value)) => try_from_u64_scalar(value.to_owned()),
+        Some(ColumnarValue::Array(_)) => Err(BuiltinError::InvalidColumnarValue(idx)),
+        None => Err(BuiltinError::MissingValueAtIndex(idx)),
+    }
 }
 
 // get_nth_string_fn_arg extracts a string value (or tries to) from a
@@ -174,8 +147,7 @@ fn get_nth_string_fn_arg(input: &[ColumnarValue], idx: usize) -> Result<String, 
             ColumnarValue::Scalar(ScalarValue::Utf8(Some(v)))
             | ColumnarValue::Scalar(ScalarValue::LargeUtf8(Some(v))) => Ok(v.to_owned()),
             ColumnarValue::Array(_) => Err(BuiltinError::InvalidColumnarValue(idx)),
-            _ => Err(BuiltinError::IncorrectTypeAtIndex(
-                idx,
+            _ => Err(BuiltinError::IncorrectType(
                 input.data_type(),
                 DataType::Utf8,
             )),
@@ -190,18 +162,12 @@ fn get_nth_string_fn_arg(input: &[ColumnarValue], idx: usize) -> Result<String, 
 fn get_nth_string_value(
     input: &[ColumnarValue],
     n: usize,
-    op: &dyn Fn(Option<String>) -> Result<ScalarValue, BuiltinError>,
+    op: &dyn Fn(String) -> Result<ScalarValue, BuiltinError>,
 ) -> Result<ColumnarValue, BuiltinError> {
     get_nth_scalar_value(input, n, &|scalar| -> Result<ScalarValue, BuiltinError> {
-        let scalar = match scalar.clone() {
-            Some(v) => v.to_owned(),
-            None => return Err(BuiltinError::MissingValueAtIndex(n)),
-        };
-
         match scalar {
-            ScalarValue::Utf8(v) | ScalarValue::LargeUtf8(v) => op(v),
-            _ => Err(BuiltinError::IncorrectTypeAtIndex(
-                n,
+            ScalarValue::Utf8(Some(v)) | ScalarValue::LargeUtf8(Some(v)) => op(v),
+            _ => Err(BuiltinError::IncorrectType(
                 scalar.data_type(),
                 DataType::Utf8,
             )),
