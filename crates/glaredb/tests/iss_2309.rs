ec43187028c6c96f205a1b052354d6a4a324ed83
universalmind303 <cory.grinstead@gmail.com>
2023-12-27 10:36:53 -0600

    add integration test for iss #2309

---
+++ crates/glaredb/tests/iss_2309.rs
@@ -0,0 +1,39 @@
+mod setup;
+
+use setup::DEFAULT_TIMEOUT;
+
+use crate::setup::make_cli;
+
+#[test]
+fn test_special_characters() {
+    let mut cmd = make_cli();
+
+    let assert = cmd
+        .timeout(DEFAULT_TIMEOUT)
+        .args(&["-q", r#"select ';'"#])
+        .assert();
+    assert.success().stdout(predicates::str::contains(
+        r#"
+┌───────────┐
+│ Utf8(";") │
+│ ──        │
+│ Utf8      │
+╞═══════════╡
+│ ;         │
+└───────────┘"#
+            .trim_start(),
+    ));
+}
+
+#[test]
+fn test_special_characters_2() {
+    let mut cmd = make_cli();
+
+    let assert = cmd
+        .timeout(DEFAULT_TIMEOUT)
+        .args(&["-q", r#"select ";""#])
+        .assert();
+    assert.failure().stderr(predicates::str::contains(
+        "Schema error: No field named \";\".",
+    ));
+}
