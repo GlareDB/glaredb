29b0aa3eb9b4df427f22a881618a5556aab472bc
universalmind303 <cory.grinstead@gmail.com>
2024-01-24 14:07:39 -0600

    fix clickhouse & bigquery

--- crates/datasources/src/clickhouse/mod.rs
+++ crates/datasources/src/clickhouse/mod.rs
@@ -331,12 +331,16 @@ impl TableProvider for ClickhouseTableProvider {
 
         // Get the projected columns, joined by a ','. This will be put in the
         // 'SELECT ...' portion of the query.
-        let projection_string = projected_schema
-            .fields
-            .iter()
-            .map(|f| f.name().clone())
-            .collect::<Vec<_>>()
-            .join(",");
+        let projection_string = if projected_schema.fields().is_empty() {
+            "*".to_string()
+        } else {
+            projected_schema
+                .fields
+                .iter()
+                .map(|f| f.name().clone())
+                .collect::<Vec<_>>()
+                .join(",")
+        };
 
         let mut query = format!("SELECT {} FROM {}", projection_string, self.table_ref);
 
