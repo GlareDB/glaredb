877b845535a0187b1cb6cc9c65c9be477249d5ea
universalmind303 <cory.grinstead@gmail.com>
2024-01-24 15:12:30 -0600

    whoops

--- read_bson.py
+++
@@ -1,7 +0,0 @@
-import bson
-
-with open("/Users/corygrinstead/Development/glaredb/copy_output.bson", "rb") as f:
-    for idx, doc in enumerate(bson.decode_file_iter(f)):
-        print(idx, doc)
-        assert len(doc) == 1
-        assert "amount" in doc
