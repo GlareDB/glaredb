328fb54bcc7c4a9bbdf6f51b83c30b22797b984d
tycho garen <garen@tychoish.com>
2024-02-07 11:09:45 -0500

    tychoish hacking

--- tests/tests/test_dbt.py
+++ tests/tests/test_dbt.py
@@ -2,6 +2,7 @@ import psycopg2.extensions
 import pytest
 import os
 
+import tools
 from tests.fixtures.glaredb import glaredb_connection, debug_path
 
 from dbt.cli.main import dbtRunner, dbtRunnerResult
@@ -12,18 +13,16 @@ def test_dbt_glaredb(
 ):
     dbt: dbtRunner = dbtRunner()
 
-    os.environ["DBT_USER"] = glaredb_connection.info.user
-
-    model_name: str = "glaredb_model" # TODO
-    project_directory: str = "../fixtures/dbt_project/" # TODO
-    dbt_profiles_directory: str = "../fixtures/dbt_project/" # TODO
+    model_name: str = "glaredb_model"  # TODO
+    project_directory: str = "../fixtures/dbt_project/"  # TODO
+    dbt_profiles_directory: str = "../fixtures/dbt_project/"  # TODO
 
     with glaredb_connection.cursor() as curr:
         curr.execute("create table dbt_test (amount int)")
         curr.execute(
             "INSERT INTO dbt_test (amount) VALUES (0), (1), (2), (3), (4), (5), (6), (7), (8), (9)"
         )
-        
+
     cli_args: list = [
         "run",
         "--project-dir",
@@ -31,16 +30,21 @@ def test_dbt_glaredb(
         "--profiles-dir",
         dbt_profiles_directory,
         "-m",
-        model_name
+        model_name,
     ]
-    #
-    res: dbtRunnerResult = dbt.invoke(cli_args)
 
-    # The below will currently fail. res contains the error message, but that message can be in different places based
-    # on where the failure is. Currently, it is under the top level `res.exception` key.
-    # assert res.success is True
-    #
-    # with glaredb_connection.cursor() as curr:
-    #     query_result: list = curr.execute(f"select * from {model_name}").fetchall()
-    #
-    # assert len(query_result) == 10
+    with tools.env("DBT_USER", glaredb_connection.info.user):
+        res: dbtRunnerResult = dbt.invoke(cli_args)
+        assert res is not None
+
+    with glaredb_connection.cursor() as curr:
+        # TODO: this shouldn't throw
+        with pytest.raises(psycopg2.errors.InternalError):
+            query_result: list = curr.execute(f"select * from {model_name}").fetchall()
+
+            # The below will currently fail. res contains the error
+            # message, but that message can be in different places
+            # based on where the failure is. Currently, it is under
+            # the top level `res.exception` key.  assert res.success
+            # is True
+            assert len(query_result) == 10
--- tests/tests/test_json.py
+++ tests/tests/test_json.py
@@ -8,6 +8,7 @@ import pytest
 
 from fixtures.glaredb import glaredb_connection, debug_path
 
+
 @pytest.fixture
 def beatle_mock_data():
     beatles = ["john", "paul", "george", "ringo"]
@@ -21,7 +22,7 @@ def beatle_mock_data():
             "case": i + 1,
             "rand": random.random(),
         }
-        
+
         if beatle_id == 0:
             obj["house"] = "the dakota"
         if beatle_id == 1:
@@ -39,27 +40,25 @@ def beatle_mock_data():
 def test_read_json_array(
     glaredb_connection: psycopg2.extensions.connection,
     tmp_path_factory: pytest.TempPathFactory,
-    beatle_mock_data: list[dict], 
+    beatle_mock_data: list[dict],
 ):
     tmp_dir = tmp_path_factory.mktemp(basename="read-json-array-", numbered=True)
     data_path = tmp_dir.joinpath("beatles.100.json")
 
     with open(data_path, "w") as f:
         json.dump(beatle_mock_data, f, indent="  ")
-            
+
     with glaredb_connection.cursor() as curr:
         curr.execute(f"select count(*) from read_json('{data_path}');")
         r = curr.fetchone()
         assert r[0] == 100
 
-    with glaredb_connection.cursor(
-            cursor_factory=psycopg2.extras.RealDictCursor
-    ) as curr:
+    with glaredb_connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as curr:
         curr.execute(f"select * from read_json('{data_path}');")
         rows = curr.fetchall()
         assert len(rows) == 100
         for row in rows:
-            assert len(row) == 8 # superset schema
+            assert len(row) == 8  # superset schema
             assert "house" in row
             assert "beatle_name" in row
             if row["beatle_name"] == "john":
@@ -72,7 +71,7 @@ def test_read_json_array(
 def test_read_json_glob(
     glaredb_connection: psycopg2.extensions.connection,
     tmp_path_factory: pytest.TempPathFactory,
-    beatle_mock_data: list[dict], 
+    beatle_mock_data: list[dict],
 ):
     beatles = ["john", "paul", "george", "ringo"]
 
@@ -81,20 +80,18 @@ def test_read_json_glob(
     for idx, doc in enumerate(beatle_mock_data):
         with open(tmp_dir.joinpath(f"file-{idx}.json"), "w") as f:
             json.dump(doc, f, indent="  ")
-            
+
     with glaredb_connection.cursor() as curr:
         curr.execute(f"select count(*) from read_json('{tmp_dir}/*.json')")
         r = curr.fetchone()
         assert r[0] == 100
 
-    with glaredb_connection.cursor(
-            cursor_factory=psycopg2.extras.RealDictCursor
-    ) as curr:
+    with glaredb_connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as curr:
         curr.execute(f"select * from read_json('{data_path}');")
         rows = curr.fetchall()
         assert len(rows) == 100
         for row in rows:
-            assert len(row) == 8 # superset schema
+            assert len(row) == 8  # superset schema
             assert "house" in row
             assert "beatle_name" in row
             if row["beatle_name"] == "john":
