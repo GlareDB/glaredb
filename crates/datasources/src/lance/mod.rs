8dcde36cf341d3a3e1f3daebdc15010e3e126062
tycho garen <garen@tychoish.com>
2024-02-08 22:29:32 -0500

    duplicate git diff

--- crates/datasources/src/lance/mod.rs
+++ crates/datasources/src/lance/mod.rs
@@ -1,5 +1,5 @@
 use std::any::Any;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use async_trait::async_trait;
 use datafusion::arrow::array::RecordBatch;
@@ -9,10 +9,20 @@ use datafusion::arrow::record_batch::RecordBatchIterator;
 use datafusion::datasource::TableProvider;
 use datafusion::error::{DataFusionError, Result};
 use datafusion::execution::context::SessionState;
+use datafusion::execution::TaskContext;
 use datafusion::logical_expr::{Expr, TableType};
+use datafusion::physical_plan::expressions::PhysicalSortExpr;
+use datafusion::physical_plan::metrics::{ExecutionPlanMetricsSet, MetricsSet};
 use datafusion::physical_plan::stream::RecordBatchStreamAdapter;
-use datafusion::physical_plan::{execute_stream, ExecutionPlan};
-use datafusion_ext::stream::StreamExecPlan;
+use datafusion::physical_plan::{
+    execute_stream,
+    DisplayAs,
+    DisplayFormatType,
+    ExecutionPlan,
+    Partitioning,
+    SendableRecordBatchStream,
+    Statistics,
+};
 use futures::StreamExt;
 use lance::dataset::builder::DatasetBuilder;
 use lance::dataset::{WriteMode, WriteParams};
@@ -34,10 +44,6 @@ impl LanceTable {
                 .await?,
         })
     }
-
-    fn as_table_provider(&self) -> impl TableProvider {
-        self.dataset.clone()
-    }
 }
 
 #[async_trait]
@@ -47,7 +53,7 @@ impl TableProvider for LanceTable {
     }
 
     fn schema(&self) -> SchemaRef {
-        self.as_table_provider().schema()
+        TableProvider::schema(&self.dataset)
     }
 
     fn table_type(&self) -> TableType {
@@ -61,47 +67,123 @@ impl TableProvider for LanceTable {
         filter: &[Expr],
         limit: Option<usize>,
     ) -> Result<Arc<dyn ExecutionPlan>> {
-        Ok(self
-            .as_table_provider()
-            .scan(state, projection, filter, limit)
-            .await?)
+        Ok(TableProvider::scan(&self.dataset, state, projection, filter, limit).await?)
     }
 
     async fn insert_into(
         &self,
-        state: &SessionState,
+        _state: &SessionState,
         input: Arc<dyn ExecutionPlan>,
         _overwrite: bool,
     ) -> Result<Arc<dyn ExecutionPlan>> {
-        let mut stream = execute_stream(input, state.task_ctx())?.chunks(32);
+        Ok(Arc::new(LanceInsertExecPlan::new(
+            self.dataset.clone(),
+            input,
+        )))
+    }
+}
+
+struct LanceInsertExecPlan {
+    dataset: Dataset,
+    input: Arc<dyn ExecutionPlan>,
+    metrics: ExecutionPlanMetricsSet,
+}
+
+impl LanceInsertExecPlan {
+    fn new(dataset: Dataset, input: Arc<dyn ExecutionPlan>) -> Self {
+        Self {
+            dataset,
+            input,
+            metrics: ExecutionPlanMetricsSet::new(),
+        }
+    }
+}
+
+impl DisplayAs for LanceInsertExecPlan {
+    fn fmt_as(&self, _t: DisplayFormatType, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "LanceInsertExecPlan")
+    }
+}
+
+impl std::fmt::Debug for LanceInsertExecPlan {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "LanceInsertExecPlan: {:?}", self.schema())
+    }
+}
+
+impl ExecutionPlan for LanceInsertExecPlan {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn schema(&self) -> SchemaRef {
+        TableProvider::schema(&self.dataset)
+    }
+
+    fn output_partitioning(&self) -> Partitioning {
+        Partitioning::UnknownPartitioning(1)
+    }
+
+    fn output_ordering(&self) -> Option<&[PhysicalSortExpr]> {
+        None
+    }
+
+    fn children(&self) -> Vec<Arc<dyn ExecutionPlan>> {
+        Vec::new()
+    }
+
+    fn with_new_children(
+        self: Arc<Self>,
+        children: Vec<Arc<dyn ExecutionPlan>>,
+    ) -> datafusion::error::Result<Arc<dyn ExecutionPlan>> {
+        if children.is_empty() {
+            Ok(self)
+        } else {
+            Err(DataFusionError::Execution(
+                "cannot replace children for LanceInsertExecPlan".to_string(),
+            ))
+        }
+    }
+
+    fn execute(
+        &self,
+        _partition: usize,
+        ctx: Arc<TaskContext>,
+    ) -> datafusion::error::Result<SendableRecordBatchStream> {
+        let mut stream = execute_stream(self.input.clone(), ctx)?.chunks(32);
         let mut ds = self.dataset.clone();
         let schema = self.schema().clone();
 
-        Ok(Arc::new(StreamExecPlan::new(
+        Ok(Box::pin(RecordBatchStreamAdapter::new(
             schema.clone(),
-            Mutex::new(Some(Box::pin(RecordBatchStreamAdapter::new(
-                schema.clone(),
-                futures::stream::once(async move {
-                    let write_opts = WriteParams {
-                        mode: WriteMode::Append,
-                        ..Default::default()
-                    };
+            futures::stream::once(async move {
+                let write_opts = WriteParams {
+                    mode: WriteMode::Append,
+                    ..Default::default()
+                };
 
-                    let mut count: u64 = 0;
-                    while let Some(batches) = stream.next().await {
-                        let start = ds.count_rows().await?;
-                        let rbi = RecordBatchIterator::new(
-                            batches
-                                .into_iter()
-                                .map(|v| v.map_err(|dfe| ArrowError::ExternalError(Box::new(dfe)))),
-                            schema.clone(),
-                        );
-                        ds.append(rbi, Some(write_opts.clone())).await?;
-                        count += (ds.count_rows().await? - start) as u64;
-                    }
-                    Ok::<RecordBatch, DataFusionError>(create_count_record_batch(count))
-                }),
-            )))),
+                let mut count: u64 = 0;
+                while let Some(batches) = stream.next().await {
+                    let start = ds.count_rows().await?;
+                    let rbi = RecordBatchIterator::new(
+                        batches
+                            .into_iter()
+                            .map(|v| v.map_err(|dfe| ArrowError::ExternalError(Box::new(dfe)))),
+                        schema.clone(),
+                    );
+                    ds.append(rbi, Some(write_opts.clone())).await?;
+                    count += (ds.count_rows().await? - start) as u64;
+                }
+                Ok::<RecordBatch, DataFusionError>(create_count_record_batch(count))
+            }),
         )))
+    }
+
+    fn statistics(&self) -> datafusion::error::Result<Statistics> {
+        Ok(Statistics::new_unknown(self.schema().as_ref()))
+    }
+
+    fn metrics(&self) -> Option<MetricsSet> {
+        Some(self.metrics.clone_inner())
     }
 }
