0eabfadbdae5b43c67a255bc6acfd88b2ccfdb6c
Sam Kleinman <sam@glaredb.com>
2024-02-27 16:32:18 -0500

    chore: make bson copy to options more consistent (#2704)

--- crates/protogen/src/metastore/types/options.rs
+++ crates/protogen/src/metastore/types/options.rs
@@ -1714,7 +1714,7 @@ pub enum CopyToFormatOptions {
     Parquet(CopyToFormatOptionsParquet),
     Lance(CopyToFormatOptionsLance),
     Json(CopyToFormatOptionsJson),
-    Bson,
+    Bson(CopyToFormatOptionsBson),
 }
 
 impl Default for CopyToFormatOptions {
@@ -1738,7 +1738,7 @@ impl CopyToFormatOptions {
             Self::Csv(_) => Self::CSV,
             Self::Parquet(_) => Self::PARQUET,
             Self::Json(_) => Self::JSON,
-            Self::Bson => Self::BSON,
+            Self::Bson(_) => Self::BSON,
             Self::Lance(_) => Self::LANCE,
         }
     }
@@ -1759,6 +1759,10 @@ pub struct CopyToFormatOptionsParquet {
 pub struct CopyToFormatOptionsJson {
     pub array: bool,
 }
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct CopyToFormatOptionsBson {}
+
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub struct CopyToFormatOptionsLance {
