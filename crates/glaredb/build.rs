c3e310ee80640e87364889169e971a6c10f675db
universalmind303 <cory.grinstead@gmail.com>
2024-02-28 19:58:40 -0600

    remove unused code

--- crates/glaredb/build.rs
+++ crates/glaredb/build.rs
@@ -1,11 +1,3 @@
 fn main() {
-    println!("cargo:include=../extensions/glaredb_distance");
-    println!("cargo:rustc-link-lib=dylib=glaredb_distance");
-    #[cfg(debug_assertions)]
-    println!("cargo:rustc-link-search=native=../glaredb_distance/target/debug");
-
-    #[cfg(not(debug_assertions))]
-    println!("cargo:rustc-link-search=native=../glaredb_distance/target/release");
-
     built::write_built_file().expect("Failed to acquire build-time information");
 }
