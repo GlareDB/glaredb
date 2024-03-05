9523d1fe2100d4bad3eb5a1b21039bbd55addc94
Sean Smith <scsmithr@gmail.com>
2024-03-04 19:15:13 -0600

    probably correct

--- crates/rayexec_execution/src/physical/plans/hash_join.rs
+++ crates/rayexec_execution/src/physical/plans/hash_join.rs
@@ -1,57 +1,132 @@
 use crate::planner::explainable::{ExplainConfig, ExplainEntry, Explainable};
 use crate::planner::operator::JoinType;
-use crate::types::batch::{DataBatch, DataBatchSchema};
-
-use arrow_array::{BooleanArray, RecordBatch, UInt32Array};
-use arrow_schema::Schema;
+use crate::types::batch::DataBatch;
+use arrow_array::{BooleanArray, UInt32Array};
 use hashbrown::raw::RawTable;
 use parking_lot::Mutex;
 use rayexec_error::{RayexecError, Result};
 use smallvec::{smallvec, SmallVec};
+use std::collections::BTreeMap;
 use std::collections::VecDeque;
 use std::fmt;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 use std::task::{Context, Poll, Waker};
 
-use super::util::hash::build_hashes;
-use super::Sink;
+use super::{Sink, Source};
 
 /// Hash join for for joining two tables on column equalities.
-///
-/// An optional filter provides outputing batches based on an arbitrary
-/// expression.
+// TODO: Probably need to add a filter since we'll need to be able to handle
+// arbitrary join conditions and left/right/anti joins will need to know which
+// rows have been visited based on the join condition.
 #[derive(Debug)]
 pub struct PhysicalHashJoin {
-    schema: DataBatchSchema,
+    /// Columns on the left side to join on.
+    left_on: Vec<usize>,
+
+    /// Columns on the right side to join on.
+    right_on: Vec<usize>,
+
     join_type: JoinType,
-    // (left, right) indices pairs.
-    join_on: Vec<(usize, usize)>,
+
+    /// Shared states across the sinks and source for all partitions.
+    states: Arc<Vec<Mutex<LocalState>>>,
+
+    /// The configured build sink. Expected to be taken during pipeline
+    /// building.
+    build_sink: Option<PhysicalHashJoinBuildSink>,
+
+    /// The configured probe sink. Expected to be taken during pipeline
+    /// building.
+    probe_sink: Option<PhysicalHashJoinProbeSink>,
 }
 
-// impl Source2 for PhysicalHashJoin {
-//     fn output_partitions(&self) -> usize {
-//         self.buffer.output_partitions()
-//     }
+impl PhysicalHashJoin {
+    pub fn try_new(
+        left_on: Vec<usize>,
+        right_on: Vec<usize>,
+        partitions: usize,
+        join_type: JoinType,
+    ) -> Result<Self> {
+        if left_on.len() != right_on.len() {
+            return Err(RayexecError::new(
+                "Left and right column indexes must be the same length",
+            ));
+        }
 
-//     fn poll_partition(
-//         &self,
-//         cx: &mut Context<'_>,
-//         partition: usize,
-//     ) -> Poll<Option<Result<DataBatch>>> {
-//         self.buffer.poll_partition(cx, partition)
-//     }
-// }
+        let states: Vec<_> = (0..)
+            .map(|_| {
+                Mutex::new(LocalState {
+                    build_finished: false,
+                    probe_finished: false,
+                    left_state: HashState::Local {
+                        state: LocalHashState {
+                            table: RawTable::new(),
+                            batches: Vec::new(),
+                        },
+                    },
+                    pending_push: None,
+                    pending_pull: None,
+                    computed: VecDeque::new(),
+                })
+            })
+            .collect();
 
-// impl Sink2 for PhysicalHashJoin {
-//     fn push(&self, input: DataBatch, child: usize, partition: usize) -> Result<()> {
-//         unimplemented!()
-//     }
+        let states = Arc::new(states);
+        let build_sink = PhysicalHashJoinBuildSink {
+            states: states.clone(),
+            remaining: partitions.into(),
+            hash_cols: left_on.clone(),
+        };
+        let probe_sink = PhysicalHashJoinProbeSink {
+            states: states.clone(),
+            hash_cols: right_on.clone(),
+            join_type,
+        };
 
-//     fn finish(&self, child: usize, partition: usize) -> Result<()> {
-//         unimplemented!()
-//     }
-// }
+        Ok(PhysicalHashJoin {
+            left_on,
+            right_on,
+            join_type,
+            states,
+            build_sink: Some(build_sink),
+            probe_sink: Some(probe_sink),
+        })
+    }
+
+    pub fn take_build_sink(&mut self) -> Option<PhysicalHashJoinBuildSink> {
+        self.build_sink.take()
+    }
+
+    pub fn take_probe_sink(&mut self) -> Option<PhysicalHashJoinProbeSink> {
+        self.probe_sink.take()
+    }
+}
+
+impl Source for PhysicalHashJoin {
+    fn output_partitions(&self) -> usize {
+        self.states.len()
+    }
+
+    fn poll_next(&self, cx: &mut Context, partition: usize) -> Poll<Option<Result<DataBatch>>> {
+        let mut state = self.states[partition].lock();
+        if !state.build_finished {
+            state.pending_pull = Some(cx.waker().clone());
+            return Poll::Pending;
+        }
+
+        if let Some(batch) = state.computed.pop_front() {
+            return Poll::Ready(Some(Ok(batch)));
+        }
+
+        if state.probe_finished {
+            return Poll::Ready(None);
+        }
+
+        state.pending_pull = Some(cx.waker().clone());
+        Poll::Pending
+    }
+}
 
 impl Explainable for PhysicalHashJoin {
     fn explain_entry(&self, _conf: ExplainConfig) -> ExplainEntry {
@@ -214,7 +289,7 @@ impl Sink for PhysicalHashJoinBuildSink {
         self.states.len()
     }
 
-    fn poll_ready(&self, cx: &mut Context, partition: usize) -> Poll<()> {
+    fn poll_ready(&self, _cx: &mut Context, _partition: usize) -> Poll<()> {
         // Always need to collect build side.
         Poll::Ready(())
     }
@@ -275,116 +350,243 @@ impl Explainable for PhysicalHashJoinBuildSink {
     }
 }
 
-struct ProbeStage {
-    batches: Vec<HashedBatch>,
+#[derive(Debug)]
+pub struct PhysicalHashJoinProbeSink {
+    /// Partition-local states.
+    states: Arc<Vec<Mutex<LocalState>>>,
+
+    /// Column indexes that have been hashed.
+    hash_cols: Vec<usize>,
+
+    join_type: JoinType,
 }
 
-impl ProbeStage {
-    fn probe(
-        &self,
-        probe: &RecordBatch,
-        key_cols: &[usize],
-        join_type: JoinType,
-        output_schema: Arc<Schema>,
-    ) -> Result<Vec<RecordBatch>> {
-        let probe_arrs: Vec<_> = key_cols
-            .iter()
-            .map(|idx| probe.columns().get(*idx).expect("valid column indexes"))
-            .collect();
+impl Sink for PhysicalHashJoinProbeSink {
+    fn input_partitions(&self) -> usize {
+        self.states.len()
+    }
 
-        let mut hashes = vec![0; probe.num_rows()]; // TODO: Don't allocate every time.
-        build_hashes(&probe_arrs, &mut hashes)?;
+    fn poll_ready(&self, cx: &mut Context, partition: usize) -> Poll<()> {
+        let mut state = self.states[partition].lock();
+        if !state.build_finished {
+            // We're still building, register for a wakeup.
+            state.pending_push = Some(cx.waker().clone());
+            return Poll::Pending;
+        }
+        assert!(!state.probe_finished);
+        Poll::Ready(())
+    }
 
-        let mut output_batches: Vec<RecordBatch> = Vec::with_capacity(self.batches.len());
+    fn push(&self, input: DataBatch, partition: usize) -> Result<()> {
+        let local_states = {
+            let state = self.states[partition].lock();
+            assert!(state.build_finished);
+            state.left_state.get_global_states()?.clone()
+        };
 
-        for batch in self.batches.iter() {
-            // Check equality based on hashes.
-            let mut partition_build_indexes: Vec<u32> = Vec::new();
-            let mut partition_probe_indexes: Vec<u32> = Vec::new();
+        let outputs = probe(
+            &local_states,
+            partition,
+            &input,
+            &self.hash_cols,
+            self.join_type,
+        )?;
 
-            for (probe_row_idx, probe_row_hash) in hashes.iter().enumerate() {
-                let entry = batch
-                    .table
-                    .get(*probe_row_hash, |(hash, _)| probe_row_hash == hash);
-                if let Some((_, indexes)) = entry {
-                    for build_index in indexes {
-                        partition_build_indexes.push(*build_index as u32);
-                        partition_probe_indexes.push(probe_row_idx as u32);
-                    }
-                }
-            }
+        let mut state = self.states[partition].lock();
+        state.computed.extend(outputs);
 
-            let mut partition_build_indexes = UInt32Array::from(partition_build_indexes);
-            let mut partition_probe_indexes = UInt32Array::from(partition_probe_indexes);
-
-            // Check equality based on key values.
-            let build_arrs: Vec<_> = key_cols
-                .iter()
-                .map(|idx| {
-                    batch
-                        .batch
-                        .columns()
-                        .get(*idx)
-                        .expect("valid column indexes")
-                })
-                .collect();
-
-            let mut equal = BooleanArray::from(vec![true; partition_probe_indexes.len()]);
-            let iter = build_arrs.iter().zip(probe_arrs.iter());
-            equal = iter
-                .map(|(build, probe)| {
-                    let build_take = arrow::compute::take(&build, &partition_build_indexes, None)?;
-                    let probe_take = arrow::compute::take(&probe, &partition_probe_indexes, None)?;
-                    // TODO: null == null
-                    arrow_ord::cmp::eq(&build_take, &probe_take)
-                })
-                .try_fold(equal, |acc, result| arrow::compute::and(&acc, &result?))?;
-
-            partition_build_indexes = arrow_array::cast::downcast_array(&arrow::compute::filter(
-                &partition_build_indexes,
-                &equal,
-            )?);
-            partition_probe_indexes = arrow_array::cast::downcast_array(&arrow::compute::filter(
-                &partition_probe_indexes,
-                &equal,
-            )?);
-
-            // Adjust indexes as needed depending on the join type.
-            // TODO: Handle everything else.
-            match join_type {
-                JoinType::Inner => {
-                    // No adjustments needed.
-                }
-                _ => unimplemented!(),
-            }
-
-            // Build final record batch.
-            let mut cols = Vec::with_capacity(output_schema.fields.len());
-            for build_col in batch.batch.columns() {
-                cols.push(arrow::compute::take(
-                    build_col,
-                    &partition_build_indexes,
-                    None,
-                )?);
-            }
-            for probe_col in probe.columns() {
-                cols.push(arrow::compute::take(
-                    probe_col,
-                    &partition_probe_indexes,
-                    None,
-                )?)
-            }
-
-            let output = RecordBatch::try_new(output_schema.clone(), cols)?;
-            output_batches.push(output);
+        if let Some(waker) = state.pending_pull.take() {
+            waker.wake();
         }
 
-        Ok(output_batches)
+        Ok(())
+    }
+
+    fn finish(&self, partition: usize) -> Result<()> {
+        let mut state = self.states[partition].lock();
+        assert!(state.build_finished);
+        assert!(!state.probe_finished);
+        state.probe_finished = true;
+
+        if let Some(waker) = state.pending_pull.take() {
+            waker.wake();
+        }
+
+        Ok(())
     }
 }
 
-/// A record batch along with key hashes.
-struct HashedBatch {
-    table: RawTable<(u64, SmallVec<[usize; 2]>)>,
-    batch: RecordBatch,
+impl Explainable for PhysicalHashJoinProbeSink {
+    fn explain_entry(&self, _conf: ExplainConfig) -> ExplainEntry {
+        ExplainEntry::new("PhsysicalHashJoinProbeSink")
+    }
+}
+
+/// Probe the left side of the join with the "right" input batch.
+///
+/// This assumes that the "right" batch was created for the given partition
+/// using the same hash repartitioning schema as the build side.
+fn probe(
+    left: &[LocalHashState],
+    partition: usize,
+    right: &DataBatch,
+    join_cols: &[usize],
+    join_type: JoinType,
+) -> Result<Vec<DataBatch>> {
+    // Assumes the right side has already been hashed.
+    let right_hash = right.get_column_hash().ok_or_else(|| {
+        RayexecError::new("Expected columns in right side of join to already have been hashed")
+    })?;
+    if !right_hash.is_for_columns(join_cols) {
+        return Err(RayexecError::new(
+            "Expected columns in right side of join to already have been hashed",
+        ));
+    }
+
+    let mut outputs = Vec::new();
+
+    // Get the partition state that corresponds to these hashes.
+    let state = left.get(partition).expect("left partition to exist");
+
+    // Indexes into the left and right batches. Note that this stored in a map
+    // since one hash value may correspond to rows in different batches on the
+    // left side. The key just indicates which of those batches contains the
+    // match, and the value is the indexes of rows in that batch.
+    //
+    // TODO: It might make sense to just concat the batches on the left side
+    // into a single large batch. _Or_ adjust row key to be a single usize and
+    // account for preceding batch lengths.
+    let mut partition_build_indexes: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
+    let mut partition_probe_indexes: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
+
+    for (probe_row_idx, probe_row_hash) in right_hash.hashes.iter().enumerate() {
+        let entry = state
+            .table
+            .get(*probe_row_hash, |(hash, _)| probe_row_hash == hash);
+        if let Some((_, indexes)) = entry {
+            for row_key in indexes {
+                use std::collections::btree_map::Entry;
+
+                match partition_build_indexes.entry(row_key.batch_idx) {
+                    Entry::Vacant(ent) => {
+                        ent.insert(vec![row_key.row_idx]);
+
+                        let existing =
+                            partition_probe_indexes.insert(row_key.batch_idx, vec![probe_row_idx]);
+                        assert!(
+                            existing.is_none(),
+                            "build indexes out of sync, previous value exists"
+                        );
+                    }
+                    Entry::Occupied(mut ent) => {
+                        let build_rows = ent.get_mut();
+                        build_rows.push(row_key.row_idx);
+
+                        let probe_rows = partition_probe_indexes
+                            .get_mut(&row_key.batch_idx)
+                            .expect("probe row indexes should already exist for this batch");
+                        probe_rows.push(probe_row_idx);
+                    }
+                }
+            }
+        }
+    }
+
+    for ((build_idx1, build), (build_idx2, probe)) in partition_build_indexes
+        .into_iter()
+        .zip(partition_probe_indexes.into_iter())
+    {
+        assert_eq!(build_idx1, build_idx2);
+        let build_batch = state.batches.get(build_idx1).expect("build batch to exist");
+
+        let mut partition_build_indexes =
+            UInt32Array::from_iter(build.into_iter().map(|idx| idx as u32));
+        let mut partition_probe_indexes =
+            UInt32Array::from_iter(probe.into_iter().map(|idx| idx as u32));
+
+        // Check equality based on key values.
+        let build_arrs: Vec<_> = join_cols
+            .iter()
+            .map(|idx| {
+                build_batch
+                    .column(*idx)
+                    .expect("column in build batch to exist")
+            })
+            .collect();
+        let probe_arrs: Vec<_> = join_cols
+            .iter()
+            .map(|idx| right.column(*idx).expect("column in probe batch to exist"))
+            .collect();
+
+        let mut equal = BooleanArray::from(vec![true; partition_probe_indexes.len()]);
+        let iter = build_arrs.iter().zip(probe_arrs.iter());
+        equal = iter
+            .map(|(build, probe)| {
+                let build_take = arrow::compute::take(&build, &partition_build_indexes, None)?;
+                let probe_take = arrow::compute::take(&probe, &partition_probe_indexes, None)?;
+                // TODO: null == null
+                arrow_ord::cmp::eq(&build_take, &probe_take)
+            })
+            .try_fold(equal, |acc, result| arrow::compute::and(&acc, &result?))?;
+
+        partition_build_indexes = arrow_array::cast::downcast_array(&arrow::compute::filter(
+            &partition_build_indexes,
+            &equal,
+        )?);
+        partition_probe_indexes = arrow_array::cast::downcast_array(&arrow::compute::filter(
+            &partition_probe_indexes,
+            &equal,
+        )?);
+
+        // Adjust indexes as needed depending on the join type.
+        // TODO: Handle everything else.
+        //
+        // Left:
+        // - Include every unvisited row in left batch, join with right nulls.
+        // - Partition local bitmap to track unvisited left batchs.
+        // - Flush out unvisited batches on finish.
+        //
+        // Right:
+        // - Include every unvisited row in right batch, join with left nulls.
+        // - Nothing else.
+        //
+        // Outer:
+        // - Include every unvisited row in right batch, join with left nulls.
+        // - Include every unvisited row in left batch, join with right nulls,
+        // - Partition local bitmap to track unvisited left batchs.
+        // - Flush out unvisited batches on finish.
+        //
+        // Left/right semi:
+        // - Just include left/right columns.
+        //
+        // Left/right anti:
+        // - Inverse of left/right
+        match join_type {
+            JoinType::Inner => {
+                // No adjustments needed.
+            }
+            _ => unimplemented!(),
+        }
+
+        let mut cols = Vec::new();
+        for build_col in build_batch.columns() {
+            cols.push(arrow::compute::take(
+                build_col,
+                &partition_build_indexes,
+                None,
+            )?);
+        }
+        for probe_col in right.columns() {
+            cols.push(arrow::compute::take(
+                probe_col,
+                &partition_probe_indexes,
+                None,
+            )?)
+        }
+
+        let batch = DataBatch::try_new(cols)?;
+        outputs.push(batch);
+    }
+
+    Ok(outputs)
 }
