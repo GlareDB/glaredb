310aef19e359b3a6bfcee58a0564c1ba1020f5b2
Tal Gluck <talagluck@gmail.com>
2024-03-06 15:54:08 +0100

    add great expectations test

---
+++ tests/test_great_expectations.py
@@ -0,0 +1,41 @@
+import great_expectations as gx
+import psycopg2.extensions
+import psycopg2.extras
+import pytest
+
+from tests.fixtures.glaredb import glaredb_connection
+
+@pytest.mark.xfail
+def test_great_expectations(
+    glaredb_connection: psycopg2.extensions.connection,
+    tmp_path_factory: pytest.TempPathFactory,
+):
+    curr = glaredb_connection.cursor()
+
+    curr.execute("create table my_data (amount int)")
+    curr.execute(
+        "INSERT INTO my_data (amount) VALUES (0), (1), (2), (3), (4), (5), (6), (7), (8), (9)"
+    )
+    curr.execute("select count(*) from my_data;")
+    res = curr.fetchone()
+
+
+    context = gx.get_context()  # gets a great expectations project context
+    gx_data_source = context.sources.add_postgres(
+        name="glaredb",
+        connection_string="postgresql://test:test@localhost:5433/db",
+    )
+
+    gx_data_asset = gx_data_source.add_table_asset("my_data")
+
+    batch_request = gx_data_asset.build_batch_request()
+
+    batch_list = gx_data_asset.get_batch_list_from_batch_request(batch_request=batch_request)
+
+    validator = context.get_validator(batch_list=batch_list)
+
+    assert validator.expect_column_values_to_not_be_null("amount")
+
+
+
+
