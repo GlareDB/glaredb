9dcda746b8771a3849a1d932676a2421e2a4c112
Tal Gluck <talagluck@gmail.com>
2024-03-06 12:24:30 +0100

    chore: add dbt read_csv test (#2732)

--- tests/test_dbt.py
+++ tests/test_dbt.py
@@ -106,3 +106,41 @@ def test_dbt_glaredb_external_postgres(
         result: list = curr.fetchone()[0]
 
     assert result == 5
+
+def test_dbt_join_csv_with_table(
+    glaredb_connection: psycopg2.extensions.connection,
+    dbt_project_path: pathlib.Path,
+):
+    model_name = "join_csv_with_table"
+
+    with glaredb_connection.cursor() as curr:
+        curr.execute("create table dbt_test (amount int)")
+        curr.execute(
+            "INSERT INTO dbt_test (amount) VALUES (0), (1), (2), (3), (4), (5), (6), (7), (8), (9)"
+        )
+        curr.execute("SELECT * FROM public.dbt_test")
+        a = curr.fetchall()
+
+    with (
+        tests.tools.env("GLAREDB_PORT", str(glaredb_connection.info.port)),
+        tests.tools.env("DBT_USER", glaredb_connection.info.user),
+    ):
+        res: dbtRunnerResult = dbtRunner().invoke(
+            [
+                "run",
+                "--project-dir",
+                dbt_project_path,
+                "--profiles-dir",
+                dbt_project_path,
+                "-m",
+                model_name,
+            ]
+        )
+
+        assert res.success is True
+
+    with glaredb_connection.cursor() as curr:
+        curr.execute(f"select count(*) from {model_name}")
+        result: list = curr.fetchone()[0]
+
+    assert result == 9
