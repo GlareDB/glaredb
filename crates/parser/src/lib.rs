4cfdb7e8553ba48043d664c71cabc73f6d969055
universalmind303 <cory.grinstead@gmail.com>
2024-03-06 12:16:52 -0600

    refactor: move parser into own crate (#2747)
    some prereq work for https://github.com/GlareDB/glaredb/issues/2744

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod errors;
 pub mod options;
 
 use std::collections::{BTreeMap, VecDeque};
@@ -12,8 +13,8 @@ use datafusion_ext::vars::Dialect;
 use prql_compiler::sql::Dialect as PrqlDialect;
 use prql_compiler::{compile, Options, Target};
 
-use self::options::{OptionValue, StmtOptions};
-use crate::errors::Result;
+use self::options::{OptionValue, StatementOptions};
+use crate::errors::{ParseError, Result};
 
 /// Wrapper around our custom parse for parsing a sql statement.
 pub fn parse_sql(sql: &str) -> Result<VecDeque<StatementWithExtensions>> {
@@ -42,7 +43,7 @@ pub struct CreateExternalTableStmt {
     /// Credentials to use for configuration.
     pub credentials: Option<Ident>,
     /// Datasource specific options.
-    pub options: StmtOptions,
+    pub options: StatementOptions,
 }
 
 impl fmt::Display for CreateExternalTableStmt {
@@ -82,7 +83,7 @@ pub struct CreateExternalDatabaseStmt {
     /// Credentials to use for configuration.
     pub credentials: Option<Ident>,
     /// Datasource specific options.
-    pub options: StmtOptions,
+    pub options: StatementOptions,
 }
 
 impl fmt::Display for CreateExternalDatabaseStmt {
@@ -194,7 +195,7 @@ pub struct CreateTunnelStmt {
     /// The tunnel type the connection is for.
     pub tunnel: Ident,
     /// Tunnel specific options.
-    pub options: StmtOptions,
+    pub options: StatementOptions,
 }
 
 impl fmt::Display for CreateTunnelStmt {
@@ -270,7 +271,7 @@ pub struct CreateCredentialsStmt {
     /// The credentials provider.
     pub provider: Ident,
     /// Credentials specific options.
-    pub options: StmtOptions,
+    pub options: StatementOptions,
     /// Optional comment (what the credentials are for).
     pub comment: String,
     /// replace if it exists
@@ -312,7 +313,7 @@ pub struct CreateCredentialStmt {
     /// The credentials provider.
     pub provider: Ident,
     /// Credentials specific options.
-    pub options: StmtOptions,
+    pub options: StatementOptions,
     /// Optional comment (what the credentials are for).
     pub comment: String,
     /// replace if it exists
@@ -385,7 +386,7 @@ pub struct CopyToStmt {
     /// Optional credentials (for cloud storage).
     pub credentials: Option<Ident>,
     /// COPY TO specific options.
-    pub options: StmtOptions,
+    pub options: StatementOptions,
 }
 
 impl fmt::Display for CopyToStmt {
@@ -824,7 +825,7 @@ impl<'a> CustomParser<'a> {
     }
 
     /// Parse options block.
-    fn parse_options(&mut self) -> Result<StmtOptions, ParserError> {
+    fn parse_options(&mut self) -> Result<StatementOptions, ParserError> {
         let has_options_keyword = self.consume_token(&Token::make_keyword("OPTIONS"));
         let has_block_opening = self.parser.consume_token(&Token::LParen);
 
@@ -833,7 +834,7 @@ impl<'a> CustomParser<'a> {
         };
 
         if !has_options_keyword && !has_block_opening {
-            return Ok(StmtOptions::new(BTreeMap::new()));
+            return Ok(StatementOptions::new(BTreeMap::new()));
         };
 
         if has_options_keyword && !has_block_opening {
@@ -879,7 +880,7 @@ impl<'a> CustomParser<'a> {
             }
         }
 
-        Ok(StmtOptions::new(options))
+        Ok(StatementOptions::new(options))
     }
 
     fn parse_options_value(&mut self) -> Result<OptionValue, ParserError> {
@@ -1084,13 +1085,26 @@ impl<'a> CustomParser<'a> {
     }
 }
 
-pub fn validate_ident(ident: &ast::Ident) -> Result<(), ParserError> {
-    sqlbuiltins::validation::validate_object_name(&ident.value)
-        .map_err(|e| ParserError::ParserError(e.to_string()))
+pub fn validate_ident(ident: &ast::Ident) -> Result<()> {
+    pg_validate_object_name(&ident.value)
+}
+
+/// Validate identifiers as per [postgres identifier
+/// syntax](https://www.postgresql.org/docs/11/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS)
+pub fn pg_validate_object_name(name: &str) -> Result<()> {
+    const POSTGRES_IDENT_MAX_LENGTH: usize = 63;
+    if name.len() > POSTGRES_IDENT_MAX_LENGTH {
+        return Err(ParseError::InvalidNameLength {
+            length: name.len(),
+            max: POSTGRES_IDENT_MAX_LENGTH,
+        });
+    }
+
+    Ok(())
 }
 
 /// Validate object names a `Vec<ast::Idents>`
-pub fn validate_object_name(name: &ast::ObjectName) -> Result<(), ParserError> {
+pub fn validate_object_name(name: &ast::ObjectName) -> Result<()> {
     for ident in name.0.iter() {
         validate_ident(ident)?;
     }
@@ -1123,7 +1137,7 @@ mod tests {
             datasource: Ident::new("postgres"),
             tunnel: None,
             credentials: None,
-            options: StmtOptions::new(options),
+            options: StatementOptions::new(options),
         };
         println!("{:?}", stmt);
 
@@ -1162,7 +1176,7 @@ mod tests {
             datasource: Ident::new("postgres"),
             tunnel: None,
             credentials: None,
-            options: StmtOptions::new(options),
+            options: StatementOptions::new(options),
         };
 
         assert_eq!(
@@ -1485,7 +1499,7 @@ mod tests {
                 parser: Parser::new(&d).with_tokens(t),
             };
             let opts = p.parse_options().unwrap();
-            let expected_opts = StmtOptions::new(map);
+            let expected_opts = StatementOptions::new(map);
             assert_eq!(opts, expected_opts);
         }
     }
