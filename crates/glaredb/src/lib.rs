2a815089b9c12d406ed66b33d0e4d9b826ff4067
Sam Kleinman <sam@glaredb.com>
2024-04-11 13:00:34 -0400

    feat: documentation for Rust SDK crate (#2871)
    Co-authored-by: Grey <grey@glaredb.com>

--- crates/glaredb/src/lib.rs
+++ crates/glaredb/src/lib.rs
@@ -1,3 +1,14 @@
+//! GlareDB is a database engine designed to provide the user
+//! experience and ergonomic embedded databases with the compute power
+//! and flexibility of large scale distributed serverless compute engines.
+//!
+//! The GlareDB Rust SDK is a set of high-level wrappers for a GlareDB
+//! instance as either a client or an embedded database. The
+//! implementation primarily underpins the implementations of the
+//! Python and Node.js bindings, but may be used/useful directly for
+//! testing GlareDB from within Rust tests, and even inside of Rust
+//! applications or to produce other bindings.
+
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::path::PathBuf;
@@ -25,31 +36,74 @@ use sqlexec::session::ExecutionResult;
 use sqlexec::OperationInfo;
 use url::Url;
 
-
+/// ConnectOptions are the set of options to configure a GlareDB
+/// instance, and are an analogue to the commandline arguments to
+/// produce a "running database". The ConnectOptionsBuilder provides a
+/// chainable interface to construct these values and produce a
+/// structure. You can construct `ConnectOptions` fully without the
+/// builder interface, depending on your preference.
+///
+/// The `.connect()` method on `ConnectionOptions` is the main way to
+/// produce a GlareDB instance. All instances use an in-process
+/// metastore (catalog).
+///
+/// The `connection_target`, `location`, `spill_path` and
+/// `storage_options` all control the behavior of a local, single node
+/// instance, while the remaining options configure a GlareDB instance
+/// for hybrid-execution.
 #[derive(Default, Builder)]
 pub struct ConnectOptions {
+    /// The connection_target specifies where the GlareDB instances
+    /// storage is. This is either, in memory (unspecified or
+    /// `memory://`), a path to something on the local file-system for
+    /// persistent local databases, or an object-store URL for
+    /// databases that are backed up onto cloud storage.
     #[builder(setter(into))]
     pub connection_target: Option<String>,
+    /// Location is that path **within** the `connection_target` where
+    /// the database's files will be stored. This is required for all
+    /// object-store backed GlareDB instances and ignored in all other
+    /// cases.
     #[builder(setter(into))]
     pub location: Option<String>,
+    /// Specifies the location on the local file system where this
+    /// process will write files so that it can spill data for
+    /// operations to local disk (sorts, large joins, etc.)
     #[builder(setter(into))]
     pub spill_path: Option<String>,
+    /// Defines the options used to configure the object store
+    /// (credentials, etc.)
     #[builder(setter(into), default = "HashMap::new()")]
     pub storage_options: HashMap<String, String>,
 
+    /// By default, the client will connect to the GlareDB service
+    /// using TLS. When this option is specified (and true), then this
+    /// GlareDB instance will establish an insecure connection. Use for
+    /// testing and development.
     #[builder(setter(strip_option))]
     pub disable_tls: Option<bool>,
+    /// Location of the GlareDB cloud instance used by GlareDB
+    /// to negotiate out-of-band certificate provisioning.
     #[builder(default = "Some(\"https://console.glaredb.com\".to_string())")]
     #[builder(setter(into, strip_option))]
     pub cloud_addr: Option<String>,
+    /// Client type distinguishes what kind of remote client this is,
+    /// and is used for logging and introspection.
     #[builder(default = "Some(RemoteClientType::Cli)")]
     #[builder(setter(strip_option))]
     pub client_type: Option<RemoteClientType>,
+    /// Specify an optional environment reader, which GlareDB uses in
+    /// embedded cases so that queries can bindings to extract tables
+    /// from variables in the binding's scope that data frames, or the
+    /// output of a query.
     #[builder(setter(strip_option))]
     pub environment_reader: Option<Arc<dyn EnvironmentReader>>,
 }
 
 impl ConnectOptionsBuilder {
+    /// Adds a single option (key/value pair) to the builder for the
+    /// storage options map. All keys must be unique, and setting the
+    /// same option more than once.
     pub fn storage_option(
         &mut self,
         key: impl Into<String>,
@@ -63,11 +117,19 @@ impl ConnectOptionsBuilder {
         self.storage_options(opts)
     }
 
+    /// Overrides the storage option map in the Builder. Passing an
+    /// empty map or None to this method removes the existing data and
+    /// resets the state of the storage options in the builder.
     pub fn set_storage_options(&mut self, opts: Option<HashMap<String, String>>) -> &mut Self {
         self.storage_options = opts;
         self
     }
 
+    /// Constructs an in-memory connection configuration, which can be
+    /// used for default operations and tests without impacting the
+    /// file system. All state (tables, catalog, etc,) are local, but
+    /// these instances can write data to files and process data in
+    /// other data sources.
     pub fn new_in_memory() -> Self {
         Self::default()
             .connection_target(None)
@@ -79,6 +141,8 @@ impl ConnectOptionsBuilder {
 }
 
 impl ConnectOptions {
+    /// Creates a Connection object according to the options
+    /// specified.
     pub async fn connect(self) -> Result<Connection, ExecError> {
         let mut engine = Engine::from_storage(self.backend()).await?;
 
@@ -134,6 +198,18 @@ impl ConnectOptions {
     }
 }
 
+/// Connections hold the state of a GlareDB object. These connections
+/// are not always backed by network connections, and in all cases
+/// include the full capabilities of a local GlareDB instance. When
+/// connected to a remote GlareDB instance, all execution is hybrid
+/// wherein queries are parsed, planed and orchestrated locally, but
+/// executation can occur locally or on the remote instance according
+/// to capacity.
+///
+/// All of the connection's operations are lazy, and return
+/// `Operation` objects that must be executed in order for the query
+/// to run. `Operation` objects can be executed more than once to
+/// rerun the query.
 #[derive(Debug, Clone)]
 pub struct Connection {
     session: Arc<Mutex<TrackedSession>>,
@@ -141,6 +217,9 @@ pub struct Connection {
 }
 
 impl Connection {
+    /// Execute creates a query that is parsed and then evaluates and
+    /// runs immediately when the Operation is invoked regardless of
+    /// the content.
     pub fn execute(&self, query: impl Into<String>) -> Operation {
         Operation {
             op: OperationType::Execute,
@@ -150,6 +229,10 @@ impl Connection {
         }
     }
 
+    /// Creates a query that is parsed when the Operation is invoked;
+    /// however, the query is only executed when the results are
+    /// iterated _unless_ the operation is a write operation or a DDL
+    /// operation, which are executed when the operation is invoked.
     pub fn sql(&self, query: impl Into<String>) -> Operation {
         Operation {
             op: OperationType::Sql,
@@ -159,6 +242,10 @@ impl Connection {
         }
     }
 
+    /// PRQL queries have the same semantics as SQL queries; however,
+    /// because PRQL does not include syntax for DML or DDL
+    /// operations, these queries only run when the result stream are
+    /// invoked.
     pub fn prql(&self, query: impl Into<String>) -> Operation {
         Operation {
             op: OperationType::Prql,
@@ -169,6 +256,9 @@ impl Connection {
     }
 }
 
+/// RecordStream is like DataFusion's `SendableRecordBatchStream`,
+/// except it does not provide access to the schema except via the
+/// results.
 pub struct RecordStream(Pin<Box<dyn Stream<Item = Result<RecordBatch, DataFusionError>> + Send>>);
 
 impl Stream for RecordStream {
@@ -205,8 +295,16 @@ impl From<Result<SendableRecordBatchStream, ExecError>> for RecordStream {
 
 #[derive(Debug, Clone)]
 enum OperationType {
+    /// SQL operations create a lazy operation that runs DDL/DML
+    /// operations directly, and executes other queries when the
+    /// results are iterated.
     Sql,
+    /// PRQL, which does not support DDL/DML in our implementation,
+    /// creates a lazy query object that only runs when the results
+    /// are iterated.
     Prql,
+    /// Execute Operations run a SQL operation directly when the
+    /// `Operation`'s `execute()` method runs.
     Execute,
 }
 
@@ -226,10 +324,20 @@ impl ToString for Operation {
 }
 
 impl Operation {
+    /// Schema returns the schema of the results. This method returns
+    /// `None` before the query executes.
     pub fn schema(&self) -> Option<Arc<Schema>> {
         self.schema.clone()
     }
 
+    /// Executes the query, according to the semantics of the operation's
+    /// type. Returns an error if there was a problem parsing the
+    /// query or creating a stream. Operations created with
+    /// `execute()` run when this `execute()` method runs. For
+    /// operations with the `sql()` method, write operations and DDL
+    /// operations run before `execute()` returns. All other
+    /// operations are lazy and only execute when the results are
+    /// processed.
     pub async fn execute(&mut self) -> Result<SendableRecordBatchStream, ExecError> {
         match self.op {
             OperationType::Sql => {
@@ -298,6 +406,9 @@ impl Operation {
         }
     }
 
+    /// Call returns the results of the query as a stream. No
+    /// processing happens until the stream is processed, and errors
+    /// parsing the query are returned as the the first result.
     pub fn call(&mut self) -> RecordStream {
         let mut op = self.clone();
         RecordStream(Box::pin(
