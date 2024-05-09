61a1e6d754f609745c681f5ee293a050dad2ee7f
tycho garen <garen@tychoish.com>
2024-05-09 11:48:21 -0400

    dir fix

--- rstests/tests/catalog_compat.rs
+++ rstests/tests/catalog_compat.rs
@@ -3,9 +3,22 @@ use crate::setup::make_cli;
 
 #[test]
 /// Assert that we can still load the old catalog without panicking
+fn test_check_compatability_fixture() {
+    let old_catalog = std::env::current_dir()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("testdata/catalog_compat/v0");
+
+    assert!(old_catalog.exists());
+    assert!(old_catalog.is_dir())
+}
+
+#[test]
+/// Assert that we can still load the old catalog without panicking
 fn test_catalog_backwards_compat() {
     let pwd = std::env::current_dir().unwrap();
-    let root_dir = pwd.parent().unwrap().parent().unwrap();
+    let root_dir = pwd.parent().unwrap();
     let old_catalog = root_dir.join("testdata/catalog_compat/v0");
 
     make_cli()
@@ -18,7 +31,7 @@ fn test_catalog_backwards_compat() {
 /// Make sure that we can read the table options from the old catalog
 fn test_catalog_backwards_compat_tbl_options() {
     let pwd = std::env::current_dir().unwrap();
-    let root_dir = pwd.parent().unwrap().parent().unwrap();
+    let root_dir = pwd.parent().unwrap();
     let old_catalog = root_dir.join("testdata/catalog_compat/v0");
 
     make_cli()
