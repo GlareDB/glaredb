839abcbd824c7aade0d1a71f4b684610d842593b
Sam Kleinman <sam@glaredb.com>
2024-07-23 21:34:40 -0400

    chore(deps): rust dependency batch (#3100)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- crates/datasources/src/sqlserver/errors.rs
+++ crates/datasources/src/sqlserver/errors.rs
@@ -1,17 +1,30 @@
+use deltalake::arrow::datatypes::DataType;
+
 #[derive(Debug, thiserror::Error)]
 pub enum SqlServerError {
     #[error("{0}")]
     String(String),
+
     #[error(transparent)]
     Tiberius(#[from] tiberius::error::Error),
+
     #[error(transparent)]
     Io(#[from] std::io::Error),
+
     #[error(transparent)]
     Arrow(#[from] datafusion::arrow::error::ArrowError),
+
     #[error(transparent)]
     Fmt(#[from] std::fmt::Error),
+
     #[error(transparent)]
     DatasourceCommon(#[from] crate::common::errors::DatasourceCommonError),
+
+    #[error("usupported sqlserver data for type: {0}")]
+    UnsupportedDataType(DataType),
+
+    #[error(transparent)]
+    IntConversion(#[from] std::num::TryFromIntError),
 }
 
 pub type Result<T, E = SqlServerError> = std::result::Result<T, E>;
