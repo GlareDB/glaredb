5daa25e9f5d69f17d748fd50e7c661ab74f44f92
tycho garen <garen@tychoish.com>
2024-08-02 11:35:28 -0400

    fix: test to match new intended behavior

--- bindings/python/tests/test_execution_model.py
+++ bindings/python/tests/test_execution_model.py
@@ -2,30 +2,20 @@ import glaredb
 import pytest
 
 
-def test_eager_ddl():
+def test_sql_validates_plan():
     con = glaredb.connect()
-    
+
     with pytest.raises(Exception):
-        con.sql("select count(*) from tblsqlhelper;").to_arrow().to_pydict()["COUNT(*)"][0] == 0
+        con.sql("select count(*) from tblsqlhelper;")
 
-    one = con.sql("create table tblsqlhelper (a int, b int);")
-    assert con.sql("select * from tblsqlhelper;").to_arrow().num_rows == 0
+def test_sql_excludes_ddl():
+    con = glaredb.connect()
 
-    two = con.sql("insert into tblsqlhelper values (4, 2);")
-    assert con.sql("select * from tblsqlhelper;").to_arrow().num_rows == 1
+    with pytest.raises(Exception):
+        con.sql("select count(*) from tblsqlhelper;").to_arrow().to_pydict()["COUNT(*)"][0]
 
-    with pytest.raises(Exception, match="Duplicate name"):
-        one.execute()
-
-    assert con.sql("select count(*) from tblsqlhelper;").to_arrow().to_pydict()["COUNT(*)"][0] == 1
-
-    two = con.sql("insert into tblsqlhelper values (1, 2);")
-    assert con.sql("select * from tblsqlhelper;").to_arrow().num_rows == 2
-    assert con.sql("select count(*) from tblsqlhelper;").to_arrow().to_pydict()["COUNT(*)"][0] == 2
-
-    two.execute()
-    assert con.sql("select * from tblsqlhelper;").to_arrow().num_rows == 3
-
+    with pytest.raises(Exception):
+        con.sql("create table tblsqlhelper (a int, b int);")
 
 def test_execute_is_eager():
     con = glaredb.connect()
@@ -36,4 +26,3 @@ def test_execute_is_eager():
     con.execute("create table tblexechelper (a int, b int);")
 
     assert con.sql("select count(*) from tblexechelper;").to_arrow().to_pydict()["COUNT(*)"][0] == 0
-
