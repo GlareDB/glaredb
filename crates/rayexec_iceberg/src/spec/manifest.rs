bb84524bb294d1ecbdafb7bff0f9d8a132610204
Sean Smith <scsmithr@gmail.com>
2024-11-21 15:26:49 -0500

    spec

---
+++ crates/rayexec_iceberg/src/spec/manifest.rs
@@ -0,0 +1,302 @@
+use std::collections::HashMap;
+use std::fmt;
+use std::str::FromStr;
+
+use apache_avro::{from_value, Reader};
+use rayexec_error::{RayexecError, Result, ResultExt};
+use serde::{Deserialize, Serialize};
+
+use super::{PartitionField, Schema};
+use crate::spec::PartitionSpec;
+
+/// Manifest lists include summary medata for the table alongside the path the
+/// actual manifest.
+#[allow(dead_code)]
+#[derive(Debug, Clone, Deserialize)]
+pub struct ManifestListEntry {
+    pub manifest_path: String,
+    pub manifest_length: i64,
+    pub partition_spec_id: i32,
+    /// > The type of files tracked by the manifest, either data or delete
+    /// > files; 0 for all v1 manifests
+    ///
+    /// `0`: data
+    /// `1`: deletes
+    #[serde(default)]
+    pub content: i32,
+    #[serde(default)]
+    pub sequence_number: i64,
+    #[serde(default)]
+    pub min_sequence_number: i64,
+    #[serde(default)]
+    pub added_snapshot_id: i64,
+    /// > Number of entries in the manifest that have status ADDED (1), when
+    /// > null this is assumed to be non-zero
+    // TODO: Remove default and deserialize into something more meaningful.
+    #[serde(default)]
+    pub added_files_count: i32,
+    /// > Number of entries in the manifest that have status EXISTING (0), when
+    /// > null this is assumed to be non-zero
+    #[serde(default)]
+    pub existing_files_count: i32,
+    /// > Number of entries in the manifest that have status DELETED (2), when
+    /// > null this is assumed to be non-zero
+    #[serde(default)]
+    pub deleted_files_count: i32,
+    /// > Number of rows in all of files in the manifest that have status ADDED,
+    /// > when null this is assumed to be non-zero
+    #[serde(default)]
+    pub added_rows_count: i32,
+    /// > Number of rows in all of files in the manifest that have status
+    /// > EXISTING, when null this is assumed to be non-zero
+    #[serde(default)]
+    pub existing_rows_count: i32,
+    pub deleted_rows_count: i64,
+    pub partitions: Vec<FieldSummary>,
+    #[serde(with = "serde_bytes")]
+    pub key_metadata: Option<Vec<u8>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ManifestList {
+    pub entries: Vec<ManifestListEntry>,
+}
+
+impl ManifestList {
+    /// Read a manifest list from a reader over an Avro file.
+    pub fn from_raw_avro(reader: impl std::io::Read) -> Result<ManifestList> {
+        let reader = Reader::new(reader).map_err(|e| {
+            RayexecError::new(format!(
+                "failed to create avro reader for manifest list: {e}"
+            ))
+        })?;
+
+        let mut entries = Vec::new();
+        for value in reader {
+            let value = value.map_err(|e| {
+                RayexecError::new(format!("failed to get value for manifest list entry: {e}"))
+            })?;
+            let entry: ManifestListEntry = from_value(&value).map_err(|e| {
+                RayexecError::new(format!(
+                    "failed to deserialize value for manifest list entry: {e}"
+                ))
+            })?;
+
+            entries.push(entry);
+        }
+
+        Ok(ManifestList { entries })
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Deserialize)]
+pub struct FieldSummary {
+    pub contains_null: bool,
+    pub contains_nan: bool,
+    #[serde(with = "serde_bytes")]
+    pub lower_bound: Option<Vec<u8>>,
+    #[serde(with = "serde_bytes")]
+    pub upper_bound: Option<Vec<u8>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ManifestMetadata {
+    pub schema: Schema,
+    pub schema_id: i32,
+    pub partition_spec: Vec<PartitionField>,
+    pub partition_spec_id: i32,
+    pub format_version: i32,
+    pub content: ManifestContent,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum ManifestContent {
+    Data,
+    Delete,
+}
+
+impl FromStr for ManifestContent {
+    type Err = RayexecError;
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(match s {
+            "data" => ManifestContent::Data,
+            "delete" => ManifestContent::Delete,
+            other => {
+                return Err(RayexecError::new(format!(
+                    "'{other}' is not valid content for manifest"
+                )))
+            }
+        })
+    }
+}
+
+impl fmt::Display for ManifestContent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ManifestContent::Data => write!(f, "data"),
+            ManifestContent::Delete => write!(f, "delete"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Manifest {
+    pub metadata: ManifestMetadata,
+    pub entries: Vec<ManifestEntry>,
+}
+
+impl Manifest {
+    pub fn from_raw_avro(reader: impl std::io::Read) -> Result<Manifest> {
+        let reader = Reader::new(reader).map_err(|e| {
+            RayexecError::new(format!("failed to create avro reader for manifest: {e}"))
+        })?;
+
+        let m = reader.user_metadata();
+
+        fn get_metadata_field<'a>(
+            m: &'a HashMap<String, Vec<u8>>,
+            field: &str,
+        ) -> Result<&'a Vec<u8>> {
+            m.get(field).ok_or_else(|| {
+                RayexecError::new(format!("Missing field '{field}' in manifest metadata"))
+            })
+        }
+
+        fn get_metadata_as_i32(m: &HashMap<String, Vec<u8>>, field: &str) -> Result<i32> {
+            let bs = get_metadata_field(m, field)?;
+            String::from_utf8_lossy(bs).parse::<i32>().map_err(|e| {
+                RayexecError::new(format!("Failed to parse 'schema-id' as an i32: {e}"))
+            })
+        }
+
+        let schema = serde_json::from_slice::<Schema>(get_metadata_field(m, "schema")?)
+            .context("failed to deserialize schema json")?;
+        // Spec says schema id is required, but seems like it's actually
+        // optional. Missing from the spark outputs.
+        let schema_id = get_metadata_as_i32(m, "schema-id").unwrap_or_default();
+
+        let partition_spec_id = get_metadata_as_i32(m, "partition-spec-id")?;
+
+        let raw_partition_spec = m.get("partition-spec").ok_or_else(|| {
+            RayexecError::new("Missing field 'partition-spec' in manifest metadata".to_string())
+        })?;
+
+        let partition_spec = match serde_json::from_slice::<PartitionSpec>(raw_partition_spec) {
+            Ok(spec) => spec.fields,
+            Err(_e) => {
+                // Try to get it as a slice of PartitionField.
+                serde_json::from_slice(raw_partition_spec)
+                    .context("failed to deserialize raw partition spec")?
+            }
+        };
+
+        let format_version = get_metadata_as_i32(m, "format-version")?;
+        let content = match get_metadata_field(m, "content") {
+            Ok(c) => String::from_utf8_lossy(c).parse()?,
+            Err(_) => ManifestContent::Data,
+        };
+
+        let metadata = ManifestMetadata {
+            schema,
+            schema_id,
+            partition_spec,
+            partition_spec_id,
+            format_version,
+            content,
+        };
+
+        let mut entries = Vec::new();
+        for value in reader {
+            let value = value.map_err(|e| {
+                RayexecError::new(format!("failed to get value for manifest entry: {e}"))
+            })?;
+            let entry: ManifestEntry = from_value(&value).map_err(|e| {
+                RayexecError::new(format!(
+                    "failed to deserialize value for manifest entry: {e}"
+                ))
+            })?;
+            entries.push(entry);
+        }
+
+        Ok(Manifest { metadata, entries })
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize)]
+pub enum ManifestEntryStatus {
+    #[default]
+    Existing,
+    Added,
+    Deleted,
+}
+
+impl ManifestEntryStatus {
+    pub fn is_deleted(&self) -> bool {
+        matches!(self, Self::Deleted)
+    }
+}
+
+impl TryFrom<i32> for ManifestEntryStatus {
+    type Error = RayexecError;
+
+    fn try_from(value: i32) -> std::prelude::v1::Result<Self, Self::Error> {
+        Ok(match value {
+            0 => Self::Existing,
+            1 => Self::Added,
+            2 => Self::Deleted,
+            i => {
+                return Err(RayexecError::new(format!(
+                    "unknown manifest entry status: {i}"
+                )))
+            }
+        })
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ManifestEntry {
+    pub status: i32,
+    /// Required in v2
+    pub snapshot_id: Option<i64>,
+    /// Required in v2
+    pub sequence_number: Option<i64>,
+    /// Required in v2
+    pub file_sequence_number: Option<i64>,
+    pub data_file: DataFile,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DataFile {
+    #[serde(default)]
+    pub content: i32,
+    pub file_path: String,
+    pub file_format: String,
+    pub record_count: i64,
+    pub file_size_in_bytes: i64,
+    pub column_sizes: Option<Vec<I64Entry>>,
+    pub value_counts: Option<Vec<I64Entry>>,
+    pub null_value_counts: Option<Vec<I64Entry>>,
+    pub nan_value_counts: Option<Vec<I64Entry>>,
+    pub distinct_counts: Option<Vec<I64Entry>>,
+    pub lower_bounds: Option<Vec<BinaryEntry>>,
+    pub upper_bounds: Option<Vec<BinaryEntry>>,
+    #[serde(with = "serde_bytes")]
+    pub key_metadata: Option<Vec<u8>>,
+    pub split_offsets: Option<Vec<i64>>,
+    pub equality_ids: Option<Vec<i32>>,
+    pub sort_order_id: Option<i32>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BinaryEntry {
+    key: i32,
+    #[serde(with = "serde_bytes")]
+    value: Vec<u8>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct I64Entry {
+    key: i32,
+    value: i64,
+}
---
+++ crates/rayexec_iceberg/src/spec/schema.rs
@@ -0,0 +1,477 @@
+use std::str::FromStr;
+use std::sync::LazyLock;
+
+use rayexec_bullet::datatype::{
+    DataType,
+    DecimalTypeMeta,
+    ListTypeMeta,
+    TimeUnit,
+    TimestampTypeMeta,
+};
+use rayexec_bullet::field::Field;
+use rayexec_error::{not_implemented, RayexecError, Result};
+use regex::Regex;
+use serde::{de, Deserialize, Deserializer};
+
+/// Primitive types supported in iceberg tables.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PrimitiveType {
+    Boolean,
+    Int,
+    Long,
+    Float,
+    Double,
+    Decimal {
+        p: u8,
+        s: u8,
+    },
+    /// > Calendar date without timezone or time
+    Date,
+    /// > Time of day, microsecond precision, without date, timezone
+    Time,
+    /// > Timestamp, microsecond precision, without timezone
+    Timestamp,
+    /// > Timestamp, microsecond precision, with timezone
+    Timestamptz,
+    String,
+    Uuid,
+    Fixed(usize),
+    Binary,
+}
+
+impl TryFrom<PrimitiveType> for DataType {
+    type Error = RayexecError;
+
+    fn try_from(value: PrimitiveType) -> Result<Self> {
+        Ok(match value {
+            PrimitiveType::Boolean => DataType::Boolean,
+            PrimitiveType::Int => DataType::Int32,
+            PrimitiveType::Long => DataType::Int64,
+            PrimitiveType::Float => DataType::Float32,
+            PrimitiveType::Double => DataType::Float64,
+            PrimitiveType::Decimal { p, s } => DataType::Decimal128(DecimalTypeMeta {
+                precision: p,
+                scale: s as i8,
+            }),
+            PrimitiveType::Date => DataType::Date32,
+            PrimitiveType::Time => {
+                DataType::Timestamp(TimestampTypeMeta::new(TimeUnit::Microsecond))
+            } // TODO: Possibly `Time32` instead?
+            PrimitiveType::Timestamp => {
+                DataType::Timestamp(TimestampTypeMeta::new(TimeUnit::Microsecond))
+            }
+            PrimitiveType::Timestamptz => not_implemented!("Timestamp with timezone"),
+            PrimitiveType::String => DataType::Utf8,
+            PrimitiveType::Uuid => DataType::Utf8,
+            PrimitiveType::Fixed(l) => not_implemented!("Fixed sized binary"),
+            PrimitiveType::Binary => DataType::Binary,
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for PrimitiveType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: &str = Deserialize::deserialize(deserializer)?;
+        PrimitiveType::from_str(s).map_err(de::Error::custom)
+    }
+}
+
+impl FromStr for PrimitiveType {
+    type Err = RayexecError;
+
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(match s {
+            "boolean" => PrimitiveType::Boolean,
+            "int" => PrimitiveType::Int,
+            "long" => PrimitiveType::Long,
+            "float" => PrimitiveType::Float,
+            "double" => PrimitiveType::Double,
+            "date" => PrimitiveType::Date,
+            "time" => PrimitiveType::Time,
+            "timestamp" => PrimitiveType::Timestamp,
+            "timestamptz" => PrimitiveType::Timestamptz,
+            "string" => PrimitiveType::String,
+            "uuid" => PrimitiveType::Uuid,
+            "binary" => PrimitiveType::Binary,
+            other if other.starts_with("decimal") => {
+                // Regex that matches:
+                // decimal(15, 2)
+                // decimal(15,2)
+                static DECIMAL_RE: LazyLock<Regex> =
+                    LazyLock::new(|| Regex::new(r"^decimal\((?P<p>\d+),\s?(?P<s>\d+)\)$").unwrap());
+
+                let captures = DECIMAL_RE
+                    .captures(other)
+                    .ok_or_else(|| RayexecError::new(format!("Invalid decimal type: {other}")))?;
+
+                let p: u8 = captures
+                    .name("p")
+                    .ok_or_else(|| RayexecError::new(format!("Invalid decimal type: {other}")))?
+                    .as_str()
+                    .parse()
+                    .map_err(|e| RayexecError::new(format!("Decimal precision not a u8: {e}")))?;
+
+                let s: u8 = captures
+                    .name("s")
+                    .ok_or_else(|| RayexecError::new(format!("Invalid decimal type: {other}")))?
+                    .as_str()
+                    .parse()
+                    .map_err(|e| RayexecError::new(format!("Decimal scale not a u8: {e}")))?;
+
+                PrimitiveType::Decimal { p, s }
+            }
+            other if other.starts_with("fixed") => {
+                // Regex that matches:
+                // fixed[16]
+                static FIXED_RE: LazyLock<Regex> =
+                    LazyLock::new(|| Regex::new(r"^fixed\[(?P<l>\d+)\]$").unwrap());
+
+                let captures = FIXED_RE
+                    .captures(other)
+                    .ok_or_else(|| RayexecError::new(format!("Invalid fixed type: {other}")))?;
+
+                let l: usize = captures
+                    .name("l")
+                    .ok_or_else(|| RayexecError::new(format!("Invalid fixed type: {other}")))?
+                    .as_str()
+                    .parse()
+                    .map_err(|e| RayexecError::new(format!("Fixed length not a usize: {e}")))?;
+
+                PrimitiveType::Fixed(l)
+            }
+            other => return Err(RayexecError::new(format!("Invalid type: {other}"))),
+        })
+    }
+}
+
+/// Union between primitive and nested types.
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
+#[serde(untagged)]
+pub enum AnyType {
+    Primitive(PrimitiveType),
+    List(ListType),
+    Struct(StructType),
+    Map(MapType),
+}
+
+impl TryFrom<&AnyType> for DataType {
+    type Error = RayexecError;
+
+    fn try_from(value: &AnyType) -> Result<Self> {
+        Ok(match value {
+            AnyType::Primitive(t) => (*t).try_into()?,
+            AnyType::List(t) => t.try_into()?,
+            AnyType::Struct(t) => t.try_into()?,
+            AnyType::Map(t) => t.try_into()?,
+        })
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case")]
+#[serde(tag = "type", rename = "list")]
+pub struct ListType {
+    pub element_id: i32,
+    pub element_required: bool,
+    pub element: Box<AnyType>,
+}
+
+impl TryFrom<&ListType> for DataType {
+    type Error = RayexecError;
+
+    fn try_from(value: &ListType) -> Result<Self> {
+        let typ = DataType::try_from(value.element.as_ref())?;
+        Ok(DataType::List(ListTypeMeta::new(typ)))
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case")]
+#[serde(tag = "type", rename = "map")]
+pub struct MapType {
+    pub key_id: i32,
+    pub key: Box<AnyType>,
+    pub value_id: i32,
+    pub value_required: bool,
+    pub value: Box<AnyType>,
+}
+
+impl TryFrom<&MapType> for DataType {
+    type Error = RayexecError;
+
+    fn try_from(_value: &MapType) -> Result<Self> {
+        Err(RayexecError::new("Iceberg map type not yet implemented"))
+        // let key_field = ArrowField::new("key", value.key.as_ref().try_into()?, false);
+        // let val_field = ArrowField::new(
+        //     "value",
+        //     value.value.as_ref().try_into()?,
+        //     value.value_required,
+        // );
+        // let field = ArrowField::new_struct("entryies", vec![key_field, val_field], false);
+
+        // let typ = DataType::Map(Arc::new(field), false);
+
+        // Ok(typ)
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case")]
+#[serde(tag = "type", rename = "struct")]
+pub struct StructType {
+    pub fields: Vec<StructField>,
+}
+
+impl TryFrom<&StructType> for DataType {
+    type Error = RayexecError;
+
+    fn try_from(value: &StructType) -> Result<Self> {
+        Err(RayexecError::new("Iceberg struct type not yet implemented"))
+        // let fields = value
+        //     .fields
+        //     .iter()
+        //     .map(|f| {
+        //         let typ = &f.r#type;
+        //         Ok(ArrowField::new(&f.name, typ.try_into()?, !f.required))
+        //     })
+        //     .collect::<Result<Vec<_>>>()?;
+
+        // let typ = DataType::Struct(fields.into());
+        // Ok(typ)
+    }
+}
+
+/// Fields on a struct.
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case")]
+pub struct StructField {
+    pub id: i32,
+    pub name: String,
+    pub required: bool,
+    pub r#type: AnyType,
+    pub doc: Option<String>,
+    /// JSON serialized initial value for the field.
+    pub initial_default: Option<String>, // TODO
+    /// JSON serialized write default value for the field.
+    pub write_default: Option<String>, // TODO
+}
+
+impl StructField {
+    pub fn to_field(&self) -> Result<Field> {
+        let typ = &self.r#type;
+        Ok(Field::new(&self.name, typ.try_into()?, !self.required))
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Schema {
+    pub schema_id: i32,
+    pub identifier_field_ids: Option<Vec<i32>>,
+    pub fields: Vec<StructField>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deserialize_long_field() {
+        let json = r#"
+            {
+              "id" : 1,
+              "name" : "l_orderkey",
+              "required" : false,
+              "type" : "long"
+            }"#;
+
+        let deserialized: StructField = serde_json::from_str(json).unwrap();
+        let expected = StructField {
+            id: 1,
+            name: "l_orderkey".to_string(),
+            required: false,
+            r#type: AnyType::Primitive(PrimitiveType::Long),
+            doc: None,
+            initial_default: None,
+            write_default: None,
+        };
+        assert_eq!(expected, deserialized);
+    }
+
+    #[test]
+    fn test_deserialize_decimal_field() {
+        let json = r#"
+            {
+              "id" : 6,
+              "name" : "l_extendedprice",
+              "required" : false,
+              "type" : "decimal(15, 2)"
+            }"#;
+
+        let deserialized: StructField = serde_json::from_str(json).unwrap();
+        let expected = StructField {
+            id: 6,
+            name: "l_extendedprice".to_string(),
+            required: false,
+            r#type: AnyType::Primitive(PrimitiveType::Decimal { p: 15, s: 2 }),
+            doc: None,
+            initial_default: None,
+            write_default: None,
+        };
+        assert_eq!(expected, deserialized);
+    }
+
+    #[test]
+    fn test_deserialize_decimal_no_space_field() {
+        // See lack of space after comma in decimal type.
+        let json = r#"
+            {
+              "id" : 6,
+              "name" : "l_extendedprice",
+              "required" : false,
+              "type" : "decimal(15,2)"
+            }"#;
+
+        let deserialized: StructField = serde_json::from_str(json).unwrap();
+        let expected = StructField {
+            id: 6,
+            name: "l_extendedprice".to_string(),
+            required: false,
+            r#type: AnyType::Primitive(PrimitiveType::Decimal { p: 15, s: 2 }),
+            doc: None,
+            initial_default: None,
+            write_default: None,
+        };
+        assert_eq!(expected, deserialized);
+    }
+
+    #[test]
+    fn test_deserialize_list_field() {
+        let json = r#"
+            {
+              "id" : 6,
+              "name" : "l_extendedprice",
+              "required" : false,
+              "type" : {
+                "type": "list",
+                "element-id": 3,
+                "element-required": true,
+                "element": "string"
+              }
+            }"#;
+
+        let deserialized: StructField = serde_json::from_str(json).unwrap();
+        let expected = StructField {
+            id: 6,
+            name: "l_extendedprice".to_string(),
+            required: false,
+            r#type: AnyType::List(ListType {
+                element_required: true,
+                element_id: 3,
+                element: Box::new(AnyType::Primitive(PrimitiveType::String)),
+            }),
+            doc: None,
+            initial_default: None,
+            write_default: None,
+        };
+        assert_eq!(expected, deserialized);
+    }
+
+    #[test]
+    fn test_deserialize_struct_field() {
+        let json = r#"
+            {
+              "id" : 6,
+              "name" : "l_extendedprice",
+              "required" : false,
+              "type" : {
+                "type": "struct",
+                "fields": [ {
+                  "id": 1,
+                  "name": "id",
+                  "required": true,
+                  "type": "uuid",
+                  "initial-default": "0db3e2a8-9d1d-42b9-aa7b-74ebe558dceb",
+                  "write-default": "ec5911be-b0a7-458c-8438-c9a3e53cffae"
+                }, {
+                  "id": 2,
+                  "name": "data",
+                  "required": false,
+                  "type": "string"
+                } ]
+              }
+            }"#;
+
+        let deserialized: StructField = serde_json::from_str(json).unwrap();
+        let expected = StructField {
+            id: 6,
+            name: "l_extendedprice".to_string(),
+            required: false,
+            r#type: AnyType::Struct(StructType {
+                fields: vec![
+                    StructField {
+                        id: 1,
+                        name: "id".to_string(),
+                        required: true,
+                        r#type: AnyType::Primitive(PrimitiveType::Uuid),
+                        initial_default: Some("0db3e2a8-9d1d-42b9-aa7b-74ebe558dceb".to_string()),
+                        write_default: Some("ec5911be-b0a7-458c-8438-c9a3e53cffae".to_string()),
+                        doc: None,
+                    },
+                    StructField {
+                        id: 2,
+                        name: "data".to_string(),
+                        required: false,
+                        r#type: AnyType::Primitive(PrimitiveType::String),
+                        initial_default: None,
+                        write_default: None,
+                        doc: None,
+                    },
+                ],
+            }),
+            doc: None,
+            initial_default: None,
+            write_default: None,
+        };
+        assert_eq!(expected, deserialized);
+    }
+
+    #[test]
+    fn test_deserialize_map_field() {
+        let json = r#"
+            {
+              "id" : 6,
+              "name" : "l_extendedprice",
+              "required" : false,
+              "type" : {
+                "type": "map",
+                "key-id": 4,
+                "key": "string",
+                "value-id": 5,
+                "value-required": false,
+                "value": "double"
+              }
+            }"#;
+
+        let deserialized: StructField = serde_json::from_str(json).unwrap();
+        let expected = StructField {
+            id: 6,
+            name: "l_extendedprice".to_string(),
+            required: false,
+            r#type: AnyType::Map(MapType {
+                key_id: 4,
+                key: Box::new(AnyType::Primitive(PrimitiveType::String)),
+                value: Box::new(AnyType::Primitive(PrimitiveType::Double)),
+                value_id: 5,
+                value_required: false,
+            }),
+            doc: None,
+            initial_default: None,
+            write_default: None,
+        };
+        assert_eq!(expected, deserialized);
+    }
+}
