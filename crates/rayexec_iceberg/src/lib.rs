a0eef66e4bf58eb6a803bc43254cecb9a8023355
Sean Smith <scsmithr@gmail.com>
2024-11-21 15:52:41 -0500

    stub read_iceberg

--- crates/rayexec_iceberg/src/lib.rs
+++ crates/rayexec_iceberg/src/lib.rs
@@ -1,1 +1,28 @@
 pub mod spec;
+
+mod read_iceberg;
+
+use rayexec_execution::datasource::{DataSource, DataSourceBuilder, FileHandler};
+use rayexec_execution::functions::table::TableFunction;
+use rayexec_execution::runtime::Runtime;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct IcebergDataSource<R: Runtime> {
+    runtime: R,
+}
+
+impl<R: Runtime> DataSourceBuilder<R> for IcebergDataSource<R> {
+    fn initialize(runtime: R) -> Box<dyn DataSource> {
+        Box::new(Self { runtime })
+    }
+}
+
+impl<R: Runtime> DataSource for IcebergDataSource<R> {
+    fn initialize_table_functions(&self) -> Vec<Box<dyn TableFunction>> {
+        Vec::new()
+    }
+
+    fn file_handlers(&self) -> Vec<FileHandler> {
+        Vec::new()
+    }
+}
---
+++ crates/rayexec_iceberg/src/read_iceberg.rs
@@ -0,0 +1,80 @@
+use futures::future::BoxFuture;
+use rayexec_bullet::field::Schema;
+use rayexec_error::Result;
+use rayexec_execution::database::DatabaseContext;
+use rayexec_execution::functions::table::{PlannedTableFunction, TableFunction, TableFunctionArgs};
+use rayexec_execution::runtime::Runtime;
+use rayexec_execution::storage::table_storage::DataTable;
+use rayexec_io::location::{AccessConfig, FileLocation};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ReadIceberg<R: Runtime> {
+    pub(crate) runtime: R,
+}
+
+impl<R: Runtime> TableFunction for ReadIceberg<R> {
+    fn name(&self) -> &'static str {
+        "read_iceberg"
+    }
+
+    fn aliases(&self) -> &'static [&'static str] {
+        &["iceberg_scan"]
+    }
+
+    fn plan_and_initialize(
+        &self,
+        context: &DatabaseContext,
+        args: TableFunctionArgs,
+    ) -> BoxFuture<'_, Result<Box<dyn PlannedTableFunction>>> {
+        unimplemented!()
+    }
+
+    fn decode_state(&self, state: &[u8]) -> Result<Box<dyn PlannedTableFunction>> {
+        unimplemented!()
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ReadIcebergState {
+    location: FileLocation,
+    conf: AccessConfig,
+    schema: Schema,
+}
+
+#[derive(Debug, Clone)]
+pub struct ReadIcebergImpl<R: Runtime> {
+    func: ReadIceberg<R>,
+    state: ReadIcebergState,
+}
+
+impl<R: Runtime> ReadIcebergImpl<R> {
+    async fn initialize(
+        func: ReadIceberg<R>,
+        args: TableFunctionArgs,
+    ) -> Result<Box<dyn PlannedTableFunction>> {
+        unimplemented!()
+    }
+}
+
+impl<R: Runtime> PlannedTableFunction for ReadIcebergImpl<R> {
+    fn reinitialize(&self) -> BoxFuture<Result<()>> {
+        // TODO: See delta
+        unimplemented!()
+    }
+
+    fn table_function(&self) -> &dyn TableFunction {
+        &self.func
+    }
+
+    fn encode_state(&self, state: &mut Vec<u8>) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn schema(&self) -> Schema {
+        self.state.schema.clone()
+    }
+
+    fn datatable(&self) -> Result<Box<dyn DataTable>> {
+        unimplemented!()
+    }
+}
