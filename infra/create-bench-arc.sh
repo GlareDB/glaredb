198ab09b83f1b1f4ab8fb70864e1cff7f51aa222
Sean Smith <scsmithr@gmail.com>
2024-11-29 19:51:42 -0500

    devops

---
+++ infra/create-bench-arc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eux
+
+: "${GITHUB_PAT:?Github access token needs to be set}"
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+INSTALLATION_NAME="arc-runner-set-bench"
+NAMESPACE="arc-runners-bench"
+
+helm install arc \
+    --namespace "${NAMESPACE}" \
+    --create-namespace \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
+helm install "${INSTALLATION_NAME}" \
+    --namespace "${NAMESPACE}" \
+    --create-namespace \
+    --values "${REPO_ROOT}/infra/bench-arc-values.yaml" \
+    --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+    --version "0.9.3" \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
