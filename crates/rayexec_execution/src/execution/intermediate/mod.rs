8277743dae81ab82c3e150873cccb5793d8eefc5
Sean Smith <scsmithr@gmail.com>
2024-12-09 13:14:10 -0600

    fixup! move intermediate pipeline

--- crates/rayexec_execution/src/execution/intermediate/mod.rs
+++ crates/rayexec_execution/src/execution/intermediate/mod.rs
@@ -1,2 +1,12 @@
+//! Intermediate pipeline planning.
+//!
+//! This module holds both the types for intermediate pipelines, and the planner
+//! that produces the pipelines from logical operators.
+//!
+//! Intermediate pipelines represent which operators will be taking part in
+//! query execution, but without operator-specific states. This easily lets us
+//! serialize plans for remote execution, as well as delaying assigning plan
+//! parallelism until we actually want to execute the pipelines.
+
 pub mod pipeline;
 pub mod planner;
