73c45fe35b16e1adddd9467bbd21bcd83937e0a0
Sean Smith <scsmithr@gmail.com>
2024-12-11 09:35:56 -0600

    negate

--- crates/rayexec_execution/src/functions/scalar/builtin/negate.rs
+++ crates/rayexec_execution/src/functions/scalar/builtin/negate.rs
@@ -1,8 +1,11 @@
-use rayexec_bullet::array::Array;
+use std::marker::PhantomData;
+
+use rayexec_bullet::array::{Array, ArrayData};
 use rayexec_bullet::datatype::{DataType, DataTypeId};
 use rayexec_bullet::executor::builder::{ArrayBuilder, BooleanBuffer, PrimitiveBuffer};
 use rayexec_bullet::executor::physical_type::{
     PhysicalBool,
+    PhysicalF16,
     PhysicalF32,
     PhysicalF64,
     PhysicalI128,
@@ -10,22 +13,16 @@ use rayexec_bullet::executor::physical_type::{
     PhysicalI32,
     PhysicalI64,
     PhysicalI8,
-    PhysicalType,
+    PhysicalStorage,
 };
 use rayexec_bullet::executor::scalar::UnaryExecutor;
+use rayexec_bullet::storage::PrimitiveStorage;
 use rayexec_error::Result;
-use rayexec_proto::packed::{PackedDecoder, PackedEncoder};
-use rayexec_proto::ProtoConv;
-use serde::{Deserialize, Serialize};
 
-use crate::functions::scalar::{PlannedScalarFunction2, ScalarFunction};
-use crate::functions::{
-    invalid_input_types_error,
-    plan_check_num_args,
-    unhandled_physical_types_err,
-    FunctionInfo,
-    Signature,
-};
+use crate::expr::Expression;
+use crate::functions::scalar::{PlannedScalarFuntion, ScalarFunction, ScalarFunctionImpl};
+use crate::functions::{invalid_input_types_error, plan_check_num_args, FunctionInfo, Signature};
+use crate::logical::binder::table_list::TableList;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Negate;
@@ -37,6 +34,11 @@ impl FunctionInfo for Negate {
 
     fn signatures(&self) -> &[Signature] {
         &[
+            Signature {
+                input: &[DataTypeId::Float16],
+                variadic: None,
+                return_type: DataTypeId::Float16,
+            },
             Signature {
                 input: &[DataTypeId::Float32],
                 variadic: None,
@@ -68,6 +70,11 @@ impl FunctionInfo for Negate {
                 return_type: DataTypeId::Int64,
             },
             Signature {
+                input: &[DataTypeId::Int128],
+                variadic: None,
+                return_type: DataTypeId::Int128,
+            },
+            Signature {
                 input: &[DataTypeId::Interval],
                 variadic: None,
                 return_type: DataTypeId::Interval,
@@ -77,110 +84,69 @@ impl FunctionInfo for Negate {
 }
 
 impl ScalarFunction for Negate {
-    fn decode_state(&self, state: &[u8]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        Ok(Box::new(NegateImpl {
-            datatype: DataType::from_proto(PackedDecoder::new(state).decode_next()?)?,
-        }))
-    }
+    fn plan(
+        &self,
+        table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedScalarFuntion> {
+        plan_check_num_args(self, &inputs, 1)?;
 
-    fn plan_from_datatypes(&self, inputs: &[DataType]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        plan_check_num_args(self, inputs, 1)?;
-        match &inputs[0] {
-            DataType::Int8
-            | DataType::Int16
-            | DataType::Int32
-            | DataType::Int64
-            | DataType::Float32
-            | DataType::Float64 => Ok(Box::new(NegateImpl {
-                datatype: inputs[0].clone(),
-            })),
-            other => Err(invalid_input_types_error(self, &[other])),
+        let dt = inputs[0].datatype(table_list)?;
+
+        // TODO: Interval
+        let function_impl: Box<dyn ScalarFunctionImpl> = match dt.clone() {
+            dt @ DataType::Int8 => Box::new(NegateImpl::<PhysicalI8>::new(dt)),
+            dt @ DataType::Int16 => Box::new(NegateImpl::<PhysicalI16>::new(dt)),
+            dt @ DataType::Int32 => Box::new(NegateImpl::<PhysicalI32>::new(dt)),
+            dt @ DataType::Int64 => Box::new(NegateImpl::<PhysicalI64>::new(dt)),
+            dt @ DataType::Int128 => Box::new(NegateImpl::<PhysicalI128>::new(dt)),
+            dt @ DataType::Float16 => Box::new(NegateImpl::<PhysicalF16>::new(dt)),
+            dt @ DataType::Float32 => Box::new(NegateImpl::<PhysicalF32>::new(dt)),
+            dt @ DataType::Float64 => Box::new(NegateImpl::<PhysicalF64>::new(dt)),
+            other => return Err(invalid_input_types_error(self, &[other])),
+        };
+
+        Ok(PlannedScalarFuntion {
+            function: Box::new(*self),
+            return_type: dt,
+            inputs,
+            function_impl,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct NegateImpl<S> {
+    datatype: DataType, // TODO: Would be nice not needing to store this.
+    _s: PhantomData<S>,
+}
+
+impl<S> NegateImpl<S> {
+    fn new(datatype: DataType) -> Self {
+        NegateImpl {
+            datatype,
+            _s: PhantomData,
         }
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct NegateImpl {
-    datatype: DataType,
-}
-
-impl PlannedScalarFunction2 for NegateImpl {
-    fn scalar_function(&self) -> &dyn ScalarFunction {
-        &Negate
-    }
-
-    fn encode_state(&self, state: &mut Vec<u8>) -> Result<()> {
-        PackedEncoder::new(state).encode_next(&self.datatype.to_proto()?)
-    }
-
-    fn return_type(&self) -> DataType {
-        self.datatype.clone()
-    }
-
+impl<S> ScalarFunctionImpl for NegateImpl<S>
+where
+    S: PhysicalStorage,
+    for<'a> S::Type<'a>: std::ops::Neg<Output = S::Type<'static>> + Default + Copy,
+    ArrayData: From<PrimitiveStorage<S::Type<'static>>>,
+{
     fn execute(&self, inputs: &[&Array]) -> Result<Array> {
+        use std::ops::Neg;
+
         let a = inputs[0];
-
         let datatype = self.datatype.clone();
+        let builder = ArrayBuilder {
+            datatype,
+            buffer: PrimitiveBuffer::with_len(a.logical_len()),
+        };
 
-        match a.physical_type() {
-            PhysicalType::Int8 => UnaryExecutor::execute::<PhysicalI8, _, _>(
-                a,
-                ArrayBuilder {
-                    datatype,
-                    buffer: PrimitiveBuffer::with_len(a.logical_len()),
-                },
-                |a, buf| buf.put(&(-a)),
-            ),
-            PhysicalType::Int16 => UnaryExecutor::execute::<PhysicalI16, _, _>(
-                a,
-                ArrayBuilder {
-                    datatype,
-                    buffer: PrimitiveBuffer::with_len(a.logical_len()),
-                },
-                |a, buf| buf.put(&(-a)),
-            ),
-            PhysicalType::Int32 => UnaryExecutor::execute::<PhysicalI32, _, _>(
-                a,
-                ArrayBuilder {
-                    datatype,
-                    buffer: PrimitiveBuffer::with_len(a.logical_len()),
-                },
-                |a, buf| buf.put(&(-a)),
-            ),
-            PhysicalType::Int64 => UnaryExecutor::execute::<PhysicalI64, _, _>(
-                a,
-                ArrayBuilder {
-                    datatype,
-                    buffer: PrimitiveBuffer::with_len(a.logical_len()),
-                },
-                |a, buf| buf.put(&(-a)),
-            ),
-            PhysicalType::Int128 => UnaryExecutor::execute::<PhysicalI128, _, _>(
-                a,
-                ArrayBuilder {
-                    datatype,
-                    buffer: PrimitiveBuffer::with_len(a.logical_len()),
-                },
-                |a, buf| buf.put(&(-a)),
-            ),
-            PhysicalType::Float32 => UnaryExecutor::execute::<PhysicalF32, _, _>(
-                a,
-                ArrayBuilder {
-                    datatype,
-                    buffer: PrimitiveBuffer::with_len(a.logical_len()),
-                },
-                |a, buf| buf.put(&(-a)),
-            ),
-            PhysicalType::Float64 => UnaryExecutor::execute::<PhysicalF64, _, _>(
-                a,
-                ArrayBuilder {
-                    datatype,
-                    buffer: PrimitiveBuffer::with_len(a.logical_len()),
-                },
-                |a, buf| buf.put(&(-a)),
-            ),
-            other => Err(unhandled_physical_types_err(self, [other])),
-        }
+        UnaryExecutor::execute::<S, _, _>(a, builder, |a, buf| buf.put(&(a.neg())))
     }
 }
 
@@ -202,14 +168,19 @@ impl FunctionInfo for Not {
 }
 
 impl ScalarFunction for Not {
-    fn decode_state(&self, _state: &[u8]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        Ok(Box::new(NotImpl))
-    }
-
-    fn plan_from_datatypes(&self, inputs: &[DataType]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        plan_check_num_args(self, inputs, 1)?;
-        match &inputs[0] {
-            DataType::Boolean => Ok(Box::new(NotImpl)),
+    fn plan(
+        &self,
+        table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedScalarFuntion> {
+        plan_check_num_args(self, &inputs, 1)?;
+        match inputs[0].datatype(table_list)? {
+            DataType::Boolean => Ok(PlannedScalarFuntion {
+                function: Box::new(*self),
+                return_type: DataType::Boolean,
+                inputs,
+                function_impl: Box::new(NotImpl),
+            }),
             other => Err(invalid_input_types_error(self, &[other])),
         }
     }
@@ -218,19 +189,7 @@ impl ScalarFunction for Not {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct NotImpl;
 
-impl PlannedScalarFunction2 for NotImpl {
-    fn scalar_function(&self) -> &dyn ScalarFunction {
-        &Not
-    }
-
-    fn encode_state(&self, _state: &mut Vec<u8>) -> Result<()> {
-        Ok(())
-    }
-
-    fn return_type(&self) -> DataType {
-        DataType::Boolean
-    }
-
+impl ScalarFunctionImpl for NotImpl {
     fn execute(&self, inputs: &[&Array]) -> Result<Array> {
         UnaryExecutor::execute::<PhysicalBool, _, _>(
             inputs[0],
