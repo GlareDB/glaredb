8d71bcdd3037a7a8b93210149bf836d56d247e97
Sean Smith <scsmithr@gmail.com>
2024-12-11 10:45:47 -0600

    l2

--- crates/rayexec_execution/src/functions/scalar/builtin/similarity/l2_distance.rs
+++ crates/rayexec_execution/src/functions/scalar/builtin/similarity/l2_distance.rs
@@ -1,20 +1,23 @@
+use std::marker::PhantomData;
 use std::ops::AddAssign;
 
 use num_traits::{AsPrimitive, Float};
-use rayexec_bullet::array::{Array, ArrayData};
+use rayexec_bullet::array::Array;
 use rayexec_bullet::datatype::{DataType, DataTypeId};
 use rayexec_bullet::executor::builder::{ArrayBuilder, PrimitiveBuffer};
 use rayexec_bullet::executor::physical_type::{
     PhysicalF16,
     PhysicalF32,
     PhysicalF64,
-    PhysicalType,
+    PhysicalStorage,
 };
 use rayexec_bullet::executor::scalar::{BinaryListReducer, ListExecutor};
-use rayexec_error::{RayexecError, Result};
+use rayexec_error::Result;
 
-use crate::functions::scalar::{PlannedScalarFunction2, ScalarFunction};
+use crate::expr::Expression;
+use crate::functions::scalar::{PlannedScalarFuntion, ScalarFunction, ScalarFunctionImpl};
 use crate::functions::{invalid_input_types_error, plan_check_num_args, FunctionInfo, Signature};
+use crate::logical::binder::table_list::TableList;
 
 /// Euclidean distance.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -41,74 +44,72 @@ impl FunctionInfo for L2Distance {
 }
 
 impl ScalarFunction for L2Distance {
-    fn decode_state(&self, _state: &[u8]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        Ok(Box::new(L2DistanceImpl))
-    }
+    fn plan(
+        &self,
+        table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedScalarFuntion> {
+        plan_check_num_args(self, &inputs, 2)?;
 
-    fn plan_from_datatypes(&self, inputs: &[DataType]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        plan_check_num_args(self, inputs, 2)?;
-        match (&inputs[0], &inputs[1]) {
+        let function_impl: Box<dyn ScalarFunctionImpl> = match (
+            inputs[0].datatype(table_list)?,
+            inputs[1].datatype(table_list)?,
+        ) {
             (DataType::List(a), DataType::List(b)) => {
                 match (a.datatype.as_ref(), b.datatype.as_ref()) {
-                    (DataType::Float16, DataType::Float16)
-                    | (DataType::Float32, DataType::Float32)
-                    | (DataType::Float64, DataType::Float64) => Ok(Box::new(L2DistanceImpl)),
-                    (a, b) => Err(invalid_input_types_error(self, &[a, b])),
+                    (DataType::Float16, DataType::Float16) => {
+                        Box::new(L2DistanceImpl::<PhysicalF16>::new())
+                    }
+                    (DataType::Float32, DataType::Float32) => {
+                        Box::new(L2DistanceImpl::<PhysicalF32>::new())
+                    }
+                    (DataType::Float64, DataType::Float64) => {
+                        Box::new(L2DistanceImpl::<PhysicalF64>::new())
+                    }
+                    (a, b) => return Err(invalid_input_types_error(self, &[a, b])),
                 }
             }
-            (a, b) => Err(invalid_input_types_error(self, &[a, b])),
-        }
+            (a, b) => return Err(invalid_input_types_error(self, &[a, b])),
+        };
+
+        Ok(PlannedScalarFuntion {
+            function: Box::new(*self),
+            return_type: DataType::Float64,
+            inputs,
+            function_impl,
+        })
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct L2DistanceImpl;
+pub struct L2DistanceImpl<S: PhysicalStorage> {
+    _s: PhantomData<S>,
+}
 
-impl PlannedScalarFunction2 for L2DistanceImpl {
-    fn scalar_function(&self) -> &dyn ScalarFunction {
-        &L2Distance
+impl<S> L2DistanceImpl<S>
+where
+    S: PhysicalStorage,
+{
+    fn new() -> Self {
+        L2DistanceImpl { _s: PhantomData }
     }
+}
 
-    fn encode_state(&self, _state: &mut Vec<u8>) -> Result<()> {
-        Ok(())
-    }
-
-    fn return_type(&self) -> DataType {
-        DataType::Float64
-    }
-
+impl<S> ScalarFunctionImpl for L2DistanceImpl<S>
+where
+    S: PhysicalStorage,
+    for<'a> S::Type<'a>: Float + AddAssign + AsPrimitive<f64> + Default + Copy,
+{
     fn execute(&self, inputs: &[&Array]) -> Result<Array> {
         let a = inputs[0];
         let b = inputs[1];
-
-        let physical_type = match a.array_data() {
-            ArrayData::List(l) => l.inner_array().physical_type(),
-            _other => return Err(RayexecError::new("Unexpected storage type")),
-        };
 
         let builder = ArrayBuilder {
             datatype: DataType::Float64,
             buffer: PrimitiveBuffer::with_len(a.logical_len()),
         };
 
-        match physical_type {
-            PhysicalType::Float16 => {
-                ListExecutor::execute_binary_reduce::<PhysicalF16, _, L2DistanceReducer<_>>(
-                    a, b, builder,
-                )
-            }
-            PhysicalType::Float32 => {
-                ListExecutor::execute_binary_reduce::<PhysicalF32, _, L2DistanceReducer<_>>(
-                    a, b, builder,
-                )
-            }
-            PhysicalType::Float64 => {
-                ListExecutor::execute_binary_reduce::<PhysicalF64, _, L2DistanceReducer<_>>(
-                    a, b, builder,
-                )
-            }
-            other => panic!("unexpected physical type: {other:?}"),
-        }
+        ListExecutor::execute_binary_reduce::<S, _, L2DistanceReducer<_>>(a, b, builder)
     }
 }
 
