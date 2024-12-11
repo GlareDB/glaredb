d90c8bf63154378e485e69de51e25782e6a95e45
Sean Smith <scsmithr@gmail.com>
2024-12-11 11:17:16 -0600

    is

--- crates/rayexec_execution/src/functions/scalar/builtin/is.rs
+++ crates/rayexec_execution/src/functions/scalar/builtin/is.rs
@@ -5,8 +5,10 @@ use rayexec_bullet::executor::physical_type::{PhysicalAny, PhysicalBool};
 use rayexec_bullet::executor::scalar::UnaryExecutor;
 use rayexec_error::Result;
 
-use crate::functions::scalar::{PlannedScalarFunction2, ScalarFunction};
+use crate::expr::Expression;
+use crate::functions::scalar::{PlannedScalarFuntion, ScalarFunction, ScalarFunctionImpl};
 use crate::functions::{plan_check_num_args, FunctionInfo, Signature};
+use crate::logical::binder::table_list::TableList;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct IsNull;
@@ -26,13 +28,19 @@ impl FunctionInfo for IsNull {
 }
 
 impl ScalarFunction for IsNull {
-    fn decode_state(&self, _state: &[u8]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        Ok(Box::new(CheckNullImpl::<true>))
-    }
+    fn plan(
+        &self,
+        _table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedScalarFuntion> {
+        plan_check_num_args(self, &inputs, 1)?;
 
-    fn plan_from_datatypes(&self, inputs: &[DataType]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        plan_check_num_args(self, inputs, 1)?;
-        Ok(Box::new(CheckNullImpl::<true>))
+        Ok(PlannedScalarFuntion {
+            function: Box::new(*self),
+            return_type: DataType::Boolean,
+            inputs,
+            function_impl: Box::new(CheckNullImpl::<true>),
+        })
     }
 }
 
@@ -54,36 +62,26 @@ impl FunctionInfo for IsNotNull {
 }
 
 impl ScalarFunction for IsNotNull {
-    fn decode_state(&self, _state: &[u8]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        Ok(Box::new(CheckNullImpl::<false>))
-    }
+    fn plan(
+        &self,
+        _table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedScalarFuntion> {
+        plan_check_num_args(self, &inputs, 1)?;
 
-    fn plan_from_datatypes(&self, inputs: &[DataType]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        plan_check_num_args(self, inputs, 1)?;
-        Ok(Box::new(CheckNullImpl::<false>))
+        Ok(PlannedScalarFuntion {
+            function: Box::new(*self),
+            return_type: DataType::Boolean,
+            inputs,
+            function_impl: Box::new(CheckNullImpl::<false>),
+        })
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct CheckNullImpl<const IS_NULL: bool>;
 
-impl<const IS_NULL: bool> PlannedScalarFunction2 for CheckNullImpl<IS_NULL> {
-    fn scalar_function(&self) -> &dyn ScalarFunction {
-        if IS_NULL {
-            &IsNull
-        } else {
-            &IsNotNull
-        }
-    }
-
-    fn encode_state(&self, _state: &mut Vec<u8>) -> Result<()> {
-        Ok(())
-    }
-
-    fn return_type(&self) -> DataType {
-        DataType::Boolean
-    }
-
+impl<const IS_NULL: bool> ScalarFunctionImpl for CheckNullImpl<IS_NULL> {
     fn execute(&self, inputs: &[&Array]) -> Result<Array> {
         let input = inputs[0];
 
@@ -128,13 +126,19 @@ impl FunctionInfo for IsTrue {
 }
 
 impl ScalarFunction for IsTrue {
-    fn decode_state(&self, _state: &[u8]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        Ok(Box::new(CheckBoolImpl::<false, true>))
-    }
+    fn plan(
+        &self,
+        _table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedScalarFuntion> {
+        plan_check_num_args(self, &inputs, 1)?;
 
-    fn plan_from_datatypes(&self, inputs: &[DataType]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        plan_check_num_args(self, inputs, 1)?;
-        Ok(Box::new(CheckBoolImpl::<false, true>))
+        Ok(PlannedScalarFuntion {
+            function: Box::new(*self),
+            return_type: DataType::Boolean,
+            inputs,
+            function_impl: Box::new(CheckBoolImpl::<false, true>),
+        })
     }
 }
 
@@ -156,13 +160,19 @@ impl FunctionInfo for IsNotTrue {
 }
 
 impl ScalarFunction for IsNotTrue {
-    fn decode_state(&self, _state: &[u8]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        Ok(Box::new(CheckBoolImpl::<true, true>))
-    }
+    fn plan(
+        &self,
+        _table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedScalarFuntion> {
+        plan_check_num_args(self, &inputs, 1)?;
 
-    fn plan_from_datatypes(&self, inputs: &[DataType]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        plan_check_num_args(self, inputs, 1)?;
-        Ok(Box::new(CheckBoolImpl::<true, true>))
+        Ok(PlannedScalarFuntion {
+            function: Box::new(*self),
+            return_type: DataType::Boolean,
+            inputs,
+            function_impl: Box::new(CheckBoolImpl::<true, true>),
+        })
     }
 }
 
@@ -184,13 +194,19 @@ impl FunctionInfo for IsFalse {
 }
 
 impl ScalarFunction for IsFalse {
-    fn decode_state(&self, _state: &[u8]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        Ok(Box::new(CheckBoolImpl::<false, false>))
-    }
+    fn plan(
+        &self,
+        _table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedScalarFuntion> {
+        plan_check_num_args(self, &inputs, 1)?;
 
-    fn plan_from_datatypes(&self, inputs: &[DataType]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        plan_check_num_args(self, inputs, 1)?;
-        Ok(Box::new(CheckBoolImpl::<false, false>))
+        Ok(PlannedScalarFuntion {
+            function: Box::new(*self),
+            return_type: DataType::Boolean,
+            inputs,
+            function_impl: Box::new(CheckBoolImpl::<false, false>),
+        })
     }
 }
 
@@ -212,37 +228,26 @@ impl FunctionInfo for IsNotFalse {
 }
 
 impl ScalarFunction for IsNotFalse {
-    fn decode_state(&self, _state: &[u8]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        Ok(Box::new(CheckBoolImpl::<true, false>))
-    }
+    fn plan(
+        &self,
+        _table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedScalarFuntion> {
+        plan_check_num_args(self, &inputs, 1)?;
 
-    fn plan_from_datatypes(&self, inputs: &[DataType]) -> Result<Box<dyn PlannedScalarFunction2>> {
-        plan_check_num_args(self, inputs, 1)?;
-        Ok(Box::new(CheckBoolImpl::<true, false>))
+        Ok(PlannedScalarFuntion {
+            function: Box::new(*self),
+            return_type: DataType::Boolean,
+            inputs,
+            function_impl: Box::new(CheckBoolImpl::<true, false>),
+        })
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct CheckBoolImpl<const NOT: bool, const BOOL: bool>;
 
-impl<const NOT: bool, const BOOL: bool> PlannedScalarFunction2 for CheckBoolImpl<NOT, BOOL> {
-    fn scalar_function(&self) -> &dyn ScalarFunction {
-        match (NOT, BOOL) {
-            (false, true) => &IsTrue,
-            (true, true) => &IsNotTrue,
-            (false, false) => &IsFalse,
-            (true, false) => &IsNotFalse,
-        }
-    }
-
-    fn encode_state(&self, _state: &mut Vec<u8>) -> Result<()> {
-        Ok(())
-    }
-
-    fn return_type(&self) -> DataType {
-        DataType::Boolean
-    }
-
+impl<const NOT: bool, const BOOL: bool> ScalarFunctionImpl for CheckBoolImpl<NOT, BOOL> {
     fn execute(&self, inputs: &[&Array]) -> Result<Array> {
         let input = inputs[0];
 
