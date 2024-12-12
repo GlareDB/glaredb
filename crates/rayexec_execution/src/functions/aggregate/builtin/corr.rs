0d28d0f91c863ba98e5ea37b69e94307a10d45e5
Sean Smith <scsmithr@gmail.com>
2024-12-12 12:16:11 -0600

    corr

--- crates/rayexec_execution/src/functions/aggregate/builtin/corr.rs
+++ crates/rayexec_execution/src/functions/aggregate/builtin/corr.rs
@@ -1,22 +1,22 @@
 use std::fmt::Debug;
 
-use rayexec_bullet::array::Array;
 use rayexec_bullet::datatype::{DataType, DataTypeId};
-use rayexec_bullet::executor::aggregate::{AggregateState, BinaryNonNullUpdater};
+use rayexec_bullet::executor::aggregate::AggregateState;
 use rayexec_bullet::executor::physical_type::PhysicalF64;
 use rayexec_error::Result;
 
 use super::covar::{CovarPopFinalize, CovarState};
 use super::stddev::{StddevPopFinalize, VarianceState};
+use crate::expr::Expression;
+use crate::functions::aggregate::states::{new_binary_aggregate_states, AggregateGroupStates};
 use crate::functions::aggregate::{
     primitive_finalize,
     AggregateFunction,
-    ChunkGroupAddressIter,
-    DefaultGroupedStates,
-    GroupedStates,
-    PlannedAggregateFunction2,
+    AggregateFunctionImpl,
+    PlannedAggregateFunction,
 };
 use crate::functions::{invalid_input_types_error, plan_check_num_args, FunctionInfo, Signature};
+use crate::logical::binder::table_list::TableList;
 
 /// Pearson coefficient, population.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -37,17 +37,23 @@ impl FunctionInfo for Corr {
 }
 
 impl AggregateFunction for Corr {
-    fn decode_state(&self, _state: &[u8]) -> Result<Box<dyn PlannedAggregateFunction2>> {
-        Ok(Box::new(CorrImpl))
-    }
-
-    fn plan_from_datatypes(
+    fn plan(
         &self,
-        inputs: &[DataType],
-    ) -> Result<Box<dyn PlannedAggregateFunction2>> {
-        plan_check_num_args(self, inputs, 2)?;
-        match (&inputs[0], &inputs[1]) {
-            (DataType::Float64, DataType::Float64) => Ok(Box::new(CorrImpl)),
+        table_list: &TableList,
+        inputs: Vec<Expression>,
+    ) -> Result<PlannedAggregateFunction> {
+        plan_check_num_args(self, &inputs, 2)?;
+
+        match (
+            inputs[0].datatype(table_list)?,
+            inputs[1].datatype(table_list)?,
+        ) {
+            (DataType::Float64, DataType::Float64) => Ok(PlannedAggregateFunction {
+                function: Box::new(*self),
+                return_type: DataType::Float64,
+                inputs,
+                function_impl: Box::new(CorrImpl),
+            }),
             (a, b) => Err(invalid_input_types_error(self, &[a, b])),
         }
     }
@@ -56,37 +62,12 @@ impl AggregateFunction for Corr {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct CorrImpl;
 
-impl PlannedAggregateFunction2 for CorrImpl {
-    fn aggregate_function(&self) -> &dyn AggregateFunction {
-        &Corr
-    }
-
-    fn encode_state(&self, _state: &mut Vec<u8>) -> Result<()> {
-        Ok(())
-    }
-
-    fn return_type(&self) -> DataType {
-        DataType::Float64
-    }
-
-    fn new_grouped_state(&self) -> Result<Box<dyn GroupedStates>> {
-        let datatype = self.return_type();
-
-        fn update(
-            arrays: &[&Array],
-            mapping: ChunkGroupAddressIter,
-            states: &mut [CorrelationState],
-        ) -> Result<()> {
-            BinaryNonNullUpdater::update::<PhysicalF64, PhysicalF64, _, _, _>(
-                arrays[0], arrays[1], mapping, states,
-            )
-        }
-
-        Ok(Box::new(DefaultGroupedStates::new(
+impl AggregateFunctionImpl for CorrImpl {
+    fn new_states(&self) -> Box<dyn AggregateGroupStates> {
+        new_binary_aggregate_states::<PhysicalF64, PhysicalF64, _, _, _, _>(
             CorrelationState::default,
-            update,
-            move |states| primitive_finalize(datatype.clone(), states),
-        )))
+            move |states| primitive_finalize(DataType::Float64, states),
+        )
     }
 }
 
