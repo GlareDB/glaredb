125c38f93384b886d0404b555790f2d9e3480a01
Sean Smith <scsmithr@gmail.com>
2024-12-14 10:09:17 -0600

    logical inout

---
+++ crates/rayexec_execution/src/logical/logical_inout.rs
@@ -0,0 +1,49 @@
+use rayexec_error::Result;
+
+use super::binder::bind_context::BindContext;
+use super::binder::table_list::TableRef;
+use super::operator::{LogicalNode, Node};
+use crate::explain::explainable::{ExplainConfig, ExplainEntry, Explainable};
+use crate::expr::Expression;
+use crate::functions::table::PlannedTableFunction;
+
+/// A table function that accepts inputs and produces outputs.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct LogicalInOut {
+    /// Table ref for referencing the output of this function.
+    pub table_ref: TableRef,
+    /// The table function.
+    pub function: PlannedTableFunction,
+}
+
+impl Explainable for LogicalInOut {
+    fn explain_entry(&self, _conf: ExplainConfig) -> ExplainEntry {
+        ExplainEntry::new("TableInOut").with_value("function", self.function.function.name())
+    }
+}
+
+impl LogicalNode for Node<LogicalInOut> {
+    fn get_output_table_refs(&self, _bind_context: &BindContext) -> Vec<TableRef> {
+        vec![self.node.table_ref]
+    }
+
+    fn for_each_expr<F>(&self, func: &mut F) -> Result<()>
+    where
+        F: FnMut(&Expression) -> Result<()>,
+    {
+        for expr in &self.node.function.positional_inputs {
+            func(expr)?
+        }
+        Ok(())
+    }
+
+    fn for_each_expr_mut<F>(&mut self, func: &mut F) -> Result<()>
+    where
+        F: FnMut(&mut Expression) -> Result<()>,
+    {
+        for expr in &mut self.node.function.positional_inputs {
+            func(expr)?
+        }
+        Ok(())
+    }
+}
