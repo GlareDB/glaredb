df00e2a16043298a89f5bd0432f43f7daff5b74c
Sean Smith <scsmithr@gmail.com>
2024-12-16 11:52:17 -0600

    initial impl

--- crates/rayexec_execution/src/functions/table/builtin/mod.rs
+++ crates/rayexec_execution/src/functions/table/builtin/mod.rs
@@ -1,16 +1,19 @@
 pub mod series;
 pub mod system;
+pub mod unnest;
 
 use std::sync::LazyLock;
 
 use series::GenerateSeries;
 use system::{ListDatabases, ListFunctions, ListSchemas, ListTables};
+use unnest::Unnest;
 
 use super::TableFunction;
 
 pub static BUILTIN_TABLE_FUNCTIONS: LazyLock<Vec<Box<dyn TableFunction>>> = LazyLock::new(|| {
     vec![
         Box::new(GenerateSeries),
+        Box::new(Unnest),
         // Various list system object functions.
         Box::new(ListDatabases::new()),
         Box::new(ListSchemas::new()),
