11520aeb29f9c87a648ec99c529db4f6362dd3cc
Sean Smith <scsmithr@gmail.com>
2024-12-29 16:56:14 -0500

    epoch

--- crates/rayexec_execution/src/functions/scalar/builtin/datetime/epoch.rs
+++ crates/rayexec_execution/src/functions/scalar/builtin/datetime/epoch.rs
@@ -1,10 +1,12 @@
+use iterutil::IntoExactSizeIterator;
 use rayexec_error::Result;
 
-use crate::arrays::array::Array2;
+use crate::arrays::array::exp::Array;
+use crate::arrays::batch_exp::Batch;
+use crate::arrays::buffer::physical_type::PhysicalI64;
 use crate::arrays::datatype::{DataType, DataTypeId, TimeUnit, TimestampTypeMeta};
-use crate::arrays::executor::builder::{ArrayBuilder, PrimitiveBuffer};
-use crate::arrays::executor::physical_type::PhysicalI64_2;
-use crate::arrays::executor::scalar::UnaryExecutor2;
+use crate::arrays::executor_exp::scalar::unary::UnaryExecutor;
+use crate::arrays::executor_exp::OutBuffer;
 use crate::expr::Expression;
 use crate::functions::scalar::{PlannedScalarFunction, ScalarFunction, ScalarFunctionImpl};
 use crate::functions::{invalid_input_types_error, plan_check_num_args, FunctionInfo, Signature};
@@ -102,21 +104,22 @@ impl ScalarFunction for EpochMs {
 pub struct EpochImpl<const S: i64>;
 
 impl<const S: i64> ScalarFunctionImpl for EpochImpl<S> {
-    fn execute2(&self, inputs: &[&Array2]) -> Result<Array2> {
-        let input = inputs[0];
-        to_timestamp::<S>(input)
+    fn execute(&self, input: &Batch, output: &mut Array) -> Result<()> {
+        let sel = input.selection();
+        let input = &input.arrays()[0];
+        to_timestamp::<S>(input, sel, output)
     }
 }
 
-fn to_timestamp<const S: i64>(input: &Array2) -> Result<Array2> {
-    let builder = ArrayBuilder {
-        datatype: DataType::Timestamp(TimestampTypeMeta {
-            unit: TimeUnit::Microsecond,
-        }),
-        buffer: PrimitiveBuffer::with_len(input.logical_len()),
-    };
-
-    UnaryExecutor2::execute::<PhysicalI64_2, _, _>(input, builder, |v, buf| {
-        buf.put(&(v * S));
-    })
+fn to_timestamp<const S: i64>(
+    input: &Array,
+    sel: impl IntoExactSizeIterator<Item = usize>,
+    out: &mut Array,
+) -> Result<()> {
+    UnaryExecutor::execute::<PhysicalI64, PhysicalI64, _>(
+        input,
+        sel,
+        OutBuffer::from_array(out)?,
+        |&v, buf| buf.put(&(v * S)),
+    )
 }
