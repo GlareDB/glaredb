85cd55dc4aa3ebe2412095c8fcc49ffd34acf9f1
Sean Smith <scsmithr@gmail.com>
2024-12-31 11:47:08 -0500

    list extract

--- crates/rayexec_execution/src/functions/scalar/builtin/list/list_extract.rs
+++ crates/rayexec_execution/src/functions/scalar/builtin/list/list_extract.rs
@@ -1,41 +1,36 @@
-use std::borrow::Borrow;
-
-use half::f16;
+use iterutil::IntoExactSizeIterator;
 use rayexec_error::{not_implemented, RayexecError, Result};
-use serde::{Deserialize, Serialize};
 
-use crate::arrays::array::{Array2, ArrayData2};
-use crate::arrays::bitmap::Bitmap;
+use crate::arrays::array::exp::Array;
+use crate::arrays::batch_exp::Batch;
+use crate::arrays::buffer::physical_type::{
+    Addressable,
+    AddressableMut,
+    MutablePhysicalStorage,
+    PhysicalBinary,
+    PhysicalBool,
+    PhysicalF16,
+    PhysicalF32,
+    PhysicalF64,
+    PhysicalI128,
+    PhysicalI16,
+    PhysicalI32,
+    PhysicalI64,
+    PhysicalI8,
+    PhysicalInterval,
+    PhysicalList,
+    PhysicalStorage,
+    PhysicalType,
+    PhysicalU128,
+    PhysicalU16,
+    PhysicalU32,
+    PhysicalU64,
+    PhysicalU8,
+    PhysicalUntypedNull,
+    PhysicalUtf8,
+};
+use crate::arrays::buffer::SecondaryBuffer;
 use crate::arrays::datatype::{DataType, DataTypeId};
-use crate::arrays::executor::builder::{
-    ArrayBuilder,
-    ArrayDataBuffer,
-    BooleanBuffer,
-    GermanVarlenBuffer,
-    PrimitiveBuffer,
-};
-use crate::arrays::executor::physical_type::{
-    PhysicalBinary_2,
-    PhysicalBool_2,
-    PhysicalF16_2,
-    PhysicalF32_2,
-    PhysicalF64_2,
-    PhysicalI128_2,
-    PhysicalI16_2,
-    PhysicalI32_2,
-    PhysicalI64_2,
-    PhysicalI8_2,
-    PhysicalList_2,
-    PhysicalStorage2,
-    PhysicalType2,
-    PhysicalU128_2,
-    PhysicalU16_2,
-    PhysicalU32_2,
-    PhysicalU64_2,
-    PhysicalU8_2,
-    PhysicalUtf8_2,
-};
-use crate::arrays::executor::scalar::UnaryExecutor2;
 use crate::expr::Expression;
 use crate::functions::documentation::{Category, Documentation, Example};
 use crate::functions::scalar::{PlannedScalarFunction, ScalarFunction, ScalarFunctionImpl};
@@ -105,194 +100,109 @@ impl ScalarFunction for ListExtract {
             function: Box::new(*self),
             return_type: inner_datatype.clone(),
             inputs,
-            function_impl: Box::new(ListExtractImpl {
-                index,
-                inner_datatype,
-            }),
+            function_impl: Box::new(ListExtractImpl { index }),
         })
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ListExtractImpl {
-    inner_datatype: DataType,
     index: usize,
 }
 
 impl ScalarFunctionImpl for ListExtractImpl {
-    fn execute2(&self, inputs: &[&Array2]) -> Result<Array2> {
-        let input = inputs[0];
-        extract(input, self.index)
+    fn execute(&self, input: &Batch, output: &mut Array) -> Result<()> {
+        let sel = input.selection();
+        let input = &input.arrays()[0];
+        list_extract(input, sel, output, self.index)
     }
 }
 
-fn extract(array: &Array2, idx: usize) -> Result<Array2> {
-    let data = match array.array_data() {
-        ArrayData2::List(list) => list.as_ref(),
-        _other => return Err(RayexecError::new("Unexpected storage type")),
+/// Extract an element from each list within a list array.
+///
+/// If the element index falls outside the bounds of a list, the result for that
+/// row will be NULL.
+pub fn list_extract(
+    array: &Array,
+    sel: impl IntoExactSizeIterator<Item = usize>,
+    output: &mut Array,
+    element_idx: usize,
+) -> Result<()> {
+    match output.datatype().physical_type() {
+        PhysicalType::UntypedNull => {
+            extract_inner::<PhysicalUntypedNull>(array, sel, output, element_idx)
+        }
+        PhysicalType::Boolean => extract_inner::<PhysicalBool>(array, sel, output, element_idx),
+        PhysicalType::Int8 => extract_inner::<PhysicalI8>(array, sel, output, element_idx),
+        PhysicalType::Int16 => extract_inner::<PhysicalI16>(array, sel, output, element_idx),
+        PhysicalType::Int32 => extract_inner::<PhysicalI32>(array, sel, output, element_idx),
+        PhysicalType::Int64 => extract_inner::<PhysicalI64>(array, sel, output, element_idx),
+        PhysicalType::Int128 => extract_inner::<PhysicalI128>(array, sel, output, element_idx),
+        PhysicalType::UInt8 => extract_inner::<PhysicalU8>(array, sel, output, element_idx),
+        PhysicalType::UInt16 => extract_inner::<PhysicalU16>(array, sel, output, element_idx),
+        PhysicalType::UInt32 => extract_inner::<PhysicalU32>(array, sel, output, element_idx),
+        PhysicalType::UInt64 => extract_inner::<PhysicalU64>(array, sel, output, element_idx),
+        PhysicalType::UInt128 => extract_inner::<PhysicalU128>(array, sel, output, element_idx),
+        PhysicalType::Float16 => extract_inner::<PhysicalF16>(array, sel, output, element_idx),
+        PhysicalType::Float32 => extract_inner::<PhysicalF32>(array, sel, output, element_idx),
+        PhysicalType::Float64 => extract_inner::<PhysicalF64>(array, sel, output, element_idx),
+        PhysicalType::Interval => {
+            extract_inner::<PhysicalInterval>(array, sel, output, element_idx)
+        }
+        PhysicalType::Utf8 => extract_inner::<PhysicalUtf8>(array, sel, output, element_idx),
+        PhysicalType::Binary => extract_inner::<PhysicalBinary>(array, sel, output, element_idx),
+        other => not_implemented!("List extract for datatype {other}"),
+    }
+}
+
+fn extract_inner<S>(
+    array: &Array,
+    sel: impl IntoExactSizeIterator<Item = usize>,
+    output: &mut Array,
+    element_idx: usize,
+) -> Result<()>
+where
+    S: MutablePhysicalStorage,
+{
+    let flat = array.flat_view()?;
+
+    let metas = PhysicalList::get_addressable(&flat.array_buffer)?;
+    let child = match flat.array_buffer.get_secondary() {
+        SecondaryBuffer::List(l) => &l.child,
+        _ => return Err(RayexecError::new("Missing secondary buffer for list")),
     };
 
-    match data.inner_array().physical_type() {
-        PhysicalType2::UntypedNull => not_implemented!("NULL list extract"),
-        PhysicalType2::Boolean => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Boolean,
-                buffer: BooleanBuffer::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalBool_2, _>(builder, array, data.inner_array(), idx)
+    let child_buf = S::get_addressable(child.data())?;
+    let child_validity = child.validity();
+
+    let mut out_buffer = S::get_addressable_mut(output.data.try_as_mut()?)?;
+    let out_validity = &mut output.validity;
+
+    for (output_idx, input_idx) in sel.into_iter().enumerate() {
+        let sel_idx = flat.selection.get(input_idx).unwrap();
+
+        if flat.validity.is_valid(sel_idx) {
+            let meta = metas.get(sel_idx).unwrap();
+            if element_idx >= meta.len as usize {
+                // Indexing outside of the list. User is allowed to do that, set
+                // the value to null.
+                out_validity.set_invalid(output_idx);
+                continue;
+            }
+
+            let offset = meta.offset as usize + element_idx;
+            if !child_validity.is_valid(offset) {
+                // Element inside list is null.
+                out_validity.set_invalid(output_idx);
+                continue;
+            }
+
+            let val = child_buf.get(offset).unwrap();
+            out_buffer.put(output_idx, val);
+        } else {
+            out_validity.set_invalid(output_idx);
         }
-        PhysicalType2::Int8 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Int8,
-                buffer: PrimitiveBuffer::<i8>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalI8_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::Int16 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Int16,
-                buffer: PrimitiveBuffer::<i16>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalI16_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::Int32 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Int32,
-                buffer: PrimitiveBuffer::<i32>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalI32_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::Int64 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Int64,
-                buffer: PrimitiveBuffer::<i64>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalI64_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::Int128 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Int128,
-                buffer: PrimitiveBuffer::<i128>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalI128_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::UInt8 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::UInt8,
-                buffer: PrimitiveBuffer::<u8>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalU8_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::UInt16 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::UInt16,
-                buffer: PrimitiveBuffer::<u16>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalU16_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::UInt32 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::UInt32,
-                buffer: PrimitiveBuffer::<u32>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalU32_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::UInt64 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::UInt64,
-                buffer: PrimitiveBuffer::<u64>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalU64_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::UInt128 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::UInt128,
-                buffer: PrimitiveBuffer::<u128>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalU128_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::Float16 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Float16,
-                buffer: PrimitiveBuffer::<f16>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalF16_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::Float32 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Float32,
-                buffer: PrimitiveBuffer::<f32>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalF32_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::Float64 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Float64,
-                buffer: PrimitiveBuffer::<f64>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalF64_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::Utf8 => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Utf8,
-                buffer: GermanVarlenBuffer::<str>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalUtf8_2, _>(builder, array, data.inner_array(), idx)
-        }
-        PhysicalType2::Binary => {
-            let builder = ArrayBuilder {
-                datatype: DataType::Binary,
-                buffer: GermanVarlenBuffer::<[u8]>::with_len(array.logical_len()),
-            };
-            extract_inner::<PhysicalBinary_2, _>(builder, array, data.inner_array(), idx)
-        }
-        other => not_implemented!("List extract for physical type {other:?}"),
     }
-}
 
-fn extract_inner<'a, S, B>(
-    mut builder: ArrayBuilder<B>,
-    outer: &Array2,
-    inner: &'a Array2,
-    el_idx: usize,
-) -> Result<Array2>
-where
-    S: PhysicalStorage2,
-    B: ArrayDataBuffer,
-    S::Type<'a>: Borrow<<B as ArrayDataBuffer>::Type>,
-{
-    let el_idx = el_idx as i32;
-
-    let mut validity = Bitmap::new_with_all_true(builder.buffer.len());
-
-    UnaryExecutor2::for_each::<PhysicalList_2, _>(outer, |idx, metadata| {
-        if let Some(metadata) = metadata {
-            if el_idx >= metadata.len {
-                // Indexing outside of the list. Mark null
-                validity.set_unchecked(idx, false);
-                return;
-            }
-
-            // Otherwise put the element into the builder.
-            let inner_el_idx = metadata.offset + el_idx;
-            match UnaryExecutor2::value_at::<S>(inner, inner_el_idx as usize) {
-                Ok(Some(el)) => {
-                    builder.buffer.put(idx, el.borrow());
-                    return;
-                }
-                _ => {
-                    // TODO: Do something if Err, just fall through right now.
-                }
-            }
-        }
-
-        // Metadata null, tried to extract from null array, mark null.
-        validity.set_unchecked(idx, false);
-    })?;
-
-    Ok(Array2::new_with_validity_and_array_data(
-        builder.datatype,
-        validity,
-        builder.buffer.into_data(),
-    ))
+    Ok(())
 }
