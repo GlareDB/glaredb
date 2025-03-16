af02ea7b1d12de5a1ccb3f665d01e1910d7f7858
Sean Smith <scsmithr@gmail.com>
2025-03-16 15:53:25 -0500

    rayexec_bench -> glaredb_bench

--- bench_bin/bench_standard.rs
+++ bench_bin/bench_standard.rs
@@ -1,8 +1,8 @@
+use glaredb_bench::DefaultEngineBuilder;
 use glaredb_error::Result;
-use rayexec_bench::DefaultEngineBuilder;
 
 pub fn main() -> Result<()> {
     let builder = DefaultEngineBuilder::try_new()?;
-    rayexec_bench::run(builder, "./bench/standard")?;
+    glaredb_bench::run(builder, "./bench/standard")?;
     Ok(())
 }
