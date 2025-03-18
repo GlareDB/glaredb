1eb11ddd148c77f786bf5b1fcf13ef2d19e3c599
Sean Smith <scsmithr@gmail.com>
2025-03-18 14:05:09 -0500

    chore: Bump packages to 0.0.98

--- crates/glaredb_execution/src/shell/shell.rs
+++ crates/glaredb_execution/src/shell/shell.rs
@@ -5,7 +5,7 @@ use tracing::trace;
 
 use super::lineedit::{KeyEvent, LineEditor, Signal, TermSize};
 use super::raw::RawTerminalWriter;
-use super::vt100::{MODE_BOLD, MODES_OFF};
+use super::vt100::{MODES_OFF, MODE_BOLD};
 use crate::arrays::format::pretty::table::PrettyTable;
 use crate::engine::single_user::SingleUserEngine;
 use crate::runtime::{PipelineExecutor, Runtime};
@@ -90,7 +90,7 @@ where
         let mut writer = RawTerminalWriter::new(self.editor.writer_mut());
         writeln!(writer, "{}{shell_msg}{}", MODE_BOLD, MODES_OFF)?;
         let version = env!("CARGO_PKG_VERSION");
-        writeln!(writer, "Preview ({version}) - There will be bugs!")?;
+        writeln!(writer, "Preview ({version})")?;
 
         Ok(())
     }
