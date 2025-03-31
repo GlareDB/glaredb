7690787a851d7eb281dffad19ffd4ca738fc58e3
Sean Smith <scsmithr@gmail.com>
2025-03-31 11:12:42 -0500

    stub cast function trait

--- crates/glaredb_core/src/functions/cast/mod.rs
+++ crates/glaredb_core/src/functions/cast/mod.rs
@@ -2,3 +2,21 @@ pub mod array;
 pub mod behavior;
 pub mod format;
 pub mod parse;
+
+use std::fmt::Debug;
+
+use glaredb_error::Result;
+
+use crate::arrays::array::Array;
+use crate::arrays::datatype::DataType;
+
+pub trait CastFunction: Copy + Debug + Sync + Send + Sized + 'static {
+    type State: Sync + Send;
+
+    /// Binds this cast function, returning any state needed to execute the
+    /// cast.
+    fn bind(src: &DataType, target: &DataType) -> Result<Self::State>;
+
+    /// Cast `src` to the target type, writing the results to `out`.
+    fn cast(src: &Array, out: &mut Array) -> Result<()>;
+}
