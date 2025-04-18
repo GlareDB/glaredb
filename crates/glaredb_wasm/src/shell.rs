66cdae569bbd05e4e7373b5b2381512edd7b12d4
Sean Smith <scsmithr@gmail.com>
2025-04-18 18:53:59 -0500

    fix: Handle paste on rust side for wasm
    Less jank

--- crates/glaredb_wasm/src/shell.rs
+++ crates/glaredb_wasm/src/shell.rs
@@ -4,11 +4,12 @@ use std::rc::Rc;
 
 use glaredb_core::shell::lineedit::{KeyEvent, TermSize};
 use glaredb_core::shell::{RawModeGuard, RawModeTerm, Shell, ShellSignal};
+use glaredb_error::{DbError, OptionExt};
 use js_sys::Function;
 use tracing::{error, trace, warn};
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::spawn_local;
-use web_sys::KeyboardEvent;
+use wasm_bindgen_futures::{JsFuture, spawn_local};
+use web_sys::{KeyboardEvent, window};
 
 use crate::errors::Result;
 use crate::runtime::{WasmExecutor, WasmSystemRuntime};
@@ -110,27 +111,7 @@ impl io::Write for TerminalBuffer {
 #[allow(dead_code)]
 pub struct WasmShell {
     /// Shareable inner logic for the shell.
-    pub(crate) inner: Rc<RefCell<WasmShellInner>>,
-}
-
-#[derive(Debug)]
-pub(crate) struct WasmShellInner {
-    shell: Shell<TerminalBuffer, WasmExecutor, WasmSystemRuntime, NopRawMode>,
-    edit_guard: Option<WasmEditGuard>,
-}
-
-type WasmEditGuard = RawModeGuard<NopRawMode>;
-
-/// Implementation of raw mode term that does nothing.
-///
-/// xterm.js is essentially always in raw mode, there no notion of "cooked" mode
-/// for it, so there's no state for use to return to.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub(crate) struct NopRawMode;
-
-impl RawModeTerm for NopRawMode {
-    fn enable_raw_mode(&self) {}
-    fn disable_raw_mode(&self) {}
+    pub(crate) inner: Rc<RefCell<ShellHandler>>,
 }
 
 #[wasm_bindgen]
@@ -150,7 +131,7 @@ impl WasmShell {
 
         let edit_guard = shell.edit_start()?;
 
-        let inner = WasmShellInner {
+        let inner = ShellHandler {
             shell,
             edit_guard: Some(edit_guard),
         };
@@ -169,10 +150,8 @@ impl WasmShell {
     }
 
     /// Consume a string verbatim.
-    ///
-    /// This should be hooked up to xterm's `onData` method to enable pasting.
     pub fn on_data(&self, text: String) -> Result<()> {
-        trace!("consuming data");
+        trace!(%text, "consuming data");
         // TODO: Not sure what we want to do yet if this errors.
         if let Ok(mut inner) = self.inner.try_borrow_mut() {
             inner.shell.consume_text(&text)?;
@@ -186,14 +165,38 @@ impl WasmShell {
     /// will handle _all_ input, so `false` should be returned to xterm after
     /// calling this for an event.
     pub fn on_key(&self, event: KeyboardEvent) -> Result<()> {
-        // Pulled out into a separate method since the procmacro doesn't handle
-        // active code editing very well.
-        self.on_key_inner(event)
+        ShellHandler::on_key(self.inner.clone(), event)
     }
 }
 
-impl WasmShell {
-    fn on_key_inner(&self, event: KeyboardEvent) -> Result<()> {
+#[derive(Debug)]
+pub(crate) struct ShellHandler {
+    shell: Shell<TerminalBuffer, WasmExecutor, WasmSystemRuntime, NopRawMode>,
+    edit_guard: Option<WasmEditGuard>,
+}
+
+type WasmEditGuard = RawModeGuard<NopRawMode>;
+
+/// Implementation of raw mode term that does nothing.
+///
+/// xterm.js is essentially always in raw mode, there no notion of "cooked" mode
+/// for it, so there's no state for use to return to.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub(crate) struct NopRawMode;
+
+impl RawModeTerm for NopRawMode {
+    fn enable_raw_mode(&self) {}
+    fn disable_raw_mode(&self) {}
+}
+
+#[derive(Debug)]
+enum ShellHandlerInput {
+    Key(KeyEvent),
+    Text(String),
+}
+
+impl ShellHandler {
+    fn on_key(handler: Rc<RefCell<Self>>, event: KeyboardEvent) -> Result<()> {
         if event.type_() != "keydown" {
             return Ok(());
         }
@@ -213,19 +216,27 @@ impl WasmShell {
             }
             other => match other.chars().next() {
                 Some(ch) => {
-                    if event.ctrl_key() {
-                        match ch {
-                            'c' => KeyEvent::CtrlC,
-                            other => {
-                                warn!(%other, "unhandled input with ctrl modifier");
-                                return Ok(());
-                            }
+                    match ch {
+                        'v' if event.meta_key() || event.ctrl_key() => {
+                            // Handle both 'ctrl-v' and 'cmd-v' as paste to
+                            // avoid needing to try to detect os.
+                            ShellHandler::handle_paste(handler);
+                            return Ok(());
                         }
-                    } else if event.meta_key() {
-                        warn!(%other, "unhandled input with meta modifier");
-                        return Ok(());
-                    } else {
-                        KeyEvent::Char(ch)
+                        'c' if event.ctrl_key() => {
+                            // TODO: Not sure if this fine since it'll prevent
+                            // non-mac users from copying using a shortcut.
+                            KeyEvent::CtrlC
+                        }
+                        _ch if event.meta_key() => {
+                            warn!(%other, "unhandled input with meta modifier");
+                            return Ok(());
+                        }
+                        _ch if event.ctrl_key() => {
+                            warn!(%other, "unhandled input with ctrl modifier");
+                            return Ok(());
+                        }
+                        ch => KeyEvent::Char(ch),
                     }
                 }
                 None => {
@@ -235,7 +246,30 @@ impl WasmShell {
             },
         };
 
-        let mut inner = match self.inner.try_borrow_mut() {
+        ShellHandler::handle_input(handler, ShellHandlerInput::Key(key))
+    }
+
+    /// Handles paste by reading from the window's clipboard.
+    fn handle_paste(handler: Rc<RefCell<Self>>) {
+        async fn inner(handler: Rc<RefCell<ShellHandler>>) -> Result<()> {
+            let window = window().required("Global window object")?;
+            let clipboard = window.navigator().clipboard();
+            let js_val = JsFuture::from(clipboard.read_text())
+                .await
+                .map_err(|_| DbError::new("Failed to read from the clipboard"))?;
+            let text = js_val.as_string().unwrap();
+            ShellHandler::handle_input(handler, ShellHandlerInput::Text(text))
+        }
+
+        spawn_local(async move {
+            if let Err(e) = inner(handler).await {
+                warn!(%e, "failed to handle paste");
+            }
+        });
+    }
+
+    fn handle_input(handler: Rc<RefCell<Self>>, input: ShellHandlerInput) -> Result<()> {
+        let mut inner = match handler.try_borrow_mut() {
             Ok(inner) => inner,
             Err(_) => {
                 // TODO: Not sure what to do yet.
@@ -244,64 +278,73 @@ impl WasmShell {
             }
         };
 
-        let guard = match inner.edit_guard.take() {
-            Some(guard) => guard,
-            None => {
-                // TODO: ???
-                warn!("missing post execute guard");
-                return Ok(());
+        match input {
+            ShellHandlerInput::Text(text) => {
+                // TODO: This should take the guard possibly.
+                inner.shell.consume_text(&text)?;
+                Ok(())
             }
-        };
+            ShellHandlerInput::Key(key) => {
+                let guard = match inner.edit_guard.take() {
+                    Some(guard) => guard,
+                    None => {
+                        // TODO: ???
+                        warn!("missing post execute guard");
+                        return Ok(());
+                    }
+                };
 
-        match inner.shell.consume_key(guard, key)? {
-            ShellSignal::Continue(guard) => {
-                inner.edit_guard = Some(guard);
-                // Continue with normal editing.
-            }
-            ShellSignal::ExecutePending(guard) => {
-                // Clone inner, we'll get a new mutable ref in the spawn.
-                //
-                // Needed since the spawn closure needs to be static.
-                let inner = self.inner.clone();
+                match inner.shell.consume_key(guard, key)? {
+                    ShellSignal::Continue(guard) => {
+                        inner.edit_guard = Some(guard);
+                        // Continue with normal editing.
+                    }
+                    ShellSignal::ExecutePending(guard) => {
+                        // Clone handler, we'll get a new mutable ref in the spawn.
+                        //
+                        // Needed since the spawn closure needs to be static.
+                        let handler = handler.clone();
 
-                spawn_local(
-                    // Holding the ref through the await here is fine, it
-                    // essentially acts as a lock preventing additional input to
-                    // the shell.
-                    //
-                    // TODO: We will want a way to cancel a running query
-                    // though. That'll probably happen through a separate
-                    // rc+refcell bool.
-                    #[allow(clippy::await_holding_refcell_ref)]
-                    async move {
-                        let mut inner = match inner.try_borrow_mut() {
-                            Ok(inner) => inner,
-                            Err(e) => {
-                                error!(%e, "error getting mut ref to inner for query execute");
-                                return;
-                            }
-                        };
+                        spawn_local(
+                            // Holding the ref through the await here is fine, it
+                            // essentially acts as a lock preventing additional input to
+                            // the shell.
+                            //
+                            // TODO: We will want a way to cancel a running query
+                            // though. That'll probably happen through a separate
+                            // rc+refcell bool.
+                            #[allow(clippy::await_holding_refcell_ref)]
+                            async move {
+                                let mut inner = match handler.try_borrow_mut() {
+                                    Ok(inner) => inner,
+                                    Err(e) => {
+                                        error!(%e, "error getting mut ref to inner for query execute");
+                                        return;
+                                    }
+                                };
 
-                        if let Err(e) = inner.shell.execute_pending(guard).await {
-                            error!(%e, "error executing pending query");
-                        }
+                                if let Err(e) = inner.shell.execute_pending(guard).await {
+                                    error!(%e, "error executing pending query");
+                                }
 
-                        match inner.shell.edit_start() {
-                            Ok(guard) => inner.edit_guard = Some(guard),
-                            Err(e) => {
-                                // Nothing we can really do.
-                                error!(%e, "error calling edit start for shell");
-                            }
-                        }
-                    },
-                );
-            }
-            ShellSignal::Exit => {
-                // Can't exit out of the web shell.
-                inner.edit_guard = Some(inner.shell.edit_start()?);
+                                match inner.shell.edit_start() {
+                                    Ok(guard) => inner.edit_guard = Some(guard),
+                                    Err(e) => {
+                                        // Nothing we can really do.
+                                        error!(%e, "error calling edit start for shell");
+                                    }
+                                }
+                            },
+                        );
+                    }
+                    ShellSignal::Exit => {
+                        // Can't exit out of the web shell.
+                        inner.edit_guard = Some(inner.shell.edit_start()?);
+                    }
+                }
+
+                Ok(())
             }
         }
-
-        Ok(())
     }
 }
