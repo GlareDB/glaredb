6f1b530dd8d0aa2e508629310a57a4d32aeb112d
Sean Smith <scsmithr@gmail.com>
2025-04-17 20:05:59 -0500

    should work

--- crates/ext_parquet/src/column/encoding/delta_byte_array.rs
+++ crates/ext_parquet/src/column/encoding/delta_byte_array.rs
@@ -1,19 +1,64 @@
 use glaredb_core::arrays::array::Array;
-use glaredb_core::arrays::array::physical_type::{MutableScalarStorage, PhysicalBinary};
-use glaredb_error::Result;
+use glaredb_core::arrays::array::physical_type::{
+    AddressableMut,
+    MutableScalarStorage,
+    PhysicalBinary,
+};
+use glaredb_core::buffer::buffer_manager::NopBufferManager;
+use glaredb_core::buffer::typed::TypedBuffer;
+use glaredb_error::{DbError, Result, ResultExt};
 
 use super::Definitions;
+use crate::column::encoding::delta_binary_packed::DeltaBinaryPackedValueDecoder;
 use crate::column::read_buffer::ReadCursor;
 
 #[derive(Debug)]
 pub struct DeltaByteArrayDecoder {
     /// If we should verify that the bytes we read is valid utf8.
     verify_utf8: bool,
+    /// Most recent value we've read.
+    ///
+    /// For each index, we truncate this to the prefix len, then extend with the
+    /// next 'suffix' bytes.
+    ///
+    /// Vec since that has a nicer API for this.
+    val_buf: Vec<u8>,
+    /// Index of the current prefix/suffix length we're on.
+    curr_len_idx: usize,
+    /// Decoded prefix lengths.
+    prefix_lengths: TypedBuffer<i32>,
+    /// Decoded suffix lengths.
+    suffix_lengths: TypedBuffer<i32>,
+    /// Cursor pointing to the current byte offset.
+    cursor: ReadCursor,
 }
 
 impl DeltaByteArrayDecoder {
     pub fn try_new(cursor: ReadCursor, num_values: usize, verify_utf8: bool) -> Result<Self> {
-        unimplemented!()
+        // Read delta binary packed lengths.
+        let read_lengths = |cursor| -> Result<(_, _)> {
+            // TODO: Not Nop
+            let mut lengths = TypedBuffer::<i32>::try_with_capacity(&NopBufferManager, num_values)?;
+            let len_slice = &mut lengths.as_slice_mut()[..num_values]; // May overallocate
+            let mut dec = DeltaBinaryPackedValueDecoder::<i32>::try_new(cursor)?;
+            dec.read(len_slice)?;
+
+            let cursor = dec.try_into_cursor()?;
+
+            Ok((lengths, cursor))
+        };
+
+        let (prefix_lengths, cursor) = read_lengths(cursor)?;
+        let (suffix_lengths, cursor) = read_lengths(cursor)?;
+
+        Ok(DeltaByteArrayDecoder {
+            verify_utf8,
+            val_buf: Vec::new(),
+            curr_len_idx: 0,
+            prefix_lengths,
+            suffix_lengths,
+            cursor,
+        })
     }
 
     pub fn read(
@@ -26,9 +71,62 @@ impl DeltaByteArrayDecoder {
         let (data, validity) = output.data_and_validity_mut();
         let mut data = PhysicalBinary::get_addressable_mut(data)?;
 
+        let prefix_lens = self.prefix_lengths.as_slice();
+        let suffix_lens = self.suffix_lengths.as_slice();
+
         match definitions {
-            Definitions::HasDefinitions { levels, max } => Ok(()),
-            Definitions::NoDefinitions => Ok(()),
+            Definitions::HasDefinitions { levels, max } => {
+                for (output_idx, &level) in levels.iter().enumerate().skip(offset).take(count) {
+                    if level < max {
+                        // Value is null.
+                        validity.set_invalid(output_idx);
+                        continue;
+                    }
+
+                    let prefix = prefix_lens[self.curr_len_idx] as usize;
+                    let suffix = suffix_lens[self.curr_len_idx] as usize;
+                    self.curr_len_idx += 1;
+
+                    self.val_buf.truncate(prefix);
+                    if self.cursor.remaining() < suffix {
+                        return Err(DbError::new("Not enough bytes remaining in the cursor"));
+                    }
+                    let bs = unsafe { self.cursor.read_bytes_unchecked(suffix) };
+                    self.val_buf.extend_from_slice(bs);
+
+                    if self.verify_utf8 {
+                        let _ = std::str::from_utf8(&self.val_buf)
+                            .context("Did not read valid utf8")?;
+                    }
+
+                    data.put(output_idx, &self.val_buf);
+                }
+
+                Ok(())
+            }
+            Definitions::NoDefinitions => {
+                for output_idx in offset..(offset + count) {
+                    let prefix = prefix_lens[self.curr_len_idx] as usize;
+                    let suffix = suffix_lens[self.curr_len_idx] as usize;
+                    self.curr_len_idx += 1;
+
+                    self.val_buf.truncate(prefix);
+                    if self.cursor.remaining() < suffix {
+                        return Err(DbError::new("Not enough bytes remaining in the cursor"));
+                    }
+                    let bs = unsafe { self.cursor.read_bytes_unchecked(suffix) };
+                    self.val_buf.extend_from_slice(bs);
+
+                    if self.verify_utf8 {
+                        let _ = std::str::from_utf8(&self.val_buf)
+                            .context("Did not read valid utf8")?;
+                    }
+
+                    data.put(output_idx, &self.val_buf);
+                }
+
+                Ok(())
+            }
         }
     }
 }
