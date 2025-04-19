ac67b93f19849dc57fdb6103d51f45f426ac8bca
Sean Smith <scsmithr@gmail.com>
2025-04-19 12:51:08 -0500

    vec

--- crates/glaredb_core/src/buffer/buffer_manager.rs
+++ crates/glaredb_core/src/buffer/buffer_manager.rs
@@ -35,6 +35,10 @@ where
             let manager = unsafe { ptr.cast::<Self>().as_ref().unwrap() };
             manager.reserve(size, align)
         },
+        resize_fn: |ptr, reservation, new_size| -> Result<()> {
+            let manager = unsafe { ptr.cast::<Self>().as_ref().unwrap() };
+            manager.resize(reservation, new_size)
+        },
         free_reservation_fn: |ptr: *const (), reservation: &mut Reservation| {
             let manager = unsafe { ptr.cast::<Self>().as_ref().unwrap() };
             manager.free_reservation(reservation);
@@ -74,6 +78,8 @@ unsafe impl Send for RawBufferManager {}
 pub struct RawBufferManagerVTable {
     /// Function called when attempting to reserve some number of bytes.
     reserve_fn: unsafe fn(*const (), usize, usize) -> Result<Reservation>,
+    /// Function when resizing a reservation.
+    resize_fn: unsafe fn(*const (), &mut Reservation, usize) -> Result<()>,
     /// Function called when a reservation should be freed.
     free_reservation_fn: unsafe fn(*const (), reservation: &mut Reservation),
 }
@@ -91,22 +97,28 @@ impl RawBufferManager {
         }
     }
 
-    pub(crate) unsafe fn call_free_reservation(&self, reservation: &mut Reservation) {
-        unsafe { (self.vtable.free_reservation_fn)(self.manager, reservation) }
+    pub(crate) unsafe fn call_reserve(&self, size: usize, align: usize) -> Result<Reservation> {
+        unsafe { (self.vtable.reserve_fn)(self.manager, size, align) }
     }
 
-    pub(crate) unsafe fn call_reserve(
+    pub(crate) unsafe fn call_resize(
         &self,
-        num_bytes: usize,
-        align: usize,
-    ) -> Result<Reservation> {
-        unsafe { (self.vtable.reserve_fn)(self.manager, num_bytes, align) }
+        reservation: &mut Reservation,
+        new_size: usize,
+    ) -> Result<()> {
+        unsafe { (self.vtable.resize_fn)(self.manager, reservation, new_size) }
+    }
+
+    pub(crate) unsafe fn call_free_reservation(&self, reservation: &mut Reservation) {
+        unsafe { (self.vtable.free_reservation_fn)(self.manager, reservation) }
     }
 }
 
 #[derive(Debug)]
 pub struct Reservation {
     /// Pointer to the start of the memory reservation.
+    ///
+    /// This pointer must always be well-aligned even for a size of zero.
     ptr: NonNull<u8>,
     /// Size in bytes of the memory reservation.
     size: usize,
@@ -134,6 +146,28 @@ pub struct DefaultBufferManager;
 
 impl BufferManager for DefaultBufferManager {
     fn reserve(&self, size: usize, align: usize) -> Result<Reservation> {
+        if align == 0 {
+            return Err(DbError::new("Cannot have zero alignment"));
+        }
+
+        // If we're trying to allocate zero bytes, we need avoid hitting the
+        // global allocator since that's UB.
+        if size == 0 {
+            // Create well-aligned dangling pointer. It's important we do this
+            // since a vec containing zero values needs to still be aligned.
+            let dangling = std::ptr::without_provenance::<u8>(align);
+            // SAFETY: We create a pointer using `align` as the address, and we
+            // already checked that `align` is non-zero.
+            let dangling = unsafe { NonNull::new_unchecked(dangling.cast_mut()) };
+
+            // Now just return the empty reservation with the dangling pointer.
+            return Ok(Reservation {
+                ptr: dangling.cast(),
+                size,
+                align,
+            });
+        }
+
         let layout =
             Layout::from_size_align(size, align).context("failed to create memory layout")?;
         // SAFETY: Alloc returns a pointer suitably aligned or null.
@@ -150,6 +184,18 @@ impl BufferManager for DefaultBufferManager {
     }
 
     fn resize(&self, reservation: &mut Reservation, new_size: usize) -> Result<()> {
+        if reservation.size() == new_size {
+            // Nothing to do.
+            return Ok(());
+        }
+
+        if reservation.size() == 0 {
+            // We have nothing to reallocate, and we have a dangling pointer.
+            // Just create a new reservation.
+            *reservation = self.reserve(new_size, reservation.align())?;
+            return Ok(());
+        }
+
         let old_layout = Layout::from_size_align(reservation.size, reservation.align)
             .context("failed to create current memory layout")?;
 
@@ -166,8 +212,75 @@ impl BufferManager for DefaultBufferManager {
     }
 
     fn free_reservation(&self, reservation: &mut Reservation) {
+        if reservation.size() == 0 {
+            // Nothing to deallocate, and our pointer is dangling.
+            return;
+        }
+
         // SAFETY: Matches the same Layout we used to alloc.
         let layout = Layout::from_size_align(reservation.size, reservation.align).unwrap();
         unsafe { alloc::dealloc(reservation.ptr.as_ptr(), layout) };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_reserve_basic() {
+        let mut reservation = DefaultBufferManager.reserve(4, 2).unwrap();
+        assert_eq!(4, reservation.size());
+        assert_eq!(2, reservation.align());
+        DefaultBufferManager.free_reservation(&mut reservation);
+    }
+
+    #[test]
+    fn default_reserve_zero_size() {
+        let mut reservation = DefaultBufferManager.reserve(0, 2).unwrap();
+        assert_eq!(0, reservation.size());
+        assert_eq!(2, reservation.align());
+        DefaultBufferManager.free_reservation(&mut reservation);
+    }
+
+    #[test]
+    fn default_reserve_zero_align_error() {
+        DefaultBufferManager.reserve(4, 0).unwrap_err();
+    }
+
+    #[test]
+    fn default_resize_grow() {
+        let mut reservation = DefaultBufferManager.reserve(4, 2).unwrap();
+        DefaultBufferManager.resize(&mut reservation, 8).unwrap();
+        assert_eq!(8, reservation.size());
+        assert_eq!(2, reservation.align());
+        DefaultBufferManager.free_reservation(&mut reservation);
+    }
+
+    #[test]
+    fn default_resize_shrink() {
+        let mut reservation = DefaultBufferManager.reserve(4, 2).unwrap();
+        DefaultBufferManager.resize(&mut reservation, 2).unwrap();
+        assert_eq!(2, reservation.size());
+        assert_eq!(2, reservation.align());
+        DefaultBufferManager.free_reservation(&mut reservation);
+    }
+
+    #[test]
+    fn default_resize_grow_from_zero() {
+        let mut reservation = DefaultBufferManager.reserve(0, 2).unwrap();
+        DefaultBufferManager.resize(&mut reservation, 8).unwrap();
+        assert_eq!(8, reservation.size());
+        assert_eq!(2, reservation.align());
+        DefaultBufferManager.free_reservation(&mut reservation);
+    }
+
+    #[test]
+    fn default_resize_same_size() {
+        let mut reservation = DefaultBufferManager.reserve(4, 2).unwrap();
+        DefaultBufferManager.resize(&mut reservation, 4).unwrap();
+        assert_eq!(4, reservation.size());
+        assert_eq!(2, reservation.align());
+        DefaultBufferManager.free_reservation(&mut reservation);
     }
 }
--- crates/glaredb_core/src/buffer/db_vec.rs
+++ crates/glaredb_core/src/buffer/db_vec.rs
@@ -4,7 +4,16 @@ use std::ptr::NonNull;
 use glaredb_error::Result;
 
 use super::buffer_manager::{AsRawBufferManager, RawBufferManager, Reservation};
+use crate::util::marker::PhantomCovariant;
 
+/// A `Vec` type that's backed by a buffer manager.
+///
+/// This should not be used as a general purpose replacement for
+/// `std::vec::Vec`. This is meant for allocations that last the life time of a
+/// query.
+///
+/// Memory is left unitialized after allocating. Care needs to be taken to
+/// ensure the vec is written to before it's read from.
 #[derive(Debug)]
 pub struct DbVec<T> {
     raw: RawDbVec<T>,
@@ -12,45 +21,128 @@ pub struct DbVec<T> {
 }
 
 impl<T> DbVec<T> {
+    /// Create a new vec backed by the given manager with an initial of _at
+    /// least_ `len`.
+    ///
+    /// This will allocate memory needed, however that memory will remain
+    /// unitilialized. It's important that a value is written to a given index
+    /// before it's read.
     pub fn new_uninit(manager: &impl AsRawBufferManager, len: usize) -> Result<Self> {
-        unimplemented!()
+        let raw = RawDbVec::new_uninit(manager, len)?;
+        Ok(DbVec { raw, len })
     }
 
     pub const fn len(&self) -> usize {
         self.len
     }
 
-    pub fn as_slice(&self) -> &[T] {
-        unimplemented!()
+    pub const fn capacity(&self) -> usize {
+        self.raw.capacity()
     }
 
-    pub fn as_slice_mut(&mut self) -> &mut [T] {
-        unimplemented!()
+    /// Get a slice reference for the elements in this vec.
+    ///
+    /// # Safety
+    ///
+    /// - Reading an element at a given index requires that it's been written to
+    ///   previously.
+    /// - There should exist no mutable references to anything within this vec's
+    ///   allocation (by creating it from a raw pointer).
+    pub unsafe fn as_slice(&self) -> &[T] {
+        debug_assert!(self.len() <= self.capacity());
+        let ptr = self.raw.ptr().cast().as_ptr();
+        unsafe { std::slice::from_raw_parts(ptr, self.len) }
+    }
+
+    /// Get a mutable slice reference for elements in the vec.
+    ///
+    /// This can be used to initialize the memory.
+    ///
+    /// # Safety
+    ///
+    /// - Reading an element at a given index requires that it's been written to
+    ///   previously.
+    /// - There should exist no mutable or immutable references to anything
+    ///   within this vec's allocation (by creating it from a raw pointer).
+    pub unsafe fn as_slice_mut(&mut self) -> &mut [T] {
+        debug_assert!(self.len() <= self.capacity());
+        let ptr = self.raw.ptr().cast().as_ptr();
+        unsafe { std::slice::from_raw_parts_mut(ptr, self.len) }
+    }
+}
+
+impl<T> Drop for DbVec<T> {
+    fn drop(&mut self) {
+        unsafe {
+            // SAFETY: If we have a dangling reference somewhere, we already
+            // lost.
+            let slice = self.as_slice_mut();
+            std::ptr::drop_in_place(slice);
+        }
+
+        // `raw` implicitly dropped...
     }
 }
 
 #[derive(Debug)]
 pub(crate) struct RawDbVec<T> {
-    pub(crate) manager: RawBufferManager,
+    manager: RawBufferManager,
     /// Memory reservation for this buffer.
     ///
     /// Stores the size in bytes of this buffer.
-    pub(crate) reservation: Reservation,
-    /// Raw pointer to start of vec.
-    pub(crate) ptr: NonNull<MaybeUninit<T>>,
+    reservation: Reservation,
     /// Capacity for the number of elements this buffer can
     /// hold.
     ///
     /// This is needed in additional to the reservation to properly handle
     /// zero-sized types (untyped null).
-    pub(crate) capacity: usize,
+    capacity: usize,
+    _t: PhantomCovariant<T>,
 }
 
 unsafe impl<T> Send for RawDbVec<T> where T: Send {}
 unsafe impl<T> Sync for RawDbVec<T> where T: Sync {}
 
 impl<T> RawDbVec<T> {
-    pub fn new_uninit(manager: &impl AsRawBufferManager, len: usize) -> Result<Self> {
-        unimplemented!()
+    pub fn new_uninit(manager: &impl AsRawBufferManager, cap: usize) -> Result<Self> {
+        let manager = manager.as_raw_buffer_manager();
+
+        let size = std::mem::size_of::<T>() * cap;
+        let align = std::mem::align_of::<T>();
+        let reservation = unsafe { manager.call_reserve(size, align) }?;
+
+        let capacity = if size == 0 {
+            // Zero-sized type, we can fit up to usize::MAX elements.
+            usize::MAX
+        } else {
+            reservation.size() / std::mem::size_of::<T>()
+        };
+
+        Ok(RawDbVec {
+            manager,
+            reservation,
+            capacity,
+            _t: PhantomCovariant::new(),
+        })
+    }
+
+    pub const fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn ptr(&self) -> NonNull<MaybeUninit<T>> {
+        self.reservation.ptr().cast()
+    }
+
+    pub fn resize(&mut self, new_size: usize) -> Result<()> {
+        unsafe { self.manager.call_resize(&mut self.reservation, new_size)? };
+        self.capacity = self.reservation.size() / std::mem::size_of::<T>();
+        Ok(())
+    }
+}
+
+impl<T> Drop for RawDbVec<T> {
+    fn drop(&mut self) {
+        unsafe { self.manager.call_free_reservation(&mut self.reservation) };
     }
 }
