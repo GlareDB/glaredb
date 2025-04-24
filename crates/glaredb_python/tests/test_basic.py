71ab24febf09a615d89cdec7f8d2c4426498c02c
Sean Smith <scsmithr@gmail.com>
2025-04-24 16:40:37 -0500

    fix dropping tokio rt too early

--- crates/glaredb_python/tests/test_basic.py
+++ crates/glaredb_python/tests/test_basic.py
@@ -1,8 +1,24 @@
 import glaredb
 import pytest
 
+# TODO: Assert outputs
 
-def test_conn_open_simple_query():
+
+def test_simple_query():
     con = glaredb.connect()
     res = con.sql("SELECT 3")
+    print(res)
+
+
+def test_invalid_query():
+    con = glaredb.connect()
+    with pytest.raises(Exception):
+        con.sql("CHOOSE 3")
+
+
+def test_query_with_remote_io():
+    # Losing the tokio task handle is very easy to mess up, ensure we're not
+    # dropping it...
+    con = glaredb.connect()
+    res = con.sql("SELECT * FROM 's3://glaredb-public/userdata0.parquet' LIMIT 5")
     print(res)
