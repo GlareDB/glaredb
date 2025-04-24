1f4c3f6ad5f3cabce849dee88ccc8609e2bf107c
Sean Smith <scsmithr@gmail.com>
2025-04-24 12:22:29 -0500

    maybe

--- test_bin/integration_slt.rs
+++ test_bin/integration_slt.rs
@@ -91,7 +91,7 @@ where
             engine,
             vars: ReplacementVars::default(),
             create_slt_tmp: false,
-            query_timeout: Duration::from_secs(10),
+            query_timeout: Duration::from_secs(5),
         })
     }
 }
