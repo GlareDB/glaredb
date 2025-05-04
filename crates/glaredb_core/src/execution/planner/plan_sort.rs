6c7cd151c46b2c08a12809749740b585389e31cb
Sean Smith <scsmithr@gmail.com>
2025-05-04 09:52:20 -0500

    rename global sort

--- crates/glaredb_core/src/execution/planner/plan_sort.rs
+++ crates/glaredb_core/src/execution/planner/plan_sort.rs
@@ -1,7 +1,7 @@
 use glaredb_error::Result;
 
 use super::OperatorPlanState;
-use crate::execution::operators::sort::PhysicalSort;
+use crate::execution::operators::sort::global_sort::PhysicalGlobalSort;
 use crate::execution::operators::{PlannedOperator, PlannedOperatorWithChildren};
 use crate::logical::logical_order::LogicalOrder;
 use crate::logical::operator::{LogicalNode, Node};
@@ -21,7 +21,7 @@ impl OperatorPlanState<'_> {
             .expr_planner
             .plan_sorts(&input_refs, &order.node.exprs)?;
 
-        let sort = PhysicalSort::new(sort_exprs, child.operator.call_output_types());
+        let sort = PhysicalGlobalSort::new(sort_exprs, child.operator.call_output_types());
 
         Ok(PlannedOperatorWithChildren {
             operator: PlannedOperator::new_execute(self.id_gen.next_id(), sort),
