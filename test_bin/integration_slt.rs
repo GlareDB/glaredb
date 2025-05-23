50ea4c9f1ab0b73d534fd11ff90baef5154aade5
Sean Smith <scsmithr@gmail.com>
2025-05-23 18:18:42 -0500

    column replacements

--- test_bin/integration_slt.rs
+++ test_bin/integration_slt.rs
@@ -301,14 +301,43 @@ where
     fn setup(engine: SingleUserEngine<E, R>) -> Result<RunConfig<E, R>> {
         engine.register_extension(ParquetExtension)?;
 
-        // run_setup_query(
-        //     &engine,
-        //     "
-        //     CREATE TEMP VIEW hits AS
-        //       SELECT * REPLACE (EventDate::DATE AS EventDate)
-        //         FROM read_parquet('../submodules/testdata/clickbench/single/hits_truncated.parquet')
-        //     ",
-        // )?;
+        run_setup_query(
+            &engine,
+            "
+            CREATE TEMP VIEW hits AS
+              SELECT * REPLACE (
+                       EventDate::DATE            AS  EventDate,
+                       Title::TEXT                AS  Title,
+                       URL::TEXT                  AS  URL,
+                       Referer::TEXT              AS  Referer,
+                       FlashMinor2::TEXT          AS  FlashMinor2,
+                       UserAgentMinor::TEXT       AS  UserAgentMinor,
+                       MobilePhoneModel::TEXT     AS  MobilePhoneModel,
+                       SearchPhrase::TEXT         AS  SearchPhrase,
+                       PageCharset::TEXT          AS  PageCharset,
+                       OriginalURL::TEXT          AS  OriginalURL,
+                       HitColor::TEXT             AS  HitColor,
+                       BrowserLanguage::TEXT      AS  BrowserLanguage,
+                       BrowserCountry::TEXT       AS  BrowserCountry,
+                       SocialNetwork::TEXT        AS  SocialNetwork,
+                       SocialAction::TEXT         AS  SocialAction,
+                       SocialSourcePage::TEXT     AS  SocialSourcePage,
+                       ParamOrderID::TEXT         AS  ParamOrderID,
+                       ParamCurrency::TEXT        AS  ParamCurrency,
+                       OpenstatServiceName::TEXT  AS  OpenstateServiceName,
+                       OpenstatCampaignID::TEXT   AS  OpenstatCampaignID,
+                       OpenstatAdID::TEXT         AS  OpenstatAdID,
+                       OpenstatSourceID::TEXT     AS  OpenstatSourceID,
+                       UTMSource::TEXT            AS  UTMSource,
+                       UTMMedium::TEXT            AS  UTMMedium,
+                       UTMCampaign::TEXT          AS  UTMCampaign,
+                       UTMContent::TEXT           AS  UTMContent,
+                       UTMTerm::TEXT              AS  UTMTerm,
+                       FromTag::TEXT              AS  FromTag
+                       )
+                FROM read_parquet('../submodules/testdata/clickbench/partitioned/hits_truncated_*.parquet')
+            ",
+        )?;
 
         Ok(RunConfig {
             engine,
