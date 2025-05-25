6b634b0aab4f4a4ea6d53644c6496c272854abd3
Sean Smith <scsmithr@gmail.com>
2025-05-25 16:21:30 -0500

    compile all branches

--- crates/glaredb_bench/src/pagecache.rs
+++ crates/glaredb_bench/src/pagecache.rs
@@ -1,16 +1,16 @@
+use std::io::Write;
 use std::process::Command;
 
 use glaredb_error::{DbError, Result};
 
 pub fn drop_page_cache() -> Result<()> {
-    #[cfg(target_os = "linux")]
-    {
+    if cfg!(target_os = "linux") {
         let status = Command::new("sync").status()?;
         if !status.success() {
-            Err(DbError::new(format!(
+            return Err(DbError::new(format!(
                 "'sync' failed with status {}",
                 status
-            )))
+            )));
         }
 
         // Requires sudo
@@ -20,25 +20,20 @@ pub fn drop_page_cache() -> Result<()> {
         // "3" = drop pagecache + dentries + inodes
         f.write_all(b"3\n")?;
         f.flush()?;
-        Ok(())
-    }
 
-    #[cfg(target_os = "macos")]
-    {
+        Ok(())
+    } else if cfg!(target_os = "macos") {
         // Use purge, requires xcode tools and sudo.
         let status = Command::new("purge").status()?;
         if !status.success() {
-            Err(DbError::new(format!(
+            return Err(DbError::new(format!(
                 "'purge' failed with status {}",
                 status
-            )))
-        } else {
-            Ok(())
+            )));
         }
-    }
 
-    #[cfg(not(any(target_os = "linux", target_os = "macos")))]
-    {
+        Ok(())
+    } else {
         Err(DbError::new(
             "dropping file cache is only implemented for Linux and macOS",
         ))
