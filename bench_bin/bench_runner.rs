a35c1027e3a4bb0ae81e98f5aecb7ec3ceb9d0bc
Sean Smith <scsmithr@gmail.com>
2025-05-28 12:42:43 -0500

    chore: Actually add clickbench partitioned

--- bench_bin/bench_runner.rs
+++ bench_bin/bench_runner.rs
@@ -32,6 +32,12 @@ pub fn main() -> Result<()> {
         "clickbench-parquet-single",
     )?;
 
+    run_with_setup::<ClickbenchPartitionedSetup>(
+        &args,
+        "../bench/clickbench/partitioned",
+        "clickbench-parquet-partitioned",
+    )?;
+
     Ok(())
 }
 
@@ -103,6 +109,61 @@ where
             CREATE TEMP VIEW hits AS
               SELECT * REPLACE (EventDate::DATE AS EventDate)
                 FROM read_parquet('../bench/data/clickbench/hits.parquet')
+            ",
+        )?;
+
+        Ok(engine)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct ClickbenchPartitionedSetup;
+
+impl<E, R> EngineSetup<E, R> for ClickbenchPartitionedSetup
+where
+    E: PipelineRuntime,
+    R: SystemRuntime,
+{
+    fn setup(engine: SingleUserEngine<E, R>) -> Result<SingleUserEngine<E, R>> {
+        register_default_extensions(&engine)?;
+
+        run_setup_query(
+            &engine,
+            "
+            CREATE TEMP VIEW hits AS
+              SELECT * REPLACE (
+                       EventDate::DATE            AS  EventDate,
+                       Title::TEXT                AS  Title,
+                       URL::TEXT                  AS  URL,
+                       Referer::TEXT              AS  Referer,
+                       FlashMinor2::TEXT          AS  FlashMinor2,
+                       UserAgentMinor::TEXT       AS  UserAgentMinor,
+                       MobilePhoneModel::TEXT     AS  MobilePhoneModel,
+                       Params::TEXT               AS  Params,
+                       SearchPhrase::TEXT         AS  SearchPhrase,
+                       PageCharset::TEXT          AS  PageCharset,
+                       OriginalURL::TEXT          AS  OriginalURL,
+                       HitColor::TEXT             AS  HitColor,
+                       BrowserLanguage::TEXT      AS  BrowserLanguage,
+                       BrowserCountry::TEXT       AS  BrowserCountry,
+                       SocialNetwork::TEXT        AS  SocialNetwork,
+                       SocialAction::TEXT         AS  SocialAction,
+                       SocialSourcePage::TEXT     AS  SocialSourcePage,
+                       ParamOrderID::TEXT         AS  ParamOrderID,
+                       ParamCurrency::TEXT        AS  ParamCurrency,
+                       OpenstatServiceName::TEXT  AS  OpenstatServiceName,
+                       OpenstatCampaignID::TEXT   AS  OpenstatCampaignID,
+                       OpenstatAdID::TEXT         AS  OpenstatAdID,
+                       OpenstatSourceID::TEXT     AS  OpenstatSourceID,
+                       UTMSource::TEXT            AS  UTMSource,
+                       UTMMedium::TEXT            AS  UTMMedium,
+                       UTMCampaign::TEXT          AS  UTMCampaign,
+                       UTMContent::TEXT           AS  UTMContent,
+                       UTMTerm::TEXT              AS  UTMTerm,
+                       FromTag::TEXT              AS  FromTag
+                       )
+                FROM read_parquet('../bench/data/clickbench/partitioned/hits_*.parquet')
+
             ",
         )?;
 
