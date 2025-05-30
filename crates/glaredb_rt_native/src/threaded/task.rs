bcc8005ae882edaa996eed6fb7760c1b094d68a9
Sean Smith <scsmithr@gmail.com>
2025-05-30 17:47:52 -0500

    chore: Rework task state to avoid executing completed pipelines (#3875)

--- crates/glaredb_rt_native/src/threaded/task.rs
+++ crates/glaredb_rt_native/src/threaded/task.rs
@@ -11,23 +11,29 @@ use rayon::ThreadPool;
 use crate::time::NativeInstant;
 
 #[derive(Debug)]
+pub(crate) struct ScheduleState {
+    pub(crate) running: bool,
+    pub(crate) pending: bool,
+    pub(crate) completed: bool,
+    pub(crate) canceled: bool,
+}
+
+#[derive(Debug)]
 pub(crate) struct TaskState {
-    /// The partition pipeline we're operating on alongside a boolean for if the
-    /// query's been canceled.
-    pub(crate) pipeline: Mutex<PipelineState>,
+    /// The partition pipeline we're operating on.
+    pub(crate) pipeline: Mutex<ExecutablePartitionPipeline>,
     /// Error sink for any errors that occur during execution.
     pub(crate) errors: Arc<dyn ErrorSink>,
     /// The threadpool to execute on.
     pub(crate) pool: Arc<ThreadPool>,
     /// Where to put the profile when this pipeline completes.
     pub(crate) profile_sink: ProfileSink,
+    pub(crate) sched_state: Mutex<ScheduleState>,
 }
 
 impl Wake for TaskState {
     fn wake(self: Arc<Self>) {
-        let pool = self.pool.clone();
-        let task = PartitionPipelineTask { state: self };
-        pool.spawn(|| task.execute());
+        self.schedule();
     }
 
     fn wake_by_ref(self: &Arc<Self>) {
@@ -35,49 +41,65 @@ impl Wake for TaskState {
     }
 }
 
-#[derive(Debug)]
-pub(crate) struct PipelineState {
-    pub(crate) pipeline: ExecutablePartitionPipeline,
-    pub(crate) query_canceled: bool,
-}
-
-/// Task for executing a partition pipeline.
-pub struct PartitionPipelineTask {
-    state: Arc<TaskState>,
-}
-
-impl PartitionPipelineTask {
-    pub(crate) fn from_task_state(state: Arc<TaskState>) -> Self {
-        PartitionPipelineTask { state }
-    }
-
-    pub(crate) fn execute(self) {
-        let mut pipeline_state = self.state.pipeline.lock();
-
-        if pipeline_state.query_canceled {
-            self.state.errors.set_error(DbError::new("Query canceled"));
+impl TaskState {
+    pub(crate) fn schedule(self: Arc<Self>) {
+        let mut sched_guard = self.sched_state.lock();
+        if sched_guard.completed {
+            return;
+        }
+        if sched_guard.canceled {
+            self.errors.set_error(DbError::new("Query canceled"));
             return;
         }
 
-        let waker: Waker = self.state.clone().into();
+        if sched_guard.running {
+            sched_guard.pending = true;
+        } else {
+            sched_guard.running = true;
+            std::mem::drop(sched_guard);
+
+            let state = self.clone();
+            self.pool.spawn(move || {
+                loop {
+                    let completed = state.execute();
+
+                    let mut sched_guard = state.sched_state.lock();
+                    sched_guard.completed = completed;
+                    if sched_guard.pending {
+                        sched_guard.pending = false;
+                        // Continue...
+                    } else {
+                        // Nom more work.
+                        sched_guard.running = false;
+                        break;
+                    }
+                }
+            });
+        }
+    }
+
+    fn execute(self: &Arc<Self>) -> bool {
+        let mut pipeline = self.pipeline.lock();
+
+        let waker: Waker = self.clone().into();
         let mut cx = Context::from_waker(&waker);
 
-        match pipeline_state
-            .pipeline
-            .poll_execute::<NativeInstant>(&mut cx)
-        {
+        match pipeline.poll_execute::<NativeInstant>(&mut cx) {
             Poll::Ready(Ok(prof)) => {
                 // Pushing through the pipeline was successful. Put our profile.
                 // We'll never execute again.
-                self.state.profile_sink.put(prof);
+                self.profile_sink.put(prof);
+                true
             }
             Poll::Ready(Err(e)) => {
-                self.state.errors.set_error(e);
+                self.errors.set_error(e);
+                false
             }
             Poll::Pending => {
-                // Exit the loop. Waker was already stored in the pending
+                //  Waker was already stored in the pending
                 // sink/source, we'll be woken back up when there's more
                 // this operator chain can start executing.
+                false
             }
         }
     }
